e482ba63c8949e8f2cdeae9875023c0e9cd246d7
hyunja27 <123gk@naver.com>
2021-12-28 11:53:46 +0900

    12/28_11:54_make_app_ts_and_add_somw_line_index_ts_for_mariadb_connecting!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path';
 import swaggerUI from 'swagger-ui-express'
 import YAML from 'yamljs'
+import {getUserList} from './app'
 
 function makeServer(){
     const app = express();
@@ -10,6 +11,7 @@ function makeServer(){
     const swaggerSpec = YAML.load(path.join(__dirname, './swagger.yaml'));
     app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.listen(port, ()=>console.log(`Listening on port ${port}`));
+    getUserList();
 }
 
 
