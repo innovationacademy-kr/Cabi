d1064a5ef444bcf0908181b133af2e1532444aeb
윤현지 <hyoon@student.42seoul.kr>
2021-12-30 15:31:12 +0900

    :technologist: fix: api and http url

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 export default function Main(){
-        
+    const dev_url = 'http://localhost:4242/auth/login';
+    const dep_url = 'http://3.38.166.176/auth/login';
+
     const handleClick = () => {
-        axios.post('http://localhost:4242/auth/login',{
+        axios.post(dev_url ,{
             data: 'Hello!'
         }).then((res)=>console.log(res.data)).catch((err)=>console.log(err));
     }
