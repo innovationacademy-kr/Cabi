d46e06ad48f4d658738efa9aa2083fb537e448f0
kyoshong <hyospark@student.42seoul.kr>
2022-01-02 17:48:15 +0900

    :white_check_mark: feat: add User api

--- backend/src/db/db_dev.ts
+++ backend/src/db/db_dev.ts
@@ -1,13 +1,13 @@
 const mysqlssh = require('mysql-ssh')
 import fs from 'fs';
-import {getUser} from './query'
+import {checkUser} from './query'
 
 export function connection(){
     console.log('start!');
     mysqlssh.connect({
         host: 'cabi.42cadet.kr',
         user: 'ec2-user',
-        privateKey: fs.readFileSync('../key/swlabs-cadet.pem')
+        privateKey: fs.readFileSync('./src/key/swlabs-cadet.pem')
     },{
         host: '127.0.0.1',
         user: 'root',
@@ -15,7 +15,7 @@ export function connection(){
         database: '42cabi_DB'
     }).then((client:any)=>{
         console.log('connection!!');
-        getUser(client);
+        checkUser(client);
     }).catch((err:any)=>{
         console.log(err);
         throw err;
--- backend/src/db/query.ts
+++ backend/src/db/query.ts
@@ -3,7 +3,15 @@ import {user} from '../user'
 
 //사용자 확인 - 사용자가 없는 경우, addUser, 있는 경우, getUser
 export function checkUser(client:any){
-
+    client.query(`select * from user where user_id = ${user.user_id}`, function(err:any, res:any, field:any)
+    {
+        if (err) throw err;
+        console.log(res);
+        if (res)
+            console.log("addUser");
+        else
+            console.log("getUser");
+    })
 }
 //사용자가 없는 경우, user 값 생성
 export function addUser(client:any){
@@ -11,7 +19,7 @@ export function addUser(client:any){
 }
 //본인 정보 및 렌트 정보 - 리턴 페이지
 export function getUser(client:any){
-    client.query(`select * from user where user_id=${user.user_id}`, function(err:any, res:any, field:any){
+    client.query(`select * from lent where user_id=${user.user_id}`, function(err:any, res:any, field:any){
         if (err) throw err;
         console.log(res);
         console.log(typeof res);
@@ -21,7 +29,7 @@ export function getUser(client:any){
 }
 //cabinet 정보 가져오기 - 렌트 페이지
 export function getCabinetList(client:any){
-
+    client.query(``)
 }
 //lent 값 생성
 export function postLent(client:any){
