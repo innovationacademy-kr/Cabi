abbabba236d9c452d41b32ee7363e45ba8787587
hyunja27 <123gk@naver.com>
2022-01-03 18:29:13 +0900

    :rocket: 01/03_CORS_line_erased_in_backend_index_ts #14

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,29 +2,29 @@ import express from 'express'
 import path from 'path'
 import swaggerUI from 'swagger-ui-express'
 import YAML from 'yamljs'
-import cors from 'cors'
+// import cors from 'cors'
 import {getUserList} from './app'
 
 function makeServer(){
     const app = express();
     const port = process.env.PORT || 4242;
 
-    const list:Array<string> = [
-        "https://cabi.42cadet.kr"
-    ]
-    app.use(
-        cors({
-            origin: function (origin, callback){
-                if (!origin || list.indexOf(origin) >= 0) {
-                    callback(null, true)
-                } else {
-                    callback(new Error("Not allowed by CORS"))
-                }
-            },
-            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-            credentials: true,
-        })
-    );
+    // const list:Array<string> = [
+    //     "https://cabi.42cadet.kr"
+    // ]
+    // app.use(
+    //     cors({
+    //         origin: function (origin, callback){
+    //             if (!origin || list.indexOf(origin) >= 0) {
+    //                 callback(null, true)
+    //             } else {
+    //                 callback(new Error("Not allowed by CORS"))
+    //             }
+    //         },
+    //         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    //         credentials: true,
+    //     })
+    // );
 
     const swaggerSpec = YAML.load(path.join(__dirname, '../api/swagger.yaml'));
     app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
