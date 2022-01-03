ff85cbd8927130afdfae16baad8b5e01959c44ed
hyunja27 <123gk@naver.com>
2022-01-03 15:31:43 +0900

    :green_heart: 01/03_CI_githubAction_added #10

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-npm install -g pm2@latest
-pm2 install typescript
 cd frontend
 npm install
 npm run build
 cp -r dist/* ../backend/public
 cd ../backend
+npm install -g pm2@latest
+pm2 install typescript
 npm install
 pm2 start src/index.ts
---
+++ develop_back.sh
@@ -0,0 +1,4 @@
+cd backend
+npm install
+npm run doc
+nodemon ./src/index.ts
