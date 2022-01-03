7b02847c84c631d022e284dcece4614ed3c8cd79
hyunja27 <123gk@naver.com>
2022-01-03 15:48:13 +0900

    :bug: 01/03_pm2_installing_erased #10

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm install
 npm run build
 cp -r dist/* ../backend/public
 cd ../backend
-# npm install -g pm2@latest
+npm install -g pm2@latest
 # pm2 install typescript
 npm install
 pm2 start src/index.ts
