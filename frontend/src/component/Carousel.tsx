dad893b4d214f29b1715adb3c1f9d2630ab76d73
subin195-09 <k_subin@naver.com>
2022-01-09 14:59:37 +0900

    1/9 🐛 carousel bug update #21

--- frontend/src/component/Carousel.tsx
+++ frontend/src/component/Carousel.tsx
@@ -1,48 +1,8 @@
 import CabinetBox from './CabinetBox'
 import './carousel.css'
-
-export type cabinetInfo = {
-  cabinet_id: number,
-  cabinet_num: number,
-  location: string,
-  floor: number,
-  section: string,
-  activation: boolean,
-}
-//one location - ex) 새롬
-export type locationInfo = {
-  location?: Array<string>,
-  floor?: Array<Array<number>>,
-  section?: Array<Array<Array<string>>>,
-  cabinet?: Array<Array<Array<Array<cabinetInfo>>>>
-}
-type lentInfo = {
-  lent_id: number,
-  lent_cabinet_id: number,
-  lent_user_id: number,
-  lent_time?: string,
-  expire_time?: string,
-  extension: boolean,
-  intra_id?: string
-}
+import { cabinetInfo } from '../pages/Lent'
 
 export default function Carousel(props:any){
-  const navSection = (idx:number) => {
-    let list = [];
-    if (!props.info || !props.info.section || props.info.section[props.l_idx].length <= idx)
-      return [];
-    // console.log(props.info.section[props.l_idx][idx].length);
-    for (let i = 0; i < props.info.section[props.l_idx][idx].length; i++){
-      list.push(
-        <div className={`carousel-item${i ? '' : ' active'}`} key={`carousel-item_${props.info.section[props.l_idx][idx][i]}`} >
-          <label className="m-3" key={`label_${props.info.section[props.l_idx][idx][i]}`}>{props.info.section[props.l_idx][idx][i]}</label>
-          {cabinetBlock(idx, i)}
-        </div>
-      );
-    }
-    return list;
-  }
-
   const cabinetBlock = (f_idx:number, s_idx:number) => {
     if (!props.info || !props.info.cabinet)
       return [];
@@ -57,9 +17,25 @@ export default function Carousel(props:any){
     return list;
   }
 
+  const navSection = (idx:number) => {
+    let list = [];
+    if (!props.info || !props.info.section || props.info.section[props.l_idx].length <= idx)
+      return [];
+    console.log(props.info.section[props.l_idx][idx].length);
+    for (let i = 0; i < props.info.section[props.l_idx][idx].length; i++){
+      list.push(
+        <div className={`carousel-item${i ? '' : ' active'}`} key={`carousel-item_${props.info.section[props.l_idx][idx][i]}`} >
+          <label className="m-3" key={`label_${props.info.section[props.l_idx][idx][i]}`}>{props.info.section[props.l_idx][idx][i]}</label>
+          {cabinetBlock(idx, i)}
+        </div>
+      );
+    }
+    return list;
+  }
+
   return (
-      <div className={`tab-pane${props.outer_i ? '' : ' active'}`} id={`nav-${props.floor_name}`} key={`nav-${props.floor_name}`} role="tabpanel" aria-labelledby={`nav-${props.floor_name}-tab`}>
-      <div id={`carousel_${props.props.l_idx}_${props.floor_name}`} className="carousel slide" data-bs-ride="carousel">
+    <div className={`tab-pane${props.outer_i ? '' : ' active'}`} id={`nav-${props.floor_name}`} key={`nav-${props.floor_name}`} role="tabpanel" aria-labelledby={`nav-${props.floor_name}-tab`}>
+      <div id={`carousel_${props.l_idx}_${props.floor_name}`} className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner" key={`nav-inner${props.floor_name}`}>
           {navSection(props.outer_i)}
         </div>
