da631659873b60974951e7d49d6f0dfc0a1cd995
John Doe <ec2-user@ip-172-26-4-219.ap-northeast-2.compute.internal>
2022-01-13 08:15:43 +0000

    1/13 main reset

--- frontend/src/component/CabinetBox.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function CabinetBox(props:any){
-    return (
-        <div className={`border text-center${props.intra_id !== "" ? ' bg-secondary' : ''}`}  data-bs-toggle={props.intra_id !== '' ? '':'modal'} data-bs-target="#lentmodal">
-            <div>{props.cabinet_id}</div>
-            <div>{props.intra_id}</div>
-        </div>
-    );
-}
