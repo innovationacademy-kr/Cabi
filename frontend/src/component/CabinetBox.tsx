9787afa8ed9d708a22f3817ae6005045b889415c
kyoshong <hyospark@student.42seoul.kr>
2022-01-19 17:59:37 +0900

    :lipstick: fix: cabinetBox font

--- frontend/src/component/CabinetBox.tsx
+++ frontend/src/component/CabinetBox.tsx
@@ -1,12 +1,14 @@
+import './cabinetBox.css'
+
 export default function CabinetBox(props:any){
     const clickHandler = () => {
         props.setTarget(props.cabinet_id);
         props.setCabiNum(props.cabinet_num);
     }
     return (
-        <div className={`border text-center${props.intra_id !== "" ? ' bg-secondary' : ''}`}  data-bs-toggle={props.intra_id !== '' ? '':'modal'} data-bs-target={props.isLent === -1 ? "#lentmodal": "#contentsmodal"} onClick={clickHandler}>
-            <div>{props.cabinet_num}</div>
-            <div>{props.intra_id}</div>
+        <div className={`border justify-conten${props.intra_id !== "" ? ' bg-secondary' : ''}`}  data-bs-toggle={props.intra_id !== '' ? '':'modal'} data-bs-target={props.isLent === -1 ? "#lentmodal": "#contentsmodal"} onClick={clickHandler}>
+            <div id="cabinet_num">{props.cabinet_num}</div>
+            <div id="intra_id">{props.intra_id}</div>
         </div>
     );
 }
