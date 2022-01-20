2c557be01bf7e4fee92acf2c35df2c6ae5ca8f52
윤현지 <hyoon@student.42seoul.kr>
2022-01-20 14:14:03 +0900

    :adhesive_bandage: fix: location variable in lent page #34

--- frontend/src/component/Location.tsx
+++ frontend/src/component/Location.tsx
@@ -1,8 +1,9 @@
 import '../pages/lent.css'
 
 export default function Location(props:any){
-  const handleInfo = (loc:string) => {
-    props.setPresintInfo(loc);
+  const handleInfo = (loc:number) => {
+    // props.setPresentInfo(loc);
+    props.setLidx(loc);
   }
   let location:Array<string> = props.info.location;
   console.log(location);
@@ -11,11 +12,11 @@ export default function Location(props:any){
     <div className="dropdown" id="location">
       <button className="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	      <i className="bi bi-caret-down-fill"></i>
-        {props.presentInfo}
+        {location ? location[props.l_idx]: ''}
       </button>
       <div className="dropdown-menu" id="locationMenu" aria-labelledby="dropdownMenuButton">
-        {location?.map((loc:string)=>{
-          return <a className="dropdown-item" onClick={()=>handleInfo(loc)}>{loc}</a>
+        {location?.map((loc:string, idx:number)=>{
+          return <a className="dropdown-item" onClick={()=>handleInfo(idx)}>{loc}</a>
         })}
       </div>
     </div>
