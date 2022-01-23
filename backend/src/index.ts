76904bde9f27ae3418ca261c12d51efe3ad5161d
hyunja27 <123gk@naver.com>
2022-01-23 16:05:19 +0900

    :art: 01/23_sentry_added_is_it_right...? #40

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,14 @@ import dotenv from 'dotenv'
 // dotenv.config({path:'/home/ec2-user/git/42cabi-dev/backend/.env'}); //dev
 dotenv.config(); //local
 
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
 function makeServer() {
     const app = express();
     const port = process.env.PORT || 4242;
@@ -29,6 +37,16 @@ function makeServer() {
             })
         );
     }
+
+    Sentry.init({
+        dsn: "https://8d771150bd1e4339baf68d827c0c11b3@o1124326.ingest.sentry.io/6162624",
+        tracingOptions: { // 하위 구성 요소를 추적하고 렌더링 프로세스에 대한 자세한 내용을 보기
+           trackComponents: true,
+        },
+        attachProps: true, // 로깅을 위해 모든 Vue 구성 요소의 props를 보기
+        tracesSampleRate: 1, // 0에서 1 사이의 숫자로 주어진 트랜잭션이 Sentry로 전송 될 확률을 제어
+        // beforeSend: (event:any, hint:any) => this._sendErrorMessage(event, hint) // 에러를 Sentry에게 전달하기 전 처리할 수 있는 hook
+     });
 
     const swaggerSpec = YAML.load(path.join(__dirname, '../api/swagger.yaml'));
     app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
