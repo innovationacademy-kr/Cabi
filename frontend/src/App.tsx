4092607f1b9c8160c7309a4f2263b9b931980cd3
John Doe <ec2-user@ip-172-26-4-219.ap-northeast-2.compute.internal>
2022-01-23 07:46:57 +0000

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,17 +8,13 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <div id="wrap">
-          <section>
             <Switch>
               <Route exact path='/' component={Main}></Route>
               <Route path='/lent' component={Lent}></Route>
               <Route path='/return' component={Return}></Route>
               <Route component={Main}></Route>
             </Switch>
-          </section>
           <Footer />
-        </div>
       </div>
     </BrowserRouter>
   )
