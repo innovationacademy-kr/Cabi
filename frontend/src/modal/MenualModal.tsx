668ca096d9185509032ba892c280b6da5c83f0c0
kyoshong <hyospark@student.42seoul.kr>
2022-01-23 14:01:25 +0900

    :lipstick: fix: menualModal align

--- frontend/src/modal/MenualModal.tsx
+++ frontend/src/modal/MenualModal.tsx
@@ -10,8 +10,7 @@ export default function MenualModal() {
             <h4 className="modal-title align-middle px-10">🗄 42cabi 이용 안내서</h4>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div className='menual-body ali'>
-            <p>
+          <div className='menual-body' id='menual-body'>
               <ul>
                 <li className='py-1'>1인 당 1개의 사물함을 대여할 수 있고,<br />대여기간 동안 자유롭게 사용할 수 있습니다.<br /></li>
                 <li className='py-1'>대여기간은 대여한 날로 부터 +30일 입니다.<br /></li>
@@ -20,7 +19,6 @@ export default function MenualModal() {
                 <li className='py-1'>사물함에 상할 수 있는 음식물이나 사물함이 <br />오염 될 수 있는 물품 보관은 자제해주세요.<br /></li>
                 <li className='py-1'>대여한 사물함이 잠겨 있거나 비밀번호를 <br />분실하셨다면 프론트의 Staff 혹은 42cabi 슬랙 채널로 문의해주세요.<br /></li>
               </ul>
-            </p>
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-primary" id="btn-primary" data-bs-dismiss="modal">Ok! 알았어요!</button>
