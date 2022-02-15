be80a7dc0c9d3c99b6a74fabaa8b5dec6a0381d5
subin195-09 <k_subin@naver.com>
2022-02-15 20:07:59 +0900

    2/15 :sparkles: event add #49

--- frontend/src/pages/Return.tsx
+++ frontend/src/pages/Return.tsx
@@ -8,7 +8,6 @@ import PasswordModal from '../modal/PasswordModal'
 import ContentsModal from '../modal/ContentsModal'
 import './main.css'
 import './return.css'
-import ExtensionModal from '../modal/ExtensionModal'
 
 export type lentCabinetInfo = {
   lent_id: number,
@@ -16,7 +15,7 @@ export type lentCabinetInfo = {
   lent_user_id: number,
   lent_time: string,
   expire_time: string,
-  extension: number,
+  extension: boolean,
   cabinet_num: number,
   location: string,
   floor: number,
@@ -31,13 +30,12 @@ export default function Return() {
   const [content, setContent] = useState<string>('');
   const [cabinetPassword, setCabinetPassword] = useState<string>('');
   const [lentCabinet, setLentCabinet] = useState<lentCabinetInfo>();
-  const [extension, setExtension] = useState<string>(lentCabinet?.lent_id === -1 ? 'hidden' : lentCabinet && lentCabinet.extension > 0 ? 'disabled' : '');
 
   useEffect(() => {
     apiCheck().then(() => {
       callReturn();
     });
-  }, [content, path, extension]);
+  }, [content, path]);
 
   const apiCheck = async () => {
     await axios.post("/api/check").then((res: any) => {
@@ -51,74 +49,55 @@ export default function Return() {
     await axios.post("/api/return_info").then((res: any) => {
       if (res.status === 200) {
         setLentCabinet(res.data);
-        setExtension(res.data.lent_id === -1 ? 'hidden' : res.data.extension > 0 ? 'disabled' : '')
       }
     }).catch((err) => { console.log(err) });
   }
-  const handleHome = () => {
-    history.push("/lent");
-  }
-  // const handleExtension = async () => {
-  //   await axios.post("/api/extension").then((res:any)=>{
-  //     if (res.status === 200){
-  //       setContent('연장되었습니다!!');
-  //     }else{
-  //       setContent('다시 시도해주세요..');
-  //     }
-  //   }).catch((err)=>{console.log(err)});
-  // }
+
 
   return (
-    <div className="container col" id='container'>
-      <div className="row align-items-center">
-        <div className="col">
-          <div className="px-4"><img src="../img/cabinet.ico" onClick={handleHome} width="30" />
-          </div>
-        </div>
-        <div className="col">
+      <div className="container" id='container'>
+        <div className="row-2">
           <Menu url="/lent"></Menu>
         </div>
-      </div>
-      <div className="card row-2 p-5 m-5">
-        <div className="card-body my-5" id="card-body">
-          <React.Fragment>
-            {
-              lentCabinet?.lent_id === -1 ? <div className="card-subtitle mb-2 text-muted text-center">현재 대여중인 사물함이 없습니다.</div> :
-                <div>
-                  <div className="card-title text-center display-5">{lentCabinet?.location} {lentCabinet?.floor}F {lentCabinet?.cabinet_num}</div>
-                  <div className="card-subtitle mb-2 text-muted text-center">~ {lentCabinet?.expire_time}</div>
-                </div>
-            }
-            {
-              localStorage.getItem('cabinetPassword') ?
-                <div>
-                  <div className="text-center" id="passwordtext">[ {localStorage.getItem('cabinetPassword')} ]</div>
-                </div> : <div className="text-center" id="passwordtext"> &lt; 비밀스러운 메모장 &gt; </div>
-            }
-          </React.Fragment>
-        </div>
-      </div>
-      <div>
-        <div className="row-2 d-grid gap-2 col-6 mx-auto m-5">
-          <div className={`btn btn-lg ${lentCabinet?.lent_id === -1 ? 'hidden' : ''}`} id="colorBtn" data-bs-toggle="modal" data-bs-target="#returnmodal">
-            반납하기
+        <div className={`card row-2 p-5 m-5 ${typeof(lentCabinet?.lent_id) === "number" && lentCabinet?.lent_id % 42 === 0 ? "event" : ""}`}>
+          <div className="card-body my-5" id="card-body">
+            <React.Fragment>
+              {
+                lentCabinet?.lent_id === -1 ? <div className={`card-subtitle mb-2 text-muted text-center`}>현재 대여중인 사물함이 없습니다.</div> :
+                  <div>
+                    <div className="card-title text-center display-5">{lentCabinet?.location} {lentCabinet?.floor}F {lentCabinet?.cabinet_num}</div>
+                    <div className="card-subtitle mb-2 text-muted text-center">~ {lentCabinet?.expire_time}</div>
+                  </div>
+              }
+              {
+                localStorage.getItem('cabinetPassword') ?
+                  <div>
+                    <div className="text-center" id="passwordtext">[ {localStorage.getItem('cabinetPassword')} ]</div>
+                  </div> : <div className="text-center" id="passwordtext"> &lt; 비밀스러운 메모장 &gt; </div>
+              }
+            </React.Fragment>
           </div>
         </div>
-        <div className="row-2 d-grid gap-2 col-6 mx-auto m-5">
-          <div className={`btn btn-lg ${lentCabinet?.lent_id === -1 ? 'hidden' : ''}`} id="colorBtn" data-bs-toggle="modal" data-bs-target="#passwordmodal">
-            비밀번호 메모장
+        <div>
+          <div className="row-2 d-grid gap-2 col-6 mx-auto m-5">
+            <div className={`btn btn-lg ${lentCabinet?.lent_id === -1 ? 'hidden' : ''}`} id="colorBtn" data-bs-toggle="modal" data-bs-target="#returnmodal">
+              반납하기
+            </div>
+          </div>
+          <div className="row-2 d-grid gap-2 col-6 mx-auto m-5">
+            <div className={`btn btn-lg ${lentCabinet?.lent_id === -1 ? 'hidden' : ''}`} id="colorBtn" data-bs-toggle="modal" data-bs-target="#passwordmodal">
+              비밀번호 메모장
+            </div>
+          </div>
+          <div className="row-2 d-grid gap-2 col-6 mx-auto m-5">
+            <div className={`btn btn-lg ${lentCabinet?.lent_id === -1 ? 'hidden' : 'disabled'}`} id="colorBtn">
+              연장하기
+            </div>
           </div>
         </div>
-        <div className="row-2 d-grid gap-2 col-6 mx-auto m-5">
-          <div className={`btn btn-lg ${extension}`} id="colorBtn" data-bs-toggle="modal" data-bs-target="#extensionmodal">
-            연장하기
-          </div>
-        </div>
+        <ReturnModal lentCabinet={lentCabinet} setContent={setContent} setPath={setPath}></ReturnModal>
+        <PasswordModal cabinetPassword={cabinetPassword} setCabinetPassword={setCabinetPassword}></PasswordModal>
+        <ContentsModal contents={content} path={path}></ContentsModal>
       </div>
-      <ReturnModal lentCabinet={lentCabinet} setContent={setContent} setPath={setPath}></ReturnModal>
-      <PasswordModal cabinetPassword={cabinetPassword} setCabinetPassword={setCabinetPassword}></PasswordModal>
-      <ContentsModal contents={content} path={path}></ContentsModal>
-      <ExtensionModal setContent={setContent}></ExtensionModal>
-    </div>
   )
 }
