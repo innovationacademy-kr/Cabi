700ad22b4756c98f559e490cd04528235d43d0b6
윤현지 <hyoon@student.42seoul.kr>
2022-02-25 15:32:36 +0900

    :bug: fix: seromFloor grid #50

--- frontend/src/component/map/SeromFloor.tsx
+++ frontend/src/component/map/SeromFloor.tsx
@@ -1,9 +1,8 @@
-import cluster from "cluster";
 import "./seromFloor.css"
 
 export function SeromFloor(props:any){
-    const cluster1:number = props.f_idx === 4 ? 3 : 5;
-    const cluster2:number = props.f_idx === 4 ? 4 : 6;
+    const cluster1:number = props.f_idx === 1 ? 3 : 5;
+    const cluster2:number = props.f_idx === 1 ? 4 : 6;
 
     const findIdx = (name:string) => {
         return props.info?.section[props.l_idx][props.f_idx].findIndex((section:string)=>section === name) + 1;
@@ -12,7 +11,7 @@ export function SeromFloor(props:any){
         <div className="seromFloor">
             <div className="blank"></div>
             <div className="oa" data-bs-target={`#carousel_${props.l_idx}_${props.floor_name}`} data-bs-slide-to={findIdx(`Cluster ${cluster1} - OA`)}>
-                <div className="nameTag">{`Cluster ${cluster1} - OA`}</div>
+                <div className="nameTag">OA</div>
             </div>
             <div className="eoc1" data-bs-target={`#carousel_${props.l_idx}_${props.floor_name}`} data-bs-slide-to={findIdx(`End of Cluster ${cluster1}`)}>
                 <div className="nameTag">{`End of Cluster ${cluster1}`}</div>
