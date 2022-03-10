f895abe6490a3e08b69d70c9248d464c302830f7
sungHyun Park <sunghyunpark@sungHyunui-MacBookAir.local>
2022-03-10 17:59:21 +0900

    :children_crossing:03/10_expired_user_color_fixed #32

--- frontend/src/component/CabinetBox.tsx
+++ frontend/src/component/CabinetBox.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './cabinetBox.css'
 
 export default function CabinetBox(props:any){
   const history = useHistory();
+  let isExpired:boolean = false;
+
+  if (props.isLent && props.expire_time){
+    isExpired = new Date(props.expire_time) < new Date();
+  }
+
   const clickHandler = () => {
     props.setTarget(props.cabinet_id);
     props.setCabiNum(props.cabinet_num);
@@ -14,9 +21,12 @@ export default function CabinetBox(props:any){
   let toggleName = props.intra_id !== "" ? "" : "modal";
   let targetName = props.isLent === -1 ? "#lentmodal" : "#contentsmodal";
   let vanilaClassName = `border justify-content-center${props.intra_id !== "" ? " lentCabinet" : ""}`;
-  if (props.intra_id === props.user)
-  {
-    vanilaClassName = `border justify-content-center myLentCabinet`;
+  if (props.intra_id === props.user){
+
+    if (isExpired)
+      vanilaClassName = `border justify-content-center expiredLentCabinet`;
+    else
+      vanilaClassName = `border justify-content-center myLentCabinet`;
     toggleName = "modal";
     targetName = "";
     clickEvent = clickMyCabinet;
