b0af56235810f51efeb44e02e5be3d9038ac3168
kyoshong <hyospark@student.42seoul.kr>
2022-03-10 10:51:46 +0000

    :white_check_mark: update dev branch #53

--- backend/app.ts
+++ backend/app.ts
@@ -14,15 +14,13 @@ import slack from './controllers/middleware/slack'
 import dotenv from 'dotenv'
 
 const env = process.env;
-if (process.env.USER === 'ec2-user'){
-	if (env.PORT == '4242')
-        dotenv.config({path:'/home/ec2-user/git/42cabi/backend/.env'}); //dep
-    else
-        dotenv.config({path:'/home/ec2-user/git/42cabi-dev/backend/.env'}); //dev
-}
-else
+if (env.USER === 'ec2-user' && env.PORT == '4242'){
+	dotenv.config({path:'/home/ec2-user/git/42cabi/backend/.env'}); //dep
+} else if (env.USER === 'ec2-user'){
+	dotenv.config({path:'/home/ec2-user/git/42cabi-dev/backend/.env'}); //dev
+} else{
 	dotenv.config(); //local
-
+}
 
 const Sentry = require("@sentry/node");
 // or use es6 import statements
--- backend/bin/www.ts
+++ backend/bin/www.ts
@@ -5,17 +5,17 @@ import debug from 'debug'
 debug("backend:server")
 import http from 'http'
 import { app } from '../app'
-const env = process.env;
-if (process.env.USER === 'ec2-user') {
-    if (env.PORT == '4242')
-        dotenv.config({ path: '/home/ec2-user/git/42cabi/backend/.env' }); //dep
-    else
-        dotenv.config({ path: '/home/ec2-user/git/42cabi-dev/backend/.env' }); //dev
-}
-else
-    dotenv.config(); //local
 
-var port = normalizePort(process.env.PORT || "4242");
+const env = process.env;
+if (env.USER === 'ec2-user' && env.PORT == '4242'){
+	dotenv.config({ path: '/home/ec2-user/git/42cabi/backend/.env' }); //dep
+} else if (env.USER === 'ec2-user') {
+	dotenv.config({ path: '/home/ec2-user/git/42cabi-dev/backend/.env' }); //dev
+} else {
+	dotenv.config(); //local
+}
+
+var port = normalizePort(env.PORT || "4242");
 app.set("port", port);
 
 var server = http.createServer(app);
