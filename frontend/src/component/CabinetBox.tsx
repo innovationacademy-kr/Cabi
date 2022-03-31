cadcddfbcad2fcdcbc74cc938059946f97a04c80
kyoshong <hyospark@student.42seoul.kr>
2022-03-31 22:55:30 +0900

    merge

--- frontend/src/component/CabinetBox.tsx
+++ frontend/src/component/CabinetBox.tsx
@@ -5,6 +5,8 @@ import './cabinetBox.css'
 export default function CabinetBox(props:any){
   const history = useHistory();
   let isExpired:boolean = false;
+  console.log("props.expire_time" + props.expire_time + "\n");
+  console.log("props.isLent" + props.isLent  + "\n");
 
   if (props.isLent && props.expire_time){
     isExpired = new Date(props.expire_time) < new Date();
@@ -14,9 +16,11 @@ export default function CabinetBox(props:any){
     props.setTarget(props.cabinet_id);
     props.setCabiNum(props.cabinet_num);
   }
+
   const clickMyCabinet = () => {
     history.push('/return');
   }
+
   let clickEvent = clickHandler;
   let toggleName = props.intra_id !== "" ? "" : "modal";
   let targetName = props.isLent === -1 ? "#lentmodal" : "#contentsmodal";
@@ -30,10 +34,13 @@ export default function CabinetBox(props:any){
 
   if (isExpired){
     vanilaClassName += " expiredLentCabinet";
+    console.log("expiredLentCabinet\n");
   }else if (props.intra_id === props.user){
     vanilaClassName += " myLentCabinet";
+    console.log("myLentCabinet\n");
   }else if (props.intra_id !== ""){
     vanilaClassName += " lentCabinet";
+    console.log("lentCabinet\n");
   }
 
   return (
