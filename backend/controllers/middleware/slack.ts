8cfd49a560b58ab588f98997820d5c28ace462da
kyoshong <hyospark@student.42seoul.kr>
2022-03-31 16:20:20 +0900

    :bug: fix: slack

--- backend/controllers/middleware/slack.ts
+++ backend/controllers/middleware/slack.ts
@@ -5,14 +5,11 @@ import dotenv from 'dotenv'
 import { slackUserList, slackUser } from '../../models/user';
 
 const env = process.env;
-if (process.env.USER === 'ec2-user') {
-    if (env.PORT == '4242')
-        dotenv.config({ path: '/home/ec2-user/git/42cabi/backend/.env' }); //dep
-    else
-        dotenv.config({ path: '/home/ec2-user/git/42cabi-dev/backend/.env' }); //dev
+if (env.USER === 'ec2-user'){
+	dotenv.config({path: env.PWD + '/.env'}); //dep
+} else {
+	dotenv.config(); //local
 }
-else
-    dotenv.config(); //local
 const slackBot = new WebClient(env.SLACK_TOKEN);
 
 //반납일 전날 슬랙 메세지 발송
