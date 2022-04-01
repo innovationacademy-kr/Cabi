fcd7089f8774696e9d25a5a1084fb169f465e256
윤현지 <hyoon@student.42seoul.kr>
2022-04-01 13:19:15 +0900

    :test_tube: test map icon #58

--- frontend/src/component/Carousel.tsx
+++ frontend/src/component/Carousel.tsx
@@ -21,7 +21,8 @@ export default function Carousel(props:any){
   }
   const mapButton = () => {
     return (
-      <span data-bs-target={`#carousel_${props.l_idx}_${props.floor_name}`} data-bs-slide-to={0} aria-current="true" aria-label="Slide 0"> 🗺</span>
+      // <span data-bs-target={`#carousel_${props.l_idx}_${props.floor_name}`} data-bs-slide-to={0} aria-current="true" aria-label="Slide 0"> 🗺</span>
+      <span data-bs-target={`#carousel_${props.l_idx}_${props.floor_name}`} data-bs-slide-to={0} aria-current="true" aria-label="Slide 0"> <i className="bi bi-map-fill"></i></span>
     )
   }
   const mapIndicator = () => {
