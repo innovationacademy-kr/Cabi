5e1513aa6debf7ee276f66cb00883fe6ec2571ed
hyunja27 <123gk@naver.com>
2022-06-07 01:31:21 +0900

    :construction: 06/07_ReOpen_ServerWorking #105

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -9,10 +9,9 @@ export default function Main() {
         <img src="../img/logo.png" alt="logo" />
       </div>
       <div className="row d-grid gap-2 col-6 mx-auto">
-        서버 점검 중입니다...
-				{/*<a className="btn btn-lg" id="loginBtn" href={url}>
+				<a className="btn btn-lg" id="loginBtn" href={url}>
           L O G I N
-        </a>*/}
+        </a>
       </div>
     </div>
   );
