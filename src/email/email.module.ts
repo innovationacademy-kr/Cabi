4b8262bbce14d88caae40d3905664bb2984f51df
sichoi42 <42.4.sichoi@gmail.com>
2022-08-24 19:45:00 +0900

    :sparkles: feat: add email scheduler #147
    feat: add email scheduler #147

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -6,33 +6,32 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
     imports: [
-        MailerModule.forRoot({
-            transport: {
-                service: process.env.MAIL_SERVICE,
-                host: process.env.MAIL_HOST,
-                port: 465,
-                auth: {
-                    user: process.env.MAIL_USER,
-                    pass: process.env.MAIL_PASS
-                },
-                secure: true,
-                defaults: {
-                    from: `42CABI ${process.env.MAIL_FROM}`,
-                },
-                template: {
-                    dir: path.join(__dirname, '../templates/'),
-                    adapter: new EjsAdapter(),
-                    options: {
-                      strict: true,
+        MailerModule.forRootAsync({
+            useFactory: () => ({
+                transport: {
+                    // service: process.env.MAIL_SERVICE,
+                    host: process.env.MAIL_HOST,
+                    port: 465,
+                    auth: {
+                        user: process.env.MAIL_USER,
+                        pass: process.env.MAIL_PASS
                     },
-                },
-                // nestjs에서도 사용하는 옵션인가요..?
-                tls: {
-                    maxVersion: process.env.MAIL_TLS_MAXVERSION,
-                    minVersion: process.env.MAIL_TLS_MINVERSION,
-                    ciphers: process.env.MAIL_TLS_CIPHERS,
+                    secure: true,
+                    template: {
+                        dir: path.join(__dirname, './templates/'),
+                        adapter: new EjsAdapter(),
+                        options: {
+                          strict: false,
+                        },
+                    },
+                    // nestjs에서도 사용하는 옵션인가요..?
+                    // tls: {
+                    //     maxVersion: process.env.MAIL_TLS_MAXVERSION,
+                    //     minVersion: process.env.MAIL_TLS_MINVERSION,
+                    //     ciphers: process.env.MAIL_TLS_CIPHERS,
+                    // }
                 }
-            }
+            })
         }),
     ],
 
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,16 +1,24 @@
 import { MailerService } from "@nestjs-modules/mailer";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { Cron, CronExpression } from "@nestjs/schedule";
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService: MailerService) {}
+    private logger = new Logger('EmailService');
+    constructor(
+        private readonly mailerService: MailerService,
+    ) {}
 
-    public sendMail(email: string, subject: string, file: string): void {
+    public sendMail(
+        intra_id: string,
+        subject: string,
+        file: string): void {
         this.mailerService.sendMail({
-            from: `42CABI ${process.env.MAIL_FROM}`,
-            to: email,
+            from: `"42CABI" <${process.env.MAIL_FROM}>`,
+            to: intra_id + '@student.42seoul.kr',
             subject: subject,
             template: `./${file}`,
+            context: { intra_id }
         })
         .then((success) => {
             console.log(success);
@@ -18,5 +26,33 @@ export class MailService {
         .catch((err) => {
             console.log(err);
         });
+    }
+
+    public mailing(info: overUserInfo[], num: number) {
+        let subject = '42CABI 사물함 연체 알림';
+        let file = 'overdue.ejs';
+        if (num === 0) {
+            file = 'soonoverdue.ejs';
+        } else if (num === 7) {
+            file = 'overdue.ejs';
+        } else if (num === 14) {
+            file = 'lastoverdue.ejs';
+        } else if (num === 15) {
+            subject = '42CABI 영구사용정지 안내';
+            file = 'ban.ejs';
+        }
+        info.forEach(user => this.sendMail(user.intra_id, subject, file));
+    }
+
+    @Cron(CronExpression.EVERY_DAY_AT_9PM)
+    public scheduling() {
+        const dayList = [0, 7, 14];
+        dayList.forEach(day => {
+            getOverUser(day).then(res => {
+                if (res) {
+                    this.mailing(res, day);
+                }
+            }).catch(e => console.error(e));
+        })
     }
 }
