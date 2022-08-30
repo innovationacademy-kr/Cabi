09c8a314627d50612bddd0ba0256e8ea223cf188
eunbi9n <gombi9_9@naver.com>
2022-08-30 14:48:31 +0900

    :sparkles: add cabinet-lent-info. dto & add cabinet service #156

--- src/cabinet/repository/rawquery.cabinet.repository.ts
+++ src/cabinet/repository/rawquery.cabinet.repository.ts
@@ -6,6 +6,7 @@ import * as mariadb from 'mariadb';
 import { Inject } from '@nestjs/common';
 import { ICabinetRepository } from './cabinet.repository';
 import { UserSessionDto } from 'src/auth/dto/user.session.dto';
+import { lentCabinetInfoDto } from '../dto/cabinet-lent-info.dto';
 
 export class RawqueryCabinetRepository implements ICabinetRepository {
   private pool;
@@ -101,6 +102,95 @@ export class RawqueryCabinetRepository implements ICabinetRepository {
     return lentInfo;
   }
 
+  async checkCabinetStatus(cabinet_id: number):Promise<number> {
+    try {
+      const content = `SELECT activation FROM cabinet WHERE cabinet_id=${cabinet_id}`;
+      const pool = await this.pool.getConnection();
+      const cabinet = await pool.query(content);
+  
+      if (pool) pool.end();
+      if (cabinet[0].activation === 1) {
+        return 1;
+      }
+    } catch (err :any) {
+      const error = new Error(err.message);
+      error.name = 'CheckCabinetStatusError';
+      console.error(error);
+      throw error;
+    }
+    return 0;
+  }
+
+  async getUser(user: UserSessionDto): Promise<lentCabinetInfoDto> {
+    let pool: mariadb.PoolConnection;
+    let lentCabinet: lentCabinetInfoDto;
+    const content: string = `SELECT * FROM lent l JOIN cabinet c ON l.lent_cabinet_id=c.cabinet_id WHERE l.lent_user_id='${user.user_id}'`;
+
+    pool = await this.pool.getConnection();
+    lentCabinet = await pool
+      .query(content)
+      .then((res: any) => {
+        if (res.length !== 0) {
+          // lent page
+          return {
+            lent_id: res[0].lent_id,
+            lent_cabinet_id: res[0].lent_cabinet_id,
+            lent_user_id: res[0].lent_user_id,
+            lent_time: res[0].lent_time,
+            expire_time: res[0].expire_time,
+            extension: res[0].extension,
+            cabinet_num: res[0].cabinet_num,
+            location: res[0].location,
+            floor: res[0].floor,
+            section: res[0].section,
+            activation: res[0].activation,
+          };
+        } else {
+          return {
+            lent_id: -1,
+            lent_cabinet_id: -1,
+            lent_user_id: -1,
+            lent_time: "",
+            expire_time: "",
+            extension: false,
+            cabinet_num: -1,
+            location: "",
+            floor: -1,
+            section: "",
+            activation: false,
+          };
+        }
+      })
+      .catch((err: any) => {
+        console.error(err);
+        throw err;
+      });
+    if (pool) pool.end();
+    return lentCabinet;
+  }
+
+  async createLent(cabinet_id: number, user: UserSessionDto):Promise<{ errno : number }> {
+    let errResult = 0;
+    let pool: mariadb.PoolConnection;
+    const content: string = `INSERT INTO lent (lent_cabinet_id, lent_user_id, lent_time, expire_time, extension) VALUES (${cabinet_id}, ${user.user_id}, now(), ADDDATE(now(), 30), 0)`;
+    pool = await this.pool.getConnection();
+    await pool
+      .query(content)
+      .then((res: any) => {
+        if (res) {
+          let date = new Date();
+          date.setDate(date.getDate() + 7);
+          let day = date.toISOString().replace(/T.+/, "");
+          //sendLentMsg(user.intra_id, day); // 슬랙 메시지 발송
+        }
+      })
+      .catch((err: any) => {
+        if (err.errno === 1062) errResult = -1;
+      });
+    if (pool) pool.end();
+    return { errno: errResult };
+  }
+  
   //lent_log 값 생성 후 lent 값 삭제
   async createLentLog(user_id: number, intra_id: string): Promise<void> {
     let pool: mariadb.PoolConnection;
