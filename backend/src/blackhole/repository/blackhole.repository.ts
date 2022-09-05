c33038084692e3072d507d3dda218a7da47ca574
sichoi42 <42.4.sichoi@gmail.com>
2022-09-06 00:41:16 +0900

    :memo: add document in new function #176
    add document in new function #176

--- backend/src/blackhole/repository/blackhole.repository.ts
+++ backend/src/blackhole/repository/blackhole.repository.ts
@@ -1,3 +1,9 @@
 export abstract class IBlackholeRepository {
+  /**
+   * 블랙홀에 빠진 유저를 DB에서 삭제한다.
+   *
+   * @Param intra_id: string
+   * @return void
+   */
   abstract deleteBlackholedUser(intra_id: string): Promise<void>;
 }
