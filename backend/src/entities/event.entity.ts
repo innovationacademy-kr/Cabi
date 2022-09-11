7f301a4f2fcde40d611dc4d8332a649e57bed2d0
eunbi9n <gombi9_9@naver.com>
2022-09-11 18:47:21 +0900

    Add: cabinet entity & ban-user entity #203

--- backend/src/entities/event.entity.ts
+++ backend/src/entities/event.entity.ts
@@ -1,9 +1,28 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export default class Event {
-    @PrimaryColumn({
-        type: 'int',
-    })
-    Key: number;
+    // @PrimaryColumn({
+    //     type: 'int',
+    //     comment: '이벤트 ID',
+    // })
+    // Key: number;
+
+    // @Column({
+    //     type: 'varchar',
+    //     length: 30,
+    //     comment: '이벤트 이름',
+    // })
+    // Field: string;
+
+    // @Column({
+    //     type: 'varchar',
+    //     length: 30,
+    // })
+    // user_id: string;
+
+    // @Column({
+    //     type: 'boolean',
+    // })
+    // isEmpty: boolean;
 }
