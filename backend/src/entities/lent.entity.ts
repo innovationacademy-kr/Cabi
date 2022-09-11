9f26c0e5db91252d827b597712152efff2fccd31
eunbi9n <gombi9_9@naver.com>
2022-09-11 16:57:31 +0900

    Add: entities lent, lent_log & lent-type enum

--- backend/src/entities/lent.entity.ts
+++ backend/src/entities/lent.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import Cabinet from "./cabinet.entity";
 import User from "./user.entity";
 
 @Entity()
@@ -6,7 +7,31 @@ export default class Lent {
     @PrimaryGeneratedColumn()
     lent_id: number;
 
+    @Column({
+        type: 'datetime',
+    })
+    lent_time: Date;
+
+    @Column({
+        type: 'datetime',
+    })
+    expire_time: Date;
+
+    /**
+     * 기존 ERD에는 존재하나 삭제하고 수정할 부분입니다.
+     * TODO: extension 삭제
+     * TODO: lent_user_id와 lent_cabinet_id FK 삭제
+     */
+    @Column({
+        default: false,
+    })
+    extension: boolean;
+
     @OneToOne(() => User)
     @JoinColumn()
     lent_user_id: User;
+
+    @OneToOne(() => Cabinet)
+    @JoinColumn()
+    lent_cabinet_id: Cabinet;
 }
---
+++ backend/src/enums/lent-type.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * 사물함 type을 정의
+ */
+enum Lent_type {
+    PRIVATE = 'PRIVATE',
+	SHARE = 'SHARE',
+	CIRCLE = 'CIRCLE'
+}
+export default Lent_type;
