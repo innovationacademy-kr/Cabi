f01fca98b6f08a28619981313700d236b2779757
--replace-all <gyuwonlee.olivia@gmail.com>
2022-09-13 19:45:54 +0900

    add: CheckButton created

--- frontend_v3/src/main.tsx
+++ frontend_v3/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import muiCustomPaletteTheme from "./themes/muiCustomPaletteTheme.tsx";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={muiCustomPaletteTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ frontend_v3/src/themes/muiCustomPaletteTheme.tsx.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const muiCustomPaletteTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#5657a5",
+      // light: main값을 통해 계산됨
+      // dark: main값을 통해 계산됨
+      // contrastText: main값을 통해 계산됨
+    },
+  },
+});
+
+export default muiCustomPaletteTheme;
