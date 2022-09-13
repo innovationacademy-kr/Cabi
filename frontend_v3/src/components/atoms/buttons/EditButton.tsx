20aac3b449bb715931aea5270765c118434c8356
--replace-all <gyuwonlee.olivia@gmail.com>
2022-09-13 16:28:45 +0900

    add: TODO remarks added

--- frontend_v3/src/components/atoms/buttons/EditButton.tsx
+++ frontend_v3/src/components/atoms/buttons/EditButton.tsx
@@ -37,6 +37,7 @@ const EditButton = (props: EditButtonProps): JSX.Element => {
     setIsToggle(false);
     // TODO: gyuwlee
     // value를 이용해 서버에 저장하는 로직 추가
+    // 버튼을 눌렀을 때 서버에 저장하는 로직이 실행되고 나서, value를 새로 저장된 값으로 업데이트하도록 다시 API를 호출해야 할 것 같습니다.
   };
 
   return isToggle === false ? (
--- frontend_v3/src/components/atoms/inputs/TestTextField.tsx
+++ frontend_v3/src/components/atoms/inputs/TestTextField.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import EditButton from "./EditButton";
+import EditButton from "../buttons/EditButton";
 
 // TODO: gyuwlee(?)
 // 본 테스트 필드 제거하고, Organism 단위로 텍스트 입력 필드 구현
@@ -13,6 +13,8 @@ const Container = styled.div`
 `;
 
 const TestTextField = (): JSX.Element => {
+  // TODO: gyuwlee(?)
+  // value 의 초기 값을 빈 문자열이 아닌 '서버에서 받아온 값'으로 설정해야 합니다.
   const [value, setValue] = useState("");
   const [isToggle, setIsToggle] = useState(false);
 
