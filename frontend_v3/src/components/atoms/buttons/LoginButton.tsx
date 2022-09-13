79f0e9312348dee2c2307a99dcbbbe20963ea080
aseungbo <seungbo1112@gmail.com>
2022-09-13 15:06:40 +0900

    Modified LoginButton

--- frontend_v3/src/components/atoms/buttons/LoginButton.tsx
+++ frontend_v3/src/components/atoms/buttons/LoginButton.tsx
@@ -1,16 +1,15 @@
 import style from "@emotion/styled";
 
+const Button = style.a`
+  color: white;
+  font-weight: bolder;
+  border: white 0.2rem solid;
+  border-radius: 1rem;
+  padding: 0.8rem 5rem;
+`;
+
 const LoginButton = (): JSX.Element => {
   const url = "/auth/login";
-  const Button = style.a`
-    color: white;
-    font-weight: bolder;
-    border: white 0.2rem solid;
-    border-radius: 1rem;
-    padding: 0.8rem;
-    padding-left: 5rem;
-    padding-right: 5rem;
-  `;
   return <Button href={url}>L O G I N</Button>;
 };
 
