7634e979907e5b8cfdb60dac9a96ffc397a5ea5c
eunbi9n <gombi9_9@naver.com>
2022-09-15 00:30:42 +0900

    Add: field name for LentLog Entity #203

--- backend/src/entities/lent.log.entity.ts
+++ backend/src/entities/lent.log.entity.ts
@@ -1,32 +1,39 @@
 import Lent_type from 'src/enums/lent.type.enum';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('lent_log')
 export default class LentLog {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({
+    name: 'log_id',
+  })
   log_id: number;
 
   @Column({
+    name: 'log_user_id',
     type: 'int',
   })
   log_user_id: number;
 
   @Column({
+    name: 'log_cabinet_id',
     type: 'int',
   })
   log_cabinet_id: number;
 
   @Column({
+    name: 'lent_time',
     type: 'datetime',
   })
   lent_time: Date;
 
   @Column({
+    name: 'return_time',
     type: 'datetime',
   })
   return_time: Date;
 
   @Column({
+    name: 'lent_type',
     type: 'enum',
   })
   lent_type: Lent_type;
