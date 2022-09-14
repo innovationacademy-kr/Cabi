e519c4e2b25f18f7d126c864e91f761ede4f0db7
eunbi9n <gombi9_9@naver.com>
2022-09-15 00:26:36 +0900

    Add: field name for Cabinet Entity #203

--- backend/src/entities/cabinet.entity.ts
+++ backend/src/entities/cabinet.entity.ts
@@ -1,37 +1,41 @@
 import Lent_type from 'src/enums/lent.type.enum';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('cabinet')
 export default class Cabinet {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({
+    name: 'cabinet_id',
+  })
   cabinet_id: number;
 
   @Column({
+    name: 'cabinet_num',
     type: 'int',
   })
   cabinet_num: number;
 
-  /**
-   * TODO: location이나 section같은 경우는 enum으로 관리하는 것이 좋을 것 같습니다.
-   */
   @Column({
+    name: 'location',
     type: 'varchar',
     length: 30,
   })
   location: string;
 
   @Column({
+    name: 'floor',
     type: 'int',
   })
   floor: number;
 
   @Column({
+    name: 'section',
     type: 'varchar',
     length: 30,
   })
   section: string;
 
   @Column({
+    name: 'activation',
     type: 'tinyint',
     default: true,
   })
@@ -41,29 +45,34 @@ export default class Cabinet {
    * 새로 추가될 필드입니다.
    */
   // @Column({
+  //   name: 'lent_type',
   //     type: 'enum',
   // })
   // lent_type: Lent_type;
 
   // @Column({
+  //   name: 'max_user',
   //     type: 'tinyint',
   //     default: 3,
   // })
   // max_user: number;
 
   // @Column({
+  //   name: 'min_user',
   //     type: 'tinyint',
   //     default: 1,
   // })
   // min_user: number;
 
   // @Column({
+  //   name: 'memo',
   //     type: 'varchar',
   //     length: 50,
   // })
-  // secret_memo: string;
+  // memo: string;
 
   // @Column({
+  //   name: 'title',
   //     type: 'varchar',
   //     length: 128,
   // })
