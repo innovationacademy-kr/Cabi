3fe7f24a21511a016b8950bfe7d64050491bf5ed
eunbi9n <gombi9_9@naver.com>
2022-09-15 00:21:55 +0900

    Add: field name for BanUser Entity

--- backend/src/entities/ban.user.entity.ts
+++ backend/src/entities/ban.user.entity.ts
@@ -1,17 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('user')
 export default class BanUser {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({
+    name: 'ban_id',
+  })
   ban_id: number;
 
   @Column({
+    name: 'ban_user_id',
     nullable: false,
     type: 'int',
   })
   ban_user_id: number;
 
   @Column({
+    name: 'intra_id',
     nullable: false,
     type: 'varchar',
     length: 30,
@@ -19,16 +23,19 @@ export default class BanUser {
   intra_id: string;
 
   @Column({
+    name: 'cabinet_id',
     type: 'int',
   })
   cabinet_id: number;
 
   @Column({
+    name: 'banned_date',
     type: 'datetime',
   })
   banned_date: Date;
 
   @Column({
+    name: 'unbanned_date',
     type: 'datetime',
   })
   unbanned_date: Date;
