78f6e88282614b88438c9947521a643bade40b96
eunbi9n <gombi9_9@naver.com>
2022-09-17 03:13:30 +0900

    Add: class-validator cabinet & lent Dto

--- backend/src/dto/cabinet.dto.ts
+++ backend/src/dto/cabinet.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
 import { LentType } from 'src/enum/lent.type.enum';
 
 /**
@@ -9,12 +10,14 @@ export class CabinetDto {
     description: '캐비넷 고유 ID',
     example: 1234,
   })
+  @IsNumber()
   cabinet_id: number; // 캐비넷 고유 ID
 
   @ApiProperty({
     description: '사물함에 붙어있는 숫자',
     example: 12,
   })
+  @IsNumber()
   cabinet_num: number; // 사물함에 붙어있는 숫자
 
   @ApiProperty({
@@ -22,17 +25,20 @@ export class CabinetDto {
     enum: ['PRIVATE', 'SHARE', 'CIRCLE'],
     example: 'PRIVATE',
   })
+  @IsEnum(LentType)
   lent_type: LentType; // 사물함의 종류 (개인, 공유, 동아리)
 
   @ApiProperty({
     description: '사물함에 대한 설명',
     example: '푸주와 아이들이 사용하는 사물함입니다',
   })
+  @IsString()
   cabinet_title: string; // 사물함에 대한 설명
 
   @ApiProperty({
     description: '해당 사물함을 대여할 수 있는 최대 유저 수',
     example: 3,
   })
-  max_user: number; // 해당 사물함을 대여할 수 있는 최대 유저 수
+  @IsNumber()
+  max_user: number = 3; // 해당 사물함을 대여할 수 있는 최대 유저 수
 }
--- backend/src/dto/lent.dto.ts
+++ backend/src/dto/lent.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsBoolean, IsDate, IsNumber, ValidateNested } from 'class-validator';
 import { UserDto } from './user.dto';
 
 /**
@@ -9,29 +10,34 @@ export class LentDto {
     description: '대여 고유 ID',
     example: 1234,
   })
+  @IsNumber()
   lent_id: number; // 대여 고유 ID
 
   @ApiProperty({
     description: '해당 사물함을 사용하고 있는 유저들',
     type: [UserDto],
   })
+  @ValidateNested({each:true})
   users: UserDto[]; // 해당 사물함을 사용하고 있는 유저들
 
   @ApiProperty({
     description: '대여한 시간',
     example: '2022-08-24 13:03:03',
   })
+  @IsDate()
   lent_time: Date; // 대여한 시간
 
   @ApiProperty({
     description: '대여한 시간',
     example: '2022-08-24 13:03:03',
   })
+  @IsDate()
   expire_time: Date; // 만료 시간
 
   @ApiProperty({
     description: '연체 되었는지 아닌지의 여부',
     example: false,
   })
+  @IsBoolean()
   is_expired: boolean; // 연체 되었는지 아닌지의 여부
 }
