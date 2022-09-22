03f4aa6edfbb210e76784f83b656b94cdae8b7b2
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-09-22 21:50:21 +0900

    fe_v3: add drag event on web

--- frontend_v3/src/components/organisms/Carousel.tsx
+++ frontend_v3/src/components/organisms/Carousel.tsx
@@ -35,6 +35,10 @@ interface CarouselProps {
 
 const Carousel = (props: CarouselProps): JSX.Element => {
   const { slideCount } = props;
+  const [mouseDownClientX, setMouseDownClientX] = useState(0);
+  const [mouseDownClientY, setMouseDownClientY] = useState(0);
+  const [mouseUpClientX, setMouseUpClientX] = useState(0);
+  const [mouseUpClientY, setMouseUpClientY] = useState(0);
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideRef = useRef<HTMLInputElement>(null);
   // TODO: hybae
@@ -49,6 +53,32 @@ const Carousel = (props: CarouselProps): JSX.Element => {
       }%)`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션을 만듭니다.
     }
   }, [currentSlide]);
+
+  useEffect(() => {
+    const dragSpaceX = Math.abs(mouseDownClientX - mouseUpClientX);
+    const dragSpaceY = Math.abs(mouseDownClientY - mouseUpClientY);
+    const vector = dragSpaceX / dragSpaceY;
+
+    if (dragSpaceX > 100 && vector > 2) {
+      if (mouseUpClientX < mouseDownClientX) {
+        if (currentSlide === lastSlide) setCurrentSlide(0);
+        else setCurrentSlide(currentSlide + 1);
+      } else if (mouseUpClientX > mouseDownClientX) {
+        if (currentSlide === 0) setCurrentSlide(lastSlide);
+        else setCurrentSlide(currentSlide - 1);
+      }
+    }
+  }, [mouseUpClientX]);
+
+  const onMouseDown = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    setMouseDownClientX(e.clientX);
+    setMouseDownClientY(e.clientY);
+  };
+
+  const onMouseUp = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    setMouseUpClientX(e.clientX);
+    setMouseUpClientY(e.clientY);
+  };
 
   const renderSectionButtons = (): JSX.Element[] => {
     const sectionButtons: JSX.Element[] = [];
@@ -75,7 +105,7 @@ const Carousel = (props: CarouselProps): JSX.Element => {
           currentSlide={currentSlide}
           setCurrentSlide={setCurrentSlide}
         />
-        <CarouselArea>
+        <CarouselArea onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
           <SlideContainer slideRef={slideRef} slideCount={slideCount} />
         </CarouselArea>
         <SlideButton
