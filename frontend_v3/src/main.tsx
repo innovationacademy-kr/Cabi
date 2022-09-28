bebe00e8ef9361de13c2dc5eae1c1cf5a0add7c6
sichoi42 <42.4.sichoi@gmail.com>
2022-09-28 21:50:59 +0900

    :sparkles: add Google Analytics code
    add Google Analytics code

--- frontend_v3/src/main.tsx
+++ frontend_v3/src/main.tsx
@@ -8,7 +8,23 @@ import store from "./redux/store";
 import muiCustomPaletteTheme from "./themes/muiCustomPaletteTheme.tsx";
 import App from "./App";
 import "./index.css";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+import { useEffect } from "react";
 
+const config = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+useEffect(() => {
+  if (config)
+  {
+    const history = createBrowserHistory();
+    history.listen((location: any) => {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+      console.log("history called");
+    });
+    ReactGA.initialize(config);
+  }
+}, []);
 const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
