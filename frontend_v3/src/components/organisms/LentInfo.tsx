8ff67273c9b6696d677cdb523100f22f10eb0245
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-09-28 15:04:49 +0900

    fix: use cabinet_id instead of cabinet_num

--- frontend_v3/src/components/organisms/LentInfo.tsx
+++ frontend_v3/src/components/organisms/LentInfo.tsx
@@ -18,7 +18,7 @@ interface LentInfoProps {
   section?: string | undefined; // 사물함의 섹션 종류 (오아시스 등)
   cabinet_memo: string | undefined; // 사물함 비밀번호와 관련된 메모
   cabinet_id: number | undefined; // 캐비넷 고유 ID
-  cabinet_num?: number | undefined; // 사물함에 붙어있는 숫자
+  cabinet_num?: number; // 사물함에 붙어있는 숫자
   lent_type?: string | undefined; // 사물함의 종류 (개인, 공유, 동아리)
   cabinet_title: string | undefined; // 공유/동아리 사물함인 경우 사물함에 대한 설명
   max_user?: number | undefined; // 해당 사물함을 대여할 수 있는 최대 유저 수
@@ -44,7 +44,7 @@ const LentInfo = (prop: LentInfoProps): JSX.Element => {
     return (
       <>
         <p>
-          {location} {floor}F {cabinet_id}
+          {location} {floor}F {cabinet_num}
         </p>
         <p>{/* lent_info.expire_time */}</p>
       </>
