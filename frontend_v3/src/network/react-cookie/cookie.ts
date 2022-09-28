387bf315abbee0016fe2deb97288ad52229363d7
--replace-all <gyuwonlee.olivia@gmail.com>
2022-09-28 16:40:48 +0900

    add: cookie check logic added

---
+++ frontend_v3/src/network/react-cookie/cookie.ts
@@ -0,0 +1,11 @@
+import { Cookies } from "react-cookie";
+
+const cookies = new Cookies();
+
+export const setCookie = (name: string, value: string, option?: any): void => {
+  return cookies.set(name, value, { ...option });
+};
+
+export const getCookie = (name: string): string => {
+  return cookies.get(name);
+};
