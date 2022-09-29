21b5b45ea5521db66c989d47dbb4a814626620f3
--replace-all <gyuwonlee.olivia@gmail.com>
2022-09-29 23:50:44 +0900

    fix: condition for login after lent and logout added

--- frontend_v3/src/pages/Lent.tsx
+++ frontend_v3/src/pages/Lent.tsx
@@ -3,14 +3,28 @@ import { useEffect } from "react";
 import LentTemplate from "../components/templates/LentTemplate";
 import FooterTemplate from "../components/templates/FooterTemplate";
 import ContentTemplate from "../components/templates/ContentTemplate";
-import { useAppSelector } from "../redux/hooks";
+import { getCookie } from "../network/react-cookie/cookie";
+import { useAppSelector, useAppDispatch } from "../redux/hooks";
+import { userAll } from "../redux/slices/userSlice";
+import { axiosMyInfo } from "../network/axios/axios.custom";
 
 const Lent = (): JSX.Element => {
+  const token = getCookie("accessToken");
   const user = useAppSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (user.intra_id === "default" || user.cabinet_id === -1) navigate("/");
+    if (token && user.intra_id === "default") {
+      axiosMyInfo()
+        .then((response) => {
+          dispatch(userAll(response.data));
+        })
+        .catch((error) => {
+          navigate("/");
+        });
+    } else if (user.intra_id === "default" || user.cabinet_id === -1)
+      navigate("/");
   }, []);
 
   return (
