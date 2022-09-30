a89e5ef9573408e163f2dfc39b352b59cae182d4
aseungbo <seungbo1112@gmail.com>
2022-09-30 15:32:57 +0900

    fix: ga dev Test

--- frontend_v3/src/network/ga/gaTracker.ts
+++ frontend_v3/src/network/ga/gaTracker.ts
@@ -7,27 +7,25 @@ const gaTracker = (): void => {
   const [initialize, setInitialize] = useState<boolean>(false);
   useEffect(() => {
     // 배포 전용
-    if (!window.location.href.includes("localhost")) {
-      ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
-      setInitialize(true);
-    }
-    if (initialize) {
-      const history = createBrowserHistory();
-      history.listen((res) => {
-        ReactGA.set({ page: res.location.pathname });
-        ReactGA.pageview(res.location.pathname);
-      });
-    }
+    // if (!window.location.href.includes("localhost")) {
+    //   ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
+    //   setInitialize(true);
+    // }
+    // if (initialize) {
+    //   const history = createBrowserHistory();
+    //   history.listen((res) => {
+    //     ReactGA.set({ page: res.location.pathname });
+    //     ReactGA.pageview(res.location.pathname);
+    //   });
+    // }
 
     // 개발용
-    // local에서 테스트 시 사용
-    // ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
-    // const history = createBrowserHistory();
-    // history.listen((res) => {
-    //   console.log(res.location);
-    //   ReactGA.set({ page: res.location.pathname });
-    //   ReactGA.pageview(res.location.pathname);
-    // });
+    ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
+    const history = createBrowserHistory();
+    history.listen((res) => {
+      ReactGA.set({ page: res.location.pathname });
+      ReactGA.pageview(res.location.pathname);
+    });
   }, []);
 };
 export default gaTracker;
