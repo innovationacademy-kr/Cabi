4a5fc4d28b3b51fafd12792a6f28b65af69dbc1d
sichoi42 <42.4.sichoi@gmail.com>
2022-10-07 20:33:52 +0900

    :sparkles: add helmet module for prevent CORS #374
    add helmet module for prevent CORS #374

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,7 @@ async function bootstrap() {
   if (is_local === true) {
     app.enableCors();
   }
+  app.use(helmet());
   await app.listen(port);
 }
 bootstrap();
