d1ee3f21e7dbd4fb54754b2c44631ac70de4d853
--replace-all <gyuwonlee.olivia@gmail.com>
2022-10-10 15:38:21 +0900

    cabinetColor palette edited for font readability

--- frontend_v3/src/themes/cabinetColor.ts
+++ frontend_v3/src/themes/cabinetColor.ts
@@ -1,7 +1,15 @@
+// export const emptyCabinet = "#dee2e6";
+// export const myCabinet = "#7566ab";
+// export const lentedCabinet = "rgba(128, 117, 140)";
+// export const expiredCabinet = "#b90e7a";
+// export const brokenCabinet = "#823439";
+// export const bannedCabinet = "#333333";
+// export const defaultColor = "white";
+
 export const emptyCabinet = "#dee2e6";
-export const myCabinet = "#7566ab";
-export const lentedCabinet = "rgba(128, 117, 140)";
-export const expiredCabinet = "#b90e7a";
-export const brokenCabinet = "#823439";
-export const bannedCabinet = "#333333";
+export const myCabinet = "#9f96e0";
+export const lentedCabinet = "#8883a4";
+export const expiredCabinet = "#fa7cac";
+export const brokenCabinet = "#8799a5";
+export const bannedCabinet = "#8799a5";
 export const defaultColor = "white";
