5bb231f2a9e13d81bcba0ddbd50e86264642025d
--replace-all <gyuwonlee.olivia@gmail.com>
2022-10-12 16:10:09 +0900

    placeholder for default title(memo) removed, cancel button added

--- frontend_v3/src/components/atoms/inputs/LentTextField.tsx
+++ frontend_v3/src/components/atoms/inputs/LentTextField.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import EditButton from "../buttons/EditButton";
 
+type divProps = {
+  isToggle: boolean;
+};
+
 const Container = styled.div`
   display: flex;
   width: 70%;
@@ -16,16 +20,27 @@ const Container = styled.div`
   box-shadow: inset 5px 5px 9px #ededed, inset -5px -5px 9px #ffffff;
 `;
 
-const TextDiv = styled.div`
+const TextDiv = styled.div<divProps>`
   box-sizing: border-box;
   height: 80%;
-  width: 90%;
+  width: ${(props) => (props.isToggle ? "72%" : "90%")};
   display: flex;
   justify-content: center;
+  align-items: center;
   overflow: auto;
   &::-webkit-scrollbar {
     display: none;
   }
+`;
+
+const ButtonDiv = styled.div<divProps>`
+  box-sizing: border-box;
+  height: 80%;
+  margin-left: 0.25rem;
+  width: ${(props) => (props.isToggle ? "28%" : "10%")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 interface LentTextFieldProps {
@@ -41,10 +56,11 @@ const LentTextField = (props: LentTextFieldProps): JSX.Element | null => {
       ? "방 제목을 입력해주세요"
       : "필요한 내용을 메모해주세요"
   );
-  const [inputValue, setInputValue] = useState(textValue);
+  const [inputValue, setInputValue] = useState(currentContent ? textValue : "");
   const [isToggle, setIsToggle] = useState(false);
 
   useEffect(() => {
+    console.log(currentContent);
     if (currentContent) {
       setTextValue(currentContent);
     } else {
@@ -54,10 +70,12 @@ const LentTextField = (props: LentTextFieldProps): JSX.Element | null => {
           : "필요한 내용을 메모해주세요"
       );
     }
-  }, [currentContent]);
+  }, []);
 
   useEffect(() => {
-    setInputValue(textValue);
+    if (textValue === "필요한 내용을 메모해주세요") {
+      setInputValue("");
+    } else setInputValue(textValue);
   }, [textValue]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -71,22 +89,29 @@ const LentTextField = (props: LentTextFieldProps): JSX.Element | null => {
     <>
       <p>{contentType === "title" ? "방 제목" : "비밀스러운 메모장"}</p>
       <Container className="Container" style={{ marginBottom: "2rem" }}>
-        <TextDiv className="textDiv">
+        <TextDiv className="textDiv" isToggle={isToggle}>
           {isToggle === false ? (
             <p style={{ margin: 0 }}>{textValue}</p> // 대체
           ) : (
-            <input type="text" value={inputValue} onChange={handleChange} />
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleChange}
+              style={{ width: "100%" }}
+            />
           )}
         </TextDiv>
-        <EditButton
-          isToggle={isToggle}
-          setIsToggle={setIsToggle}
-          contentType={contentType}
-          inputValue={inputValue}
-          textValue={textValue}
-          setTextValue={setTextValue}
-          setInputValue={setInputValue}
-        />
+        <ButtonDiv className="buttonDiv" isToggle={isToggle}>
+          <EditButton
+            isToggle={isToggle}
+            setIsToggle={setIsToggle}
+            contentType={contentType}
+            inputValue={inputValue}
+            textValue={textValue}
+            setTextValue={setTextValue}
+            setInputValue={setInputValue}
+          />
+        </ButtonDiv>
       </Container>
     </>
   );
