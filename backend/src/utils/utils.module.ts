b6f7f1ae6dd6d8bc03c403be7a63ce9c2e7c0b7d
eunbi9n <gombi9_9@naver.com>
2022-10-12 21:36:58 +0900

    fix: utils module #438

--- backend/src/utils/utils.module.ts
+++ backend/src/utils/utils.module.ts
@@ -2,10 +2,12 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { BanModule } from 'src/ban/ban.module';
 import MailerConfigService from 'src/config/mailer.config';
 import { LentModule } from 'src/lent/lent.module';
 import { CabinetModule } from '../cabinet/cabinet.module';
 import { EmailSender } from './email.sender.component';
+import { ExpiredChecker } from './expired.checker.component';
 
 @Module({
   imports: [
@@ -16,9 +18,10 @@ import { EmailSender } from './email.sender.component';
     }),
     CabinetModule,
     LentModule,
+    BanModule,
   ],
 
-  providers: [EmailSender],
+  providers: [EmailSender, ExpiredChecker],
   exports: [EmailSender],
 })
 export class UtilsModule {}
