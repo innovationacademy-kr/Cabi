5748aa76fc57a9b84283375a6fd92f770288c774
sichoi42 <42.4.sichoi@gmail.com>
2022-10-18 14:04:43 +0900

    :pencil2: fix type from any to boolean in userSessionDto.staff #443
    fix type from any to boolean in userSessionDto.staff #443

--- backend/src/dto/user.session.dto.ts
+++ backend/src/dto/user.session.dto.ts
@@ -8,5 +8,5 @@ export class UserSessionDto extends UserDto {
   iat?: number; // JWT 발급 시간
   ext?: number; // JWT 만료 시간
   blackholed_at?: Date; // 블랙홀 날짜
-  staff?: any; // staff 여부
+  staff?: boolean; // staff 여부
 }
