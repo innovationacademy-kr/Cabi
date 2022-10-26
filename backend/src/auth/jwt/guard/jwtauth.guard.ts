60b8da61f2a042bdf2ab753a08b0f40481e0c23c
sichoi42 <42.4.sichoi@gmail.com>
2022-10-27 01:57:21 +0900

    :sparkles: add custom error message when throw unauthorizedException #519
    add custom error message when throw unauthorizedException #519

--- backend/src/auth/jwt/guard/jwtauth.guard.ts
+++ backend/src/auth/jwt/guard/jwtauth.guard.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { UserSessionDto } from 'src/dto/user.session.dto';
 
 /**
- * passport-jwt의 기본 인증을 사용합니다. 현재는 커스텀이 불필요하므로 커스텀하지 않습니다.
+ * passport-jwt의 기본 인증을 사용합니다.
+ * #519 이슈에 따라 401 에러의 커스텀 메시지를 추가합니다.
  */
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+  handleRequest<TUser = any>(err: any, user: any): TUser {
+    if (err || !user) {
+      throw err || new UnauthorizedException('🚨 로그인 정보가 만료되었습니다. 🥲 🚨\n다시 로그인해주세요.');
+    }
+    return user;
+  }
+}
