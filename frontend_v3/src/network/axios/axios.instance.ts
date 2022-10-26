e82e90d0b00b488da3f18e8a2f72c0b469d6c6ee
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-10-26 18:58:51 +0900

    feat: add alert message for 401

--- frontend_v3/src/network/axios/axios.instance.ts
+++ frontend_v3/src/network/axios/axios.instance.ts
@@ -13,6 +13,7 @@ instance.interceptors.response.use(
     // access_token unauthorized
     if (error.response?.status === 401) {
       removeCookie("access_token");
+      alert(`🚨 로그인 정보가 만료되었습니다. 🚨\n다시 로그인해주세요`);
       window.location.href = "/";
     }
     return Promise.reject(error);
