3a822cfd01cb46146d59e43c925935499741a666
inshin <inshin@student.42seoul.kr>
2022-12-19 19:39:48 +0900

    [FE] FIX: LoginPage.css 삭제->styled component로 변경

--- frontend_v4/src/pages/LoginPage.tsx
+++ frontend_v4/src/pages/LoginPage.tsx
@@ -1,37 +1,129 @@
-import "./LoginPage.css";
+import styled from "styled-components";
 
 const LoginPage = () => {
   return (
-    <div id="loginPage" className="wrapper">
-      <section className="leftLoginPage">
-        <div className="topContents">
-          <p>42서울 캐비닛 서비스</p>
-          <p className="lightPuple">여러분의 일상을 가볍게</p>
-        </div>
-        <div className="loginImg">
+    <LoginPageStyled id="loginPage">
+      <LeftSectionStyled className="leftLoginPage">
+        <TopContentsStyled>
+          <LoginTitleStyled color="var(--black)">
+            42서울 캐비닛 서비스
+          </LoginTitleStyled>
+          <LoginTitleStyled color="var(--lightpurple-color)">
+            여러분의 일상을 가볍게
+          </LoginTitleStyled>
+        </TopContentsStyled>
+        <LoginImgStyled>
           <img src="src/assets/images/loginImg.svg" alt="" />
-        </div>
-        <div className="bottomContents">
+        </LoginImgStyled>
+        <BottomContentsStyled>
           <p>
-            <span>Cabi</span>로
+            <CabiStyled>Cabi</CabiStyled>로
           </p>
           <p>일상의 무게를 덜어보세요.</p>
-        </div>
-      </section>
-      <div className="rightLoginPage">
-        <div className="loginCard modal">
-          <div className="loginLogo">
+        </BottomContentsStyled>
+      </LeftSectionStyled>
+      <RightSectionStyled className="rightLoginPage">
+        <LoginCardStyled className="modal">
+          <CardLogoStyled>
             <img src="src/assets/images/logo.svg" alt="" />
-          </div>
-          <div className="loginTitle">
-            <h1>42Cabi</h1>
-            <p>여러분의 일상을 가볍게</p>
-          </div>
+          </CardLogoStyled>
+          <CardTitleBoxStyled>
+            <CardTitleStyled>42Cabi</CardTitleStyled>
+            <CardSubTitleStyled>여러분의 일상을 가볍게</CardSubTitleStyled>
+          </CardTitleBoxStyled>
           <button>L O G I N</button>
-        </div>
-      </div>
-    </div>
+        </LoginCardStyled>
+      </RightSectionStyled>
+    </LoginPageStyled>
   );
 };
+
+const LoginPageStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 100%;
+  height: 100%;
+`;
+
+const LeftSectionStyled = styled.section`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  padding-left: 7%;
+`;
+const TopContentsStyled = styled.div`
+  font-size: 2rem;
+  font-family: var(--main-font);
+  font-weight: 700;
+  line-height: 3rem;
+`;
+
+const LoginTitleStyled = styled.p<{ color: string }>`
+  font-size: 2rem;
+  font-family: var(--main-font);
+  color: ${(props) => props.color};
+  font-weight: 700;
+  line-height: 3rem;
+`;
+
+const LoginImgStyled = styled.div`
+  margin-top: 10%;
+  margin-bottom: 10%;
+`;
+
+const BottomContentsStyled = styled.div`
+  font-size: 1.75rem;
+  font-family: var(--main-font);
+  font-weight: 700;
+  line-height: 3rem;
+`;
+
+const CabiStyled = styled.span`
+  color: var(--main-color);
+`;
+
+const RightSectionStyled = styled.section`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--main-color);
+`;
+
+const LoginCardStyled = styled.div`
+  width: 350px;
+  height: 500px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  padding: 85px 0;
+  background-color: var(--white);
+`;
+
+const CardLogoStyled = styled.div`
+  width: 70px;
+  height: 70px;
+`;
+
+const CardTitleBoxStyled = styled.div`
+  text-align: center;
+  margin-top: -40px;
+`;
+
+const CardTitleStyled = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+`;
+
+const CardSubTitleStyled = styled.p`
+  color: var(--main-color);
+`;
 
 export default LoginPage;
