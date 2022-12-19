656864b3f9f649742291f51adf0c12a6b46defb9
Oh Yoon Hwan <yooh@Ohs-MacBook-Pro.local>
2022-12-19 14:47:52 +0900

    [FEAT] Create Button Component#599

---
+++ frontend_v4/src/containers/ModalContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalContainer = () => {
+    return <ModalConatinerStyled />
+} 
+
+const ModalConatinerStyled = styled.div`
+    width : 360px;
+    height : 500px;
+    display : flex;
+`
