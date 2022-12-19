143f8d6adefa63b36b4304c0ecf4088072eb5fe3
inshin <inshin@student.42seoul.kr>
2022-12-19 17:15:34 +0900

    [FE] FEAT: 로그인페이지 추가 #604

--- frontend_v4/src/pages/LoginPage.tsx
+++ frontend_v4/src/pages/LoginPage.tsx
@@ -1,7 +1,35 @@
+import "./LoginPage.css";
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div id="loginPage" className="wrapper">
+      <section className="leftLoginPage">
+        <div className="topContents">
+          <p>42서울 캐비닛 서비스</p>
+          <p className="lightPuple">여러분의 일상을 가볍게</p>
+        </div>
+        <div className="loginImg">
+          <img src="src/assets/images/loginImg.svg" alt="" />
+        </div>
+        <div className="bottomContents">
+          <p>
+            <span>Cabi</span>로
+          </p>
+          <p>일상의 무게를 덜어보세요.</p>
+        </div>
+      </section>
+      <div className="rightLoginPage">
+        <div className="loginCard modal">
+          <div className="loginLogo">
+            <img src="src/assets/images/logo.svg" alt="" />
+          </div>
+          <div className="loginTitle">
+            <h1>42Cabi</h1>
+            <p>여러분의 일상을 가볍게</p>
+          </div>
+          <button>L O G I N</button>
+        </div>
+      </div>
     </div>
   );
 };
