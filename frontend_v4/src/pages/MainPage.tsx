dcee878896340a22bd5711c008525e77c3ac1a65
inshin <inshin@student.42seoul.kr>
2022-12-21 23:24:15 +0900

    [FE] FEAT: 왼쪽 네비게이션 추가 #611

--- frontend_v4/src/pages/MainPage.tsx
+++ frontend_v4/src/pages/MainPage.tsx
@@ -1,11 +1,37 @@
+import styled from "styled-components";
 import TopNavContainer from "@/containers/TopNavContainer";
+import LeftNavContainer from "@/containers/LeftNavContainer";
 
 const MainPage = () => {
   return (
-    <div>
+    <>
       <TopNavContainer />
-    </div>
+      <WrapperStyled>
+        <LeftNavContainer />
+        <MainStyled>메인 컨텐츠 위치</MainStyled>
+        <DetailInfoContainerStyled>
+          사물함 상세 정보 위치
+        </DetailInfoContainerStyled>
+      </WrapperStyled>
+    </>
   );
 };
+
+const WrapperStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+`;
+
+const MainStyled = styled.main`
+  width: 100%;
+  height: 100%;
+  background: lightgray;
+`;
+
+const DetailInfoContainerStyled = styled.div`
+  min-width: 330px;
+`;
 
 export default MainPage;
