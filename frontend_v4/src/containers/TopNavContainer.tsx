eb81c29280cd1d51e151ec88d3d9a8835337d718
inshin <inshin@student.42seoul.kr>
2022-12-21 15:16:12 +0900

    [FE] FEAT: topNavi 셀렉트 기능 추가 #611

--- frontend_v4/src/containers/TopNavContainer.tsx
+++ frontend_v4/src/containers/TopNavContainer.tsx
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+const locations = ["새롬관", "서초", "강남"];
+
+interface ILocationListItem {
+  location: string;
+  onUpdate: React.Dispatch<string>;
+  onClose: React.Dispatch<boolean>;
+}
+
+const LocationListItem: React.FC<ILocationListItem> = ({
+  location,
+  onUpdate,
+  onClose,
+}) => {
+  return (
+    <LocationListItemStyled
+      onClick={() => {
+        onUpdate(location);
+        onClose(false);
+      }}
+    >
+      {location}
+    </LocationListItemStyled>
+  );
+};
+
 const TopNavContainer = () => {
+  const [locationName, setLocationName] = useState("새롬관");
+  const [locationClicked, setLocationClicked] = useState(false);
+
   return (
     <TopNavContainerStyled>
       <LogoStyled>
         <div>
           <img src="src/assets/images/logo.svg" alt="" />
         </div>
-        <LocationSelectBoxStyled>새롬관</LocationSelectBoxStyled>
+        <LocationSelectBoxStyled>
+          <span onClick={() => setLocationClicked(!locationClicked)}>
+            {locationName}
+          </span>
+          {locationClicked && (
+            <LocationListStyled>
+              {locations.map((location, index) => (
+                <LocationListItem
+                  location={location}
+                  key={index}
+                  onUpdate={setLocationName}
+                  onClose={setLocationClicked}
+                />
+              ))}
+            </LocationListStyled>
+          )}
+        </LocationSelectBoxStyled>
       </LogoStyled>
       <MapButtonStyled>
         <img src="src/assets/images/map.svg" alt="" />
@@ -35,14 +79,53 @@ const LogoStyled = styled.div`
   cursor: pointer;
 `;
 
-const LocationSelectBoxStyled = styled.div`
+const LocationSelectBoxStyled = styled.span`
   position: relative;
   margin-left: 40px;
   font-size: 24px;
   font-family: var(--location-font);
-  background: url(src/assets/images/select.svg) no-repeat 100% 50%;
-  width: 80px;
   cursor: pointer;
+  & > span {
+    user-select: none !important;
+    -webkit-user-select: none !important;
+    -moz-user-select: none !important;
+    -ms-user-select: none !important;
+  }
+  & > span:hover {
+    opacity: 0.9;
+  }
+  & > span::after {
+    content: "";
+    position: absolute;
+    right: -20px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 20px;
+    background: url(src/assets/images/select.svg) no-repeat 100%;
+  }
+`;
+
+const LocationListStyled = styled.ul`
+  position: absolute;
+  top: 30px;
+  left: -15px;
+  padding: 5px 10px;
+  background: var(--white);
+  opacity: 0.8;
+  border-radius: 4px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+`;
+
+const LocationListItemStyled = styled.li`
+  width: 80px;
+  height: 40px;
+  color: var(--main-color);
+  line-height: 40px;
+  text-indent: 5px;
+  &:hover {
+    opacity: 0.9;
+  }
 `;
 
 const MapButtonStyled = styled.div`
