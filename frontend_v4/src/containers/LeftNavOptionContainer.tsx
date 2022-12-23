8fbe75049705f54722ed6bc21f9ecc27fed95011
inshin <inshin@student.42seoul.kr>
2022-12-23 21:56:44 +0900

    [FE] FEAT: 좌측섹션네비 추가 #611

---
+++ frontend_v4/src/containers/LeftNavOptionContainer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const floorSection = [
+  "End of Cluster1",
+  "Cluster 1 - OA",
+  "Cluster 1 - Terrace",
+  "Oasis",
+  "End of Cluster 2",
+];
+
+const LeftNavOptionContainer = () => {
+  return (
+    <LeftNavOptionStyled>
+      {floorSection.map((section: string, index: number) => (
+        <FloorSectionStyled key={index}>{section}</FloorSectionStyled>
+      ))}
+    </LeftNavOptionStyled>
+  );
+};
+
+const LeftNavOptionStyled = styled.div`
+  min-width: 240px;
+  height: 100%;
+  padding: 32px 10px;
+  border-right: 1px solid var(--line-color);
+  font-weight: 300;
+`;
+
+const FloorSectionStyled = styled.div`
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 10px;
+  text-indent: 20px;
+  color: var(--gray-color);
+  margin: 2px 0;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--main-color);
+    color: var(--white);
+  }
+`;
+
+export default LeftNavOptionContainer;
