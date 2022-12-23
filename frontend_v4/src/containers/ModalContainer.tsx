2743dd591bd7f7fb3b33e528b16f733e695affb3
SeyoungCho <wnl383@naver.com>
2022-12-23 16:08:58 +0900

    [FE] REFACTOR: prettier 적용 #598

--- frontend_v4/src/containers/ModalContainer.tsx
+++ frontend_v4/src/containers/ModalContainer.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const ModalContainer = () => {
-    return <ModalConatinerStyled />
-} 
+  return <ModalConatinerStyled />;
+};
 
 const ModalConatinerStyled = styled.div`
-    width : 360px;
-    height : 500px;
-    display : flex;
-`
+  width: 360px;
+  height: 500px;
+  display: flex;
+`;
