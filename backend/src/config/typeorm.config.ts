6f211b1fef6d8bd0bd26b3c0492575a4c7ebce0c
eunbi9n <gombi9_9@naver.com>
2022-12-26 20:36:36 +0900

    [BE] FEAT: V4 개발 환경을 위해 새로운 테이블 추가 #659

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -9,13 +9,19 @@ export default class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
     const log = this.configService.get<boolean>('debug.log');
 
+    //TODO: v4 개발을 위한 코드입니다. database가 완전히 바뀐 후에는 수정이 필요합니다.
+    const local = this.configService.get<boolean>('is_local');
+    let db = this.configService.get<string>('database.database');
+    if (local) 
+      db = this.configService.get<string>('database.database_v4');
+    
     return {
       type: 'mysql',
       host: this.configService.get<string>('database.host'),
       username: this.configService.get<string>('database.user'),
       port: this.configService.get<number>('database.port'),
       password: this.configService.get<string>('database.password'),
-      database: this.configService.get<string>('database.database'),
+      database: db,
       entities: [`${__dirname}/../**/entities/*.entity.{js,ts}`],
       synchronize: false,
       logging: log,
