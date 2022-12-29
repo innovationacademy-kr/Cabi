2f1a6b0413c7b93ff3871096118f2f51066c48e7
eunbi9n <gombi9_9@naver.com>
2022-12-29 15:12:21 +0900

    [BE] FIX: 오류 수정 #673

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -10,9 +10,9 @@ export default class TypeOrmConfigService implements TypeOrmOptionsFactory {
     const log = this.configService.get<boolean>('debug.log');
 
     //TODO: v4 개발을 위한 코드입니다. database가 완전히 바뀐 후에는 수정이 필요합니다.
-    const local = this.configService.get<boolean>('is_local');
+    const version = this.configService.get<string>('database_v4');
     let db = this.configService.get<string>('database.database');
-    if (local) db = this.configService.get<string>('database.database_v4');
+    if (version !== undefined) db = version;
 
     return {
       type: 'mysql',
