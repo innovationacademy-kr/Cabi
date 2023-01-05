29f396d041b8eb52d07d2ec289e8ac86c8b53db0
SeyoungCho <wnl383@naver.com>
2023-01-05 21:13:23 +0900

    [FE] FEAT: 캐비넷 선택시 호버효과 고정 적용 #722

--- frontend_v4/src/containers/CabinetListItemContainer.tsx
+++ frontend_v4/src/containers/CabinetListItemContainer.tsx
@@ -1,4 +1,4 @@
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import {
   currentCabinetIdState,
   targetCabinetInfoState,
@@ -19,7 +19,9 @@ import { UserDto } from "@/types/dto/user.dto";
 
 const CabinetListItemContainer = (props: CabinetInfo): JSX.Element => {
   const MY_INFO = useRecoilValue<UserDto>(userState);
-  const setCurrentCabinetId = useSetRecoilState<number>(currentCabinetIdState);
+  const [currentCabinetId, setCurrentCabinetId] = useRecoilState<number>(
+    currentCabinetIdState
+  );
   const setTargetCabinetInfo = useSetRecoilState<CabinetInfo>(
     targetCabinetInfoState
   );
@@ -132,6 +134,7 @@ const CabinetListItemContainer = (props: CabinetInfo): JSX.Element => {
     <CabinetListItemStyled
       status={props.status}
       isMine={isMine}
+      isSelected={currentCabinetId === props.cabinet_id}
       onClick={() => {
         selectCabinetOnClick(props.status, props.cabinet_id);
       }}
@@ -201,6 +204,7 @@ const cabinetLabelColorMap = {
 const CabinetListItemStyled = styled.div<{
   status: CabinetStatus;
   isMine: boolean;
+  isSelected: boolean;
 }>`
   position: relative;
   background-color: ${(props) => cabinetStatusColorMap[props.status]};
@@ -219,6 +223,11 @@ const CabinetListItemStyled = styled.div<{
   padding: 8px 8px 14px;
   transition: transform 0.2s, opacity 0.2s;
   cursor: pointer;
+  ${({ isSelected }) =>
+    isSelected &&
+    css`
+      transform: scale(1.05);
+    `}
   &:hover {
     opacity: 0.9;
     transform: scale(1.05);
