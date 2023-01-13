610fcece989d2ea2ab1fbc4f86ce72b7ded87573
inshin <inshin@student.42seoul.kr>
2023-01-14 02:34:07 +0900

    [FE] FIX: mapInfo폴더의 컴포넌트 파일 분리 #805

--- frontend/src/components/MapInfo/MapFloorSelect/MapFloorSelect.tsx
+++ frontend/src/components/MapInfo/MapFloorSelect/MapFloorSelect.tsx
@@ -1,25 +1,11 @@
 import styled from "styled-components";
+import MapFloorSelectOption from "@/components/MapInfo/MapFloorSelectOption/MapFloorSelectOption";
 
 interface IMapFloorSelect {
   floor: number;
   setFloor: React.Dispatch<React.SetStateAction<number>>;
   floorInfo: number[];
 }
-
-const OptionsContainer: React.FC<{
-  selectFloor: Function;
-  floorInfo: number[];
-}> = ({ floorInfo, selectFloor }) => {
-  return (
-    <OptionsContainerStyled id="mapFloorOptionBox">
-      {floorInfo.map((info, idx) => (
-        <OptionStyled onClick={() => selectFloor(info)} key={idx}>
-          {info}층
-        </OptionStyled>
-      ))}
-    </OptionsContainerStyled>
-  );
-};
 
 const MapFloorSelect = ({ floor, setFloor, floorInfo }: IMapFloorSelect) => {
   const onClickFloorOption = () => {
@@ -34,27 +20,15 @@ const MapFloorSelect = ({ floor, setFloor, floorInfo }: IMapFloorSelect) => {
 
   return (
     <div style={{ position: "relative" }}>
-      <CurrentFloorStyled onClick={onClickFloorOption}>
+      <MapFloorSelectStyled onClick={onClickFloorOption}>
         {`${floor}층`}
-      </CurrentFloorStyled>
-      <OptionsContainer selectFloor={selectFloor} floorInfo={floorInfo} />
+      </MapFloorSelectStyled>
+      <MapFloorSelectOption selectFloor={selectFloor} floorInfo={floorInfo} />
     </div>
   );
 };
 
-const OptionStyled = styled.div`
-  width: 65px;
-  height: 40px;
-  border-bottom: 1px solid #e6e6e6;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: var(--white);
-  color: black;
-  cursor: pointer;
-`;
-
-const CurrentFloorStyled = styled.div`
+const MapFloorSelectStyled = styled.div`
   background: url("src/assets/images/select.svg") var(--main-color) no-repeat
     80% 55%;
   color: white;
@@ -67,21 +41,6 @@ const CurrentFloorStyled = styled.div`
   margin: 30px 0;
   &:hover {
     opacity: 0.9;
-  }
-`;
-
-const OptionsContainerStyled = styled.div`
-  position: absolute;
-  left: 0;
-  top: 75px;
-  background: white;
-  border-radius: 10px;
-  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-  overflow: hidden;
-  z-index: 99;
-  display: none;
-  &.on {
-    display: block;
   }
 `;
 
--- frontend/src/components/MapInfo/MapGrid/MapGrid.tsx
+++ frontend/src/components/MapInfo/MapGrid/MapGrid.tsx
@@ -1,22 +1,17 @@
+import styled from "styled-components";
+import { useSetRecoilState } from "recoil";
 import {
-  currentFloorNumberState,
   currentSectionNameState,
   isCurrentSectionRenderState,
 } from "@/recoil/atoms";
-import { currentLocationFloorState } from "@/recoil/selectors";
-import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import styled from "styled-components";
-import useDetailInfo from "@/hooks/useDetailInfo";
-import { ISectionInfo, mapPostionData } from "@/assets/data/mapPositionData";
+import { mapPostionData } from "@/assets/data/mapPositionData";
+import MapItem from "@/components/MapInfo/MapItem/MapItem";
 
 const MapGrid = ({ floor }: { floor: number }) => {
   const setSection = useSetRecoilState(currentSectionNameState);
   const setIsCurrentSectionRender = useSetRecoilState(
     isCurrentSectionRenderState
   );
-
   const selectSection = (section: string) => {
     setIsCurrentSectionRender(true);
     setSection(section);
@@ -35,52 +30,6 @@ const MapGrid = ({ floor }: { floor: number }) => {
     </MapGridStyled>
   );
 };
-
-const MapItem: React.FC<{
-  floor: number;
-  info: ISectionInfo;
-  selectSection: Function;
-}> = ({ floor, info, selectSection }) => {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const setCurrentFloor = useSetRecoilState(currentFloorNumberState);
-  const floors = useRecoilValue<Array<number>>(currentLocationFloorState);
-  const { closeMap } = useDetailInfo();
-  const onClick = () => {
-    if (pathname === "/home") navigate("/main");
-    setCurrentFloor(floor ? floor : floors[0]);
-    selectSection(info.name);
-    closeMap();
-  };
-  return (
-    <ItemStyled onClick={onClick} info={info}>
-      {info.name}
-    </ItemStyled>
-  );
-};
-
-const ItemStyled = styled.div<{
-  onClick: React.MouseEventHandler;
-  info: ISectionInfo;
-}>`
-  font-size: 0.8rem;
-  cursor: pointer;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-  letter-spacing: -0.05rem;
-  grid-column-start: ${({ info }) => info.colStart};
-  grid-column-end: ${({ info }) => info.colEnd};
-  grid-row-start: ${({ info }) => info.rowStart};
-  grid-row-end: ${({ info }) => info.rowEnd};
-  background: ${({ info }) =>
-    info.type === "cabinet" ? "#9747ff" : "#bcb9b9"};
-  &:hover {
-    opacity: ${({ info }) => (info.type === "cabinet" ? 0.9 : 1)};
-  }
-`;
 
 const MapGridStyled = styled.div`
   width: 100%;
