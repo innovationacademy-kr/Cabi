351e5d122b2fc5616c447bbf89fa9727976c1f36
SeyoungCho <wnl383@naver.com>
2023-01-13 17:35:52 +0900

    [FE] FEAT: 테스트용 대여페널티 리셋 버튼 추가 #792

--- frontend/src/assets/data/maps.ts
+++ frontend/src/assets/data/maps.ts
@@ -19,6 +19,7 @@ export const cabinetLabelColorMap = {
   [CabinetStatus.EXPIRED]: "var(--white)",
   [CabinetStatus.BROKEN]: "var(--white)",
   [CabinetStatus.BANNED]: "var(--white)",
+  MINE: "var(--black)",
 };
 
 export const cabinetStatusColorMap = {
@@ -28,6 +29,7 @@ export const cabinetStatusColorMap = {
   [CabinetStatus.EXPIRED]: "var(--expired)",
   [CabinetStatus.BROKEN]: "var(--broken)",
   [CabinetStatus.BANNED]: "var(--banned)",
+  MINE: "var(--mine)",
 };
 
 export const modalPropsMap = {
