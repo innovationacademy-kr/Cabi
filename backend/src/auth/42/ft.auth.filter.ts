bdedd5272e9e716b596e28089578ca67d27471e1
Ssuamje <side2850@naver.com>
2023-01-16 20:45:24 +0900

    [BE] FEAT: 서비스 불가 지역에 따른 리디렉션 및 로거 추가 #793

---
+++ backend/src/auth/42/ft.auth.filter.ts
@@ -0,0 +1,19 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(UnauthorizedException)
+export class FtAuthFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    response.status(status).redirect('/login/failure');
+  }
+}
