a58fd9c60b8a61e7d4845c34f215b4f5412c7aa2
inshin <inshin@student.42seoul.kr>
2023-01-18 05:03:44 +0900

    [FE] BUG: 뒤로가기, 앞으로가기 시, 캐비닛정보를 못받아오는 이슈 #814

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,20 +1,15 @@
 import { useEffect } from "react";
 import ServiceManual from "@/components/Home/ServiceManual";
 import { useNavigate } from "react-router-dom";
-import {
-  currentFloorNumberState,
-  currentSectionNameState,
-} from "@/recoil/atoms";
+import { currentFloorNumberState } from "@/recoil/atoms";
 import { currentLocationFloorState } from "@/recoil/selectors";
-import { useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import "@/assets/css/homePage.css";
 import useMenu from "@/hooks/useMenu";
 
 const HomePage = () => {
   const floors = useRecoilValue<Array<number>>(currentLocationFloorState);
   const setCurrentFloor = useSetRecoilState<number>(currentFloorNumberState);
-  const resetCurrentFloor = useResetRecoilState(currentFloorNumberState);
-  const resetCurrentSection = useResetRecoilState(currentSectionNameState);
   const { closeLeftNav, closeAll } = useMenu();
   const navigator = useNavigate();
 
