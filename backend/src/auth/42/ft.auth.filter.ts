c411e1318fcf8a620abda3837f23f293205b5fc8
Ssuamje <side2850@naver.com>
2023-01-19 22:38:08 +0900

    [BE] FIX: 로그인 실패 리디렉션 수정/#738

--- backend/src/auth/42/ft.auth.filter.ts
+++ backend/src/auth/42/ft.auth.filter.ts
@@ -3,17 +3,24 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  Inject,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
 
 @Catch(UnauthorizedException)
 export class FtAuthFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+    constructor(
+        @Inject(ConfigService)
+        private configService: ConfigService,
+    ){}
+    catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
 
-    response.status(status).redirect('/login/failure');
+    response.status(status)
+    .redirect(`${this.configService.get<string>('FE_HOST')}/login/failure`);
   }
 }
