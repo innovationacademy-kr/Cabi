8060b4e2a3a2100caaa85d48d45a4496ba8ea621
Ssuamje <side2850@naver.com>
2023-01-24 14:56:27 +0900

    [BE] FIX: Lint 체크 #870

--- backend/src/admin/develop/develop.service.ts
+++ backend/src/admin/develop/develop.service.ts
@@ -1,20 +1,20 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { IAdminDevelopRepository } from "src/admin/develop/repository/develop.repository.interface";
+import { Inject, Injectable } from '@nestjs/common';
+import { IAdminDevelopRepository } from 'src/admin/develop/repository/develop.repository.interface';
 
 @Injectable()
 export class AdminDevelopService {
-    constructor(
-        @Inject('IAdminDevelopRepository')
-        private adminDevelopRepository: IAdminDevelopRepository,
-    ) {}
-    
+  constructor(
+    @Inject('IAdminDevelopRepository')
+    private adminDevelopRepository: IAdminDevelopRepository,
+  ) {}
+
   /**
    * 해당 유저의 role을 1(Admin)로 설정합니다.
    *
    * @param email 해당 유저의 google email.
    * @returns boolean
    */
-    async setUserToAdminByEmail(email: string): Promise<boolean> {
-        return await this.adminDevelopRepository.setUserToAdminByEmail(email);
-    }
+  async setUserToAdminByEmail(email: string): Promise<boolean> {
+    return await this.adminDevelopRepository.setUserToAdminByEmail(email);
+  }
 }
--- backend/src/admin/develop/repository/develop.repository.ts
+++ backend/src/admin/develop/repository/develop.repository.ts
@@ -1,35 +1,34 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { IAdminDevelopRepository } from "src/admin/develop/repository/develop.repository.interface";
-import { AdminUserDto } from "src/admin/dto/admin.user.dto";
-import AdminUserRole from "src/admin/enums/admin.user.role.enum";
-import AdminUser from "src/entities/admin.user.entity";
-import { Repository } from "typeorm";
+import { InjectRepository } from '@nestjs/typeorm';
+import { IAdminDevelopRepository } from 'src/admin/develop/repository/develop.repository.interface';
+import AdminUserRole from 'src/admin/enums/admin.user.role.enum';
+import AdminUser from 'src/entities/admin.user.entity';
+import { Repository } from 'typeorm';
 
 export class AdminDevelopRepository implements IAdminDevelopRepository {
-    constructor(
-        @InjectRepository(AdminUser)
-        private adminDevelopRepository: Repository<AdminUser>,
-    ) {}
+  constructor(
+    @InjectRepository(AdminUser)
+    private adminDevelopRepository: Repository<AdminUser>,
+  ) {}
 
-    async setUserToAdminByEmail(email: string): Promise<boolean> {
-        const find = await this.adminDevelopRepository.findOne({
-            where: {
-                email: email,
-            },
-        });
-        if (!find) {
-            return false;
-        }
-        await this.adminDevelopRepository
-        .createQueryBuilder()
-        .update(AdminUser)
-        .set({
-            role: AdminUserRole.ADMIN,   
-        })
-        .where({
-            email: email,
-        })
-        .execute();
-        return true;
+  async setUserToAdminByEmail(email: string): Promise<boolean> {
+    const find = await this.adminDevelopRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (!find) {
+      return false;
     }
+    await this.adminDevelopRepository
+      .createQueryBuilder()
+      .update(AdminUser)
+      .set({
+        role: AdminUserRole.ADMIN,
+      })
+      .where({
+        email: email,
+      })
+      .execute();
+    return true;
+  }
 }
