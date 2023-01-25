44adaa569bf8a022dcfc9e9443c845c141eda603
inshin <inshin@student.42seoul.kr>
2023-01-25 20:58:13 +0900

    [FE] FIX: React.lazy 및 Suspense를 사용한 페이지 분할 #877

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
 import MainPage from "@/pages/MainPage";
-const LogPage = React.lazy(() => import("@/pages/LogPage"));
-const NotFoundPage = React.lazy(() => import("@/pages/NotFoundPage"));
-const LoginFailurePage = React.lazy(() => import("@/pages/LoginFailurePage"));
-const AdminLayout = React.lazy(() => import("@/pages/admin/AdminLayout"));
-const AdminLoginPage = React.lazy(() => import("@/pages/admin/AdminLoginPage"));
-const AdminHomePage = React.lazy(() => import("@/pages/admin/AdminHomePage"));
+import LoadingAnimation from "./components/Common/LoadingAnimation";
+import LogPage from "@/pages/LogPage";
+const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
+const LoginFailurePage = lazy(() => import("@/pages/LoginFailurePage"));
+const AdminLayout = lazy(() => import("@/pages/admin/AdminLayout"));
+const AdminLoginPage = lazy(() => import("@/pages/admin/AdminLoginPage"));
+const AdminHomePage = lazy(() => import("@/pages/admin/AdminHomePage"));
 
 function App(): React.ReactElement {
   return (
@@ -21,15 +22,17 @@ function App(): React.ReactElement {
           <Route path="login" element={<LoginPage />} />
           <Route path="log" element={<LogPage />} />
         </Route>
-        {/* admin용 라우터 */}
-        <Route path="/admin/" element={<AdminLayout />}>
-          <Route path="login" element={<AdminLoginPage />} />
-          <Route path="home" element={<AdminHomePage />} />
-          <Route path="main" element={<MainPage />} />
-        </Route>
-        <Route path="/login/failure" element={<LoginFailurePage />} />
-        <Route path="/admin/login/failure" element={<LoginFailurePage />} />
-        <Route path="/*" element={<NotFoundPage />} />
+        <Suspense fallback={<LoadingAnimation />}>
+          {/* admin용 라우터 */}
+          <Route path="/admin/" element={<AdminLayout />}>
+            <Route path="login" element={<AdminLoginPage />} />
+            <Route path="home" element={<AdminHomePage />} />
+            <Route path="main" element={<MainPage />} />
+          </Route>
+          <Route path="/login/failure" element={<LoginFailurePage />} />
+          <Route path="/admin/login/failure" element={<LoginFailurePage />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Suspense>
       </Routes>
     </BrowserRouter>
   );
