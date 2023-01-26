03c6586844d02537a055c82dad1ae58fd1585f66
inshin <inshin@student.42seoul.kr>
2023-01-27 00:15:45 +0900

    [BE] FIX: AdminMainAuthGuard 적용 #877

--- backend/src/auth/jwt/guard/admin.main.jwtauth.guard.ts
+++ backend/src/auth/jwt/guard/admin.main.jwtauth.guard.ts
@@ -1,17 +1,17 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class AdminMainAuthGuard extends AuthGuard(['admin_jwt', 'jwt']) {
-	handleRequest<TUser = any>(err: any, user: any): TUser {
-		if (err || !user) {
-		  throw (
-			err ||
-			new UnauthorizedException(
-			  '🚨 아무런 토큰이 없습니다. 🥲 🚨\n다시 로그인해주세요.',
-			)
-		  );
-		}
-		return user;
-	  }
+  handleRequest<TUser = any>(err: any, user: any): TUser {
+    if (err || !user) {
+      throw (
+        err ||
+        new UnauthorizedException(
+          '🚨 아무런 토큰이 없습니다. 🥲 🚨\n다시 로그인해주세요.',
+        )
+      );
+    }
+    return user;
+  }
 }
