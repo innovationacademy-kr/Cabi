fb4c807ae1e2fc27b3fef8c9031c9947e3ff59e4
inshin <inshin@student.42seoul.kr>
2023-01-27 20:17:37 +0900

    [FE] log title 에 섹션 위치 추가 #877

--- frontend/src/components/LentLog/LogTable/LogTable.tsx
+++ frontend/src/components/LentLog/LogTable/LogTable.tsx
@@ -30,9 +30,14 @@ const LogTable = ({ lentLog }: IlentLog) => {
         {lentLog !== BAD_REQUEST && (
           <TbodyStyled>
             {lentLog.map(
-              ({ floor, cabinet_num, lent_time, return_time }, idx) => (
+              (
+                { floor, section, cabinet_num, lent_time, return_time },
+                idx
+              ) => (
                 <tr key={idx}>
-                  <td>{`${floor}F - ${cabinet_num}번`}</td>
+                  <td
+                    title={`${floor}층 ${section}`}
+                  >{`${floor}F - ${cabinet_num}번`}</td>
                   <td>
                     {new Date(lent_time).toLocaleString("ko-KR", dateOptions)}
                   </td>
