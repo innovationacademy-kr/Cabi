7ac06f319c72810ae44dae3df8435b453f92933a
sichoi42 <42.4.sichoi@gmail.com>
2023-02-04 16:56:02 +0900

    [BE] ETC: lint 에러 무시 #793

--- backend/test/main/lent.e2e-spec.ts
+++ backend/test/main/lent.e2e-spec.ts
@@ -14,6 +14,7 @@ import { initializeTransactionalContext } from 'typeorm-transactional';
  * @param filePath
  */
 async function loadSQL(queryRunner: QueryRunner, filePath: string) {
+  /* eslint-disable */
   const sql = require('fs').readFileSync(filePath, 'utf8');
   const commands = sql.split(';');
 
