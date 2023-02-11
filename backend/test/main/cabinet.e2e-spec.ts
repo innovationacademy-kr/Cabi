e1476b69529ef22646132a97b56a7dab6ff9f937
Ssuamje <side2850@naver.com>
2023-02-11 21:31:16 +0900

    [BE] FEAT : e2e_test admin, main cabinet 틀 작성 및 OK 확인  #793

--- backend/test/main/cabinet.e2e-spec.ts
+++ backend/test/main/cabinet.e2e-spec.ts
@@ -12,6 +12,7 @@ import { UserSessionDto } from "src/dto/user.session.dto";
 describe('Main Cabinet 모듈 테스트 (e2e)', () => {
 	let app: INestApplication;
 	let jwtService: JwtService;
+	let token: string;
 
 	beforeAll(async () =>{
 		await initTestDB('test_main_cabinet');
@@ -49,6 +50,12 @@ describe('Main Cabinet 모듈 테스트 (e2e)', () => {
 		 app = moduleFixture.createNestApplication();
 		 await app.init();
 
+		 // given : 일반 유저의 토큰
+		 const user: UserSessionDto = {
+			user_id: 6,
+			intra_id: 'lentuser2',
+		  };
+		token = jwtService.sign(user);
 	});
 
 	it('DB 연동 확인', () => {
@@ -63,22 +70,51 @@ describe('Main Cabinet 모듈 테스트 (e2e)', () => {
 		await queryRunner.release();
 	});
 
-	describe('GET : api/cabinet_info/', () => {
-		describe('정상적인 요청 - 현재 서비스하는 건물 / 층 정보를 가져옵니다.', () => {
+	describe('GET : /api/cabinet_info/', () => {
+		describe('정상적인 요청 - 현재 서비스하는 건물의 층별 정보를 가져옵니다.', () => {
 			it('건물 / 층 정보를 조회합니다.', async () => {
 				// given : 일반 유저의 토큰
-				const user: UserSessionDto = {
-					user_id: 6,
-					intra_id: 'lentuser2',
-				  };
-				const token = jwtService.sign(user);
 
 				// when : 조회 요청
 				const response = await request(app.getHttpServer())
-				.get('api/cabinet_info')
+				.get('/api/cabinet_info')
 				.set('Authorization', `Bearer ${token}`);
 				
 				// then : OK
+				expect(response.status).toBe(HttpStatus.OK);
+			});
+		});
+	});
+
+	describe('GET : /api/cabinet_info/:location/:floor', () => {
+		describe('정상적인 요청 - 해당 층에 존재하는 섹션과 사물함의 정보를 가져옵니다.', () => {
+			it('존재하는 층의 정보를 조회합니다.', async () => {
+				// given : 토큰, 건물(encoded), 층
+				const location = encodeURIComponent('새롬관');
+				const floor = 2;
+
+				// when : 조회 요청
+				const response = await request(app.getHttpServer())
+				.get(`/api/cabinet_info/${location}/${floor}`)
+				.set('Authorization', `Bearer ${token}`);
+
+				// then : OK
+				expect(response.status).toBe(HttpStatus.OK);
+			});
+		});
+	});
+
+	describe('GET : /api/cabinet_info/:cabinet_id', () => {
+		describe('정상적인 요청 - cabinetId로 해당 사물함의 정보를 조회합니다.', () => {
+			it ('있는 사물함의 정보를 조회합니다.', async () => {
+				// given : 토큰, cabinetId
+				const cabinetId = 1;
+				
+				// when
+				const response = await request(app.getHttpServer())
+				.get(`/api/cabinet_info/${cabinetId}`)
+				.set('Authorization', `Bearer ${token}`);
+				
 				expect(response.status).toBe(HttpStatus.OK);
 			});
 		});
