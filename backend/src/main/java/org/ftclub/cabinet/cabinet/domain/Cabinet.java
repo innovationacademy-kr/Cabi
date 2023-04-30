64b2675064b9707cf9ea30f820af557bd54f9e8b
Ssuamje <side2850@naver.com>
2023-05-01 00:45:25 +0900

    [BE] TEST: test DB docker 셋업 및 연결 테스트 추가

--- backend/src/main/java/org/ftclub/cabinet/cabinet/domain/Cabinet.java
+++ backend/src/main/java/org/ftclub/cabinet/cabinet/domain/Cabinet.java
@@ -1,19 +1,32 @@
 package org.ftclub.cabinet.cabinet.domain;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.ftclub.cabinet.lent.domain.LentHistory;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.ftclub.cabinet.lent.domain.LentHistory;
 
 @Entity
 @Table(name = "CABINET")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cabinet {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CABINET_ID")
     private Long cabinetId;
 
@@ -34,7 +47,8 @@ public class Cabinet {
     @Column(name = "STATUS_NOTE", length = 64)
     private String statusNote;
 
-    @Embedded Grid grid;
+    @Embedded
+    Grid grid;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "CABINET_PLACE_ID")
@@ -43,7 +57,8 @@ public class Cabinet {
     @OneToMany(mappedBy = "cabinet", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LentHistory> lentHistoryList = new ArrayList<>();
 
-    public Cabinet(Integer visibleNum, CabinetStatus status, LentType lentType, Integer maxUser,  Grid grid, CabinetPlace cabinetPlace) {
+    public Cabinet(Integer visibleNum, CabinetStatus status, LentType lentType, Integer maxUser,
+            Grid grid, CabinetPlace cabinetPlace) {
         this.visibleNum = visibleNum;
         this.status = status;
         this.lentType = lentType;
@@ -52,5 +67,7 @@ public class Cabinet {
         this.cabinetPlace = cabinetPlace;
     }
 
-
+    public CabinetStatus status() {
+        return this.status;
+    }
 }
--- backend/src/test/java/org/ftclub/cabinet/cabinet/service/CabinetServiceTest.java
+++ backend/src/test/java/org/ftclub/cabinet/cabinet/service/CabinetServiceTest.java
@@ -1,31 +1,49 @@
 package org.ftclub.cabinet.cabinet.service;
 
-import org.ftclub.cabinet.cabinet.domain.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.ftclub.cabinet.cabinet.domain.Cabinet;
+import org.ftclub.cabinet.cabinet.domain.CabinetDto;
+import org.ftclub.cabinet.cabinet.domain.CabinetGrid;
+import org.ftclub.cabinet.cabinet.domain.CabinetPlace;
+import org.ftclub.cabinet.cabinet.domain.CabinetRepository;
+import org.ftclub.cabinet.cabinet.domain.CabinetStatus;
+import org.ftclub.cabinet.cabinet.domain.Grid;
+import org.ftclub.cabinet.cabinet.domain.LentType;
+import org.ftclub.cabinet.cabinet.domain.Location;
+import org.ftclub.cabinet.cabinet.domain.MapArea;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class CabinetServiceTest {
+
     @Mock
     private CabinetRepository cabinetRepository;
 
     @InjectMocks
     private CabinetService myService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testQuery() {
+        int count = jdbcTemplate.queryForObject("select count(*) from cabinet", Integer.class);
+        assertThat(count).isEqualTo(28);
+    }
+
     @Test
     public void testFindAll() {
-        Cabinet cabinet = new Cabinet(1, CabinetStatus.AVAILABLE, LentType.PUBLIC, 3, new Grid(), new CabinetPlace(new Location(), new CabinetGrid(), new MapArea()));
+        Cabinet cabinet = new Cabinet(1, CabinetStatus.AVAILABLE, LentType.PUBLIC, 3, new Grid(),
+                new CabinetPlace(new Location(), new CabinetGrid(), new MapArea()));
         // Add expected entities to the list...
 
         when(cabinetRepository.findById(1L)).thenReturn(Optional.of(cabinet));
