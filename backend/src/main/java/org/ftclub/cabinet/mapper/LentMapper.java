7678ca503b6d94eeae858723cd9fec393a2c6e99
Ssuamje <side2850@naver.com>
2023-05-05 17:34:29 +0900

    [BE] FEAT: facade 패턴 임시구현

--- backend/src/main/java/org/ftclub/cabinet/mapper/LentMapper.java
+++ backend/src/main/java/org/ftclub/cabinet/mapper/LentMapper.java
@@ -8,10 +8,8 @@ import org.ftclub.cabinet.lent.domain.LentHistory;
 import org.ftclub.cabinet.user.domain.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
-@Component
 public interface LentMapper {
 
 	LentMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(LentMapper.class);
@@ -19,7 +17,7 @@ public interface LentMapper {
 	LentDto toLentDto(String name, LentHistory lentHistory);
 
 	@Mapping(target = "userId", source = "lentHistory.userId")
-	@Mapping(target = "cabinetId", source = "lentHistory.cabinetId")
+	@Mapping(target = "cabinetId", source = "cabinet.cabinetId")
 	LentHistoryDto toLentHistoryDto(LentHistory lentHistory, User user, Cabinet cabinet,
 			Location location);
 }
