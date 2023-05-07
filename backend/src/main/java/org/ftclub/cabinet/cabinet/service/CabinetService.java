bd3d3e55a1262f6995174250fe9943964f2999a8
eunbi9n <gombi9_9@naver.com>
2023-05-08 03:25:37 +0900

    [BE] FEAT : ban 로직 수정 #1038

--- backend/src/main/java/org/ftclub/cabinet/cabinet/service/CabinetService.java
+++ backend/src/main/java/org/ftclub/cabinet/cabinet/service/CabinetService.java
@@ -1,33 +1,29 @@
 package org.ftclub.cabinet.cabinet.service;
 
-<<<<<<< HEAD
 import org.ftclub.cabinet.cabinet.domain.Cabinet;
-=======
-import java.util.List;
->>>>>>> e1d6a71c ([BE] FEAT : banPolicy 추가 및 기타 userService 메서드 추가 #1038)
 import org.ftclub.cabinet.cabinet.domain.CabinetStatus;
 import org.ftclub.cabinet.cabinet.domain.Grid;
 import org.ftclub.cabinet.cabinet.domain.LentType;
 
 public interface CabinetService {
 
-	Cabinet getCabinet(Long cabinetId);
+    Cabinet getCabinet(Long cabinetId);
 
-	void updateStatus(Long cabinetId, CabinetStatus status);
+    void updateStatus(Long cabinetId, CabinetStatus status);
 
-	void updateStatusByUserCount(Long cabinetId, Integer userCount);
+    void updateStatusByUserCount(Long cabinetId, Integer userCount);
 
-	void updateMemo(Long cabinetId, String memo);
+    void updateMemo(Long cabinetId, String memo);
 
-	void updateVisibleNum(Long cabinetId, Integer visibleNum);
+    void updateVisibleNum(Long cabinetId, Integer visibleNum);
 
-	void updateTitle(Long cabinetId, String title);
-	
-	void updateMaxUser(Long cabinetId, Integer maxUser);
+    void updateTitle(Long cabinetId, String title);
 
-	void updateLentType(Long cabinetId, LentType lentType);
+    void updateMaxUser(Long cabinetId, Integer maxUser);
 
-	void updateGrid(Long cabinetId, Grid grid);
+    void updateLentType(Long cabinetId, LentType lentType);
 
-	void updateStatusNote(Long cabinetId, String statusNote);
+    void updateGrid(Long cabinetId, Grid grid);
+
+    void updateStatusNote(Long cabinetId, String statusNote);
 }
