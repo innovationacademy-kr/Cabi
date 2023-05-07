9120a26acdcb064c6ec6ef83bd9b9babbb68c8f4
donggyu <leedong1848@gmail.com>
2023-05-07 15:03:10 +0900

    [BE] FIX: 생성자 대신 of로 생성하도록 변경

--- backend/src/main/java/org/ftclub/cabinet/cabinet/domain/Grid.java
+++ backend/src/main/java/org/ftclub/cabinet/cabinet/domain/Grid.java
@@ -2,13 +2,14 @@ package org.ftclub.cabinet.cabinet.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Grid {
 
@@ -19,5 +20,9 @@ public class Grid {
 
 	public boolean isValid() {
 		return (this.row > 0 && this.col > 0);
+	}
+
+	public static Grid of(Integer row, Integer col) {
+		return new Grid(row, col);
 	}
 }
--- backend/src/main/java/org/ftclub/cabinet/cabinet/domain/Location.java
+++ backend/src/main/java/org/ftclub/cabinet/cabinet/domain/Location.java
@@ -2,11 +2,9 @@ package org.ftclub.cabinet.cabinet.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.Validate;
 
 @Embeddable
 @NoArgsConstructor
@@ -14,16 +12,20 @@ import org.apache.commons.lang3.Validate;
 @Getter
 public class Location {
 
-    @Column(name = "BUILDING")
-    private String building;
-    @Column(name = "FLOOR")
-    private Integer floor;
-    @Column(name = "SECTION")
-    private String section;
+	@Column(name = "BUILDING")
+	private String building;
+	@Column(name = "FLOOR")
+	private Integer floor;
+	@Column(name = "SECTION")
+	private String section;
 
-    public Location(String building, Integer floor, String section) {
-        this.building = building;
-        this.floor = floor;
-        this.section = section;
-    }
+	protected Location(String building, Integer floor, String section) {
+		this.building = building;
+		this.floor = floor;
+		this.section = section;
+	}
+
+	public static Location of(String building, Integer floor, String section) {
+		return new Location(building, floor, section);
+	}
 }
