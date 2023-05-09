fa8e03e6ba4451663532d306c155c5082ae82637
Ssuamje <side2850@naver.com>
2023-05-09 11:48:10 +0900

    [BE] DOCS: 인증 JavaDoc 작성

--- backend/src/main/java/org/ftclub/cabinet/admin/domain/AdminRole.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/domain/AdminRole.java
@@ -1,6 +1,16 @@
 package org.ftclub.cabinet.admin.domain;
 
 
+/**
+ * 어드민의 권한을 나타내는 열거형 클래스입니다.
+ */
 public enum AdminRole {
-    NONE, ADMIN, MASTER;
+	/**
+	 * NONE: 승인 대기 - 무권한
+	 * <p>
+	 * ADMIN: 일반 관리자
+	 * <p>
+	 * MASTER: 최고 관리자
+	 */
+	NONE, ADMIN, MASTER;
 }
--- backend/src/main/java/org/ftclub/cabinet/auth/AuthGuard.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/AuthGuard.java
@@ -5,15 +5,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 인증이 필요한 부분에 붙이는 AOP 어노테이션입니다.
+ * <br>
+ * 컨트롤러 클래스, 혹은 컨트롤러 메서드에만 사용할 수 있습니다.
+ * <br>
+ * {@link org.ftclub.cabinet.auth.AuthAspect}에서 이 어노테이션을 검사합니다.
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AuthGuard {
 
-    Level level() default Level.USER_ONLY;
+	Level level() default Level.USER_ONLY;
 
-    enum Level {
-        USER_ONLY,
-        ADMIN_ONLY,
-        USER_OR_ADMIN
-    }
+	/**
+	 * 해당 어노테이션의 Level을 이용해서, 필요한 인증의 유무를 명시합니다.
+	 */
+	enum Level {
+		USER_ONLY,
+		ADMIN_ONLY,
+		USER_OR_ADMIN
+	}
 }
