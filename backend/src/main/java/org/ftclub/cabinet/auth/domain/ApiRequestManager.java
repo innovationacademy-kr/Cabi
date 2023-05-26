ccef6bf895701a2b9ce6b4378e65b50e493ac913
Ssuamje <side2850@naver.com>
2023-05-26 11:28:54 +0900

    [BE] DOCS: 주석 수정

--- backend/src/main/java/org/ftclub/cabinet/auth/domain/ApiRequestManager.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/domain/ApiRequestManager.java
@@ -5,10 +5,13 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 /**
- * API URI를 생성하는 빌더 클래스입니다.
+ * API 요청을 관리하는 매니저 클래스입니다.
  */
 public class ApiRequestManager {
 
+	/**
+	 * API 프로퍼티를 주입받아 사용합니다. OAuth 2.0 기준으로 작동합니다.
+	 */
 	private final ApiProperties apiProperties;
 
 	protected ApiRequestManager(ApiProperties apiProperties) {
@@ -21,8 +24,6 @@ public class ApiRequestManager {
 
 	/**
 	 * 로그인 리다이렉션을 위한 인증 URI를 생성합니다.
-	 * <p>
-	 * 인증 URI, 클라이언트 ID, 리다이렉트 URI, 스코프, 인증 방식을 이용하여 빌드된 빌더로 uri를 생성합니다.
 	 *
 	 * @return 인증 URI
 	 */
@@ -35,6 +36,12 @@ public class ApiRequestManager {
 				apiProperties.getGrantType());
 	}
 
+	/**
+	 * AccessToken 요청을 위한 RequestBodyMap을 생성합니다.
+	 *
+	 * @param code 인증 코드
+	 * @return AccessToken 요청을 위한 RequestBodyMap
+	 */
 	public MultiValueMap<String, String> getAccessTokenRequestBodyMap(String code) {
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
 		map.add("client_id", apiProperties.getClientId());
