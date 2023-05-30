c311212a43dbd9edb190d8a947f0dc53c7c35e69
donggyu <leedong1848@gmail.com>
2023-05-30 16:01:14 +0900

    [BE] TEST: restdocs를 위한 설정 추가

--- backend/src/test/java/org/ftclub/cabinet/auth/controller/AdminAuthControllerTest.java
+++ backend/src/test/java/org/ftclub/cabinet/auth/controller/AdminAuthControllerTest.java
@@ -1,28 +1,22 @@
 package org.ftclub.cabinet.auth.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.http.HttpServletResponse;
 import org.ftclub.cabinet.config.JwtProperties;
 import org.ftclub.cabinet.config.MasterProperties;
 import org.ftclub.cabinet.dto.MasterLoginDto;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import javax.servlet.http.HttpServletResponse;
 
 @SpringBootTest
 @AutoConfigureMockMvc
