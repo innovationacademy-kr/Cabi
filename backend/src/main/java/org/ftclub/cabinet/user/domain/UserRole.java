9642bd7fed64363c34c5cde1499e4aa149093789
space <enaenen@naver.com>
2023-06-13 00:05:56 +0900

    [BE] FIX: #1188(1190,1189)  메소드 병합

--- backend/src/main/java/org/ftclub/cabinet/user/domain/UserRole.java
+++ backend/src/main/java/org/ftclub/cabinet/user/domain/UserRole.java
@@ -2,5 +2,9 @@ package org.ftclub.cabinet.user.domain;
 
 
 public enum UserRole {
-    USER, CLUB
+    USER, CLUB;
+
+    public boolean isValid() {
+        return this.equals(USER) || this.equals(CLUB);
+    }
 }
