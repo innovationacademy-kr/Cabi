b5dc762385a61e50d29e9a95c71a34d89f296e63
Woo Joo Chae <enaenen@naver.com>
2023-06-12 14:49:14 +0900

    delete backendcodes

--- backend/src/admin/enums/admin.user.role.enum.ts
+++
@@ -1,9 +0,0 @@
-/**
- * admin_user의 권한을 나타냄
- */
-enum AdminUserRole {
-  AUTHORIZE_WAITING,
-  ADMIN,
-  ROOT_ADMIN,
-}
-export default AdminUserRole;
--- backend/src/enums/lent.type.enum.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 사물함 type을 정의
- */
-enum LentType {
-  PRIVATE = 'PRIVATE',
-  SHARE = 'SHARE',
-  CLUB = 'CLUB',
-}
-export default LentType;
