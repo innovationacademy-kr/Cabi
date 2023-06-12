39c1c113b068a35ef9e9750b3b31968df38d394e
Ssuamje <side2850@naver.com>
2023-06-12 20:26:07 +0900

    [BE] FIX: PathVariable -> Query Param으로 변경

--- backend/src/main/java/org/ftclub/cabinet/search/controller/SearchController.java
+++ backend/src/main/java/org/ftclub/cabinet/search/controller/SearchController.java
@@ -3,43 +3,46 @@ package org.ftclub.cabinet.search.controller;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.cabinet.service.CabinetFacadeService;
 import org.ftclub.cabinet.dto.CabinetInfoPaginationDto;
+import org.ftclub.cabinet.dto.UserCabinetPaginationDto;
 import org.ftclub.cabinet.dto.UserProfilePaginationDto;
 import org.ftclub.cabinet.user.service.UserFacadeService;
-import org.springframework.web.bind.annotation.*;
-import org.ftclub.cabinet.dto.UserCabinetPaginationDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v4/admin/search")
 public class SearchController {
 
-    private final CabinetFacadeService cabinetFacadeService;
-    private final UserFacadeService userFacadeService;
+	private final CabinetFacadeService cabinetFacadeService;
+	private final UserFacadeService userFacadeService;
 
-    @GetMapping("/cabinets")
-    public CabinetInfoPaginationDto getCabinetsInfo(
-            @RequestParam("visibleNum") Integer visibleNum
-    ) {
-        return cabinetFacadeService.getCabinetsInfo(visibleNum);
-    }
+	@GetMapping("/cabinets")
+	public CabinetInfoPaginationDto getCabinetsInfo(
+			@RequestParam("visibleNum") Integer visibleNum
+	) {
+		return cabinetFacadeService.getCabinetsInfo(visibleNum);
+	}
 
-    @GetMapping("/users-simple")
-    public UserProfilePaginationDto getUsersProfile(
-            @PathVariable("name") String name,
-            @RequestParam("page") Integer page,
-            @RequestParam("size") Integer size
-    ) {
-        return userFacadeService.getUserProfileListByPartialName(name, page, size);
-    }
+	@GetMapping("/users-simple")
+	public UserProfilePaginationDto getUsersProfile(
+			@RequestParam("name") String name,
+			@RequestParam("page") Integer page,
+			@RequestParam("size") Integer size
+	) {
+		return userFacadeService.getUserProfileListByPartialName(name, page, size);
+	}
 
-    @GetMapping("/users")
-    public UserCabinetPaginationDto getCabinetsLentInfo(
-            @PathVariable("name") String name,
-            @RequestParam("page") Integer page,
-            @RequestParam("size") Integer size
-    ){
-        return userFacadeService.findUserCabinetListByPartialName(name, page, size);
-    }
+	@GetMapping("/users")
+	public UserCabinetPaginationDto getCabinetsLentInfo(
+			@RequestParam("name") String name,
+			@RequestParam("page") Integer page,
+			@RequestParam("size") Integer size
+	) {
+		return userFacadeService.findUserCabinetListByPartialName(name, page, size);
+	}
 
 
 }
