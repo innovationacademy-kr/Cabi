bf15706e1e6d20b39d712a0a908a71754e44cd7b
Ssuamje <side2850@naver.com>
2023-06-12 21:37:59 +0900

    [BE] FIX: final인 DTO NoArgsConstructor로 변경

--- backend/src/main/java/org/ftclub/cabinet/dto/UpdateCabinetTitleDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/UpdateCabinetTitleDto.java
@@ -1,14 +1,13 @@
 package org.ftclub.cabinet.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class UpdateCabinetTitleDto {
 
 	@Size(max = 64)
-	private final String cabinetTitle;
+	private String cabinetTitle;
 }
