8768a01908a82f41bfe2777808f37bbce2b6cbad
Ssuamje <side2850@naver.com>
2023-06-12 21:48:56 +0900

    [BE] FIX: return-memo 작동되게끔 수정

--- backend/src/main/java/org/ftclub/cabinet/dto/LentEndMemoDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/LentEndMemoDto.java
@@ -2,15 +2,16 @@ package org.ftclub.cabinet.dto;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@ToString
 public class LentEndMemoDto {
 
-	@JsonProperty("cabinet_memo")
+	//	@JsonProperty("cabinetMemo") <- 얘 넣으면 작동 안 함
 	@Pattern(regexp = "\\d{4}", message = "cabinetMemo는 4글자의 숫자만 포함해야 합니다.")
 	@Size(min = 4, max = 4, message = "cabinetMemo는 4글자여야 합니다.")
 	private String cabinetMemo;
