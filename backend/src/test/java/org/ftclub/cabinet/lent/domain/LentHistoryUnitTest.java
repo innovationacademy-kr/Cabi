5f609870bb9ec6c4e2e098ab92309285410f2906
Woo Joo Chae <enaenen@naver.com>
2023-06-15 19:38:03 +0900

    [BE] TEST: Display, 메소드명 컨벤션에 맞게 수정

--- backend/src/test/java/org/ftclub/cabinet/lent/domain/LentHistoryUnitTest.java
+++ backend/src/test/java/org/ftclub/cabinet/lent/domain/LentHistoryUnitTest.java
@@ -16,8 +16,8 @@ import org.junit.jupiter.api.Test;
 class LentHistoryUnitTest {
 
     @Test
-    @DisplayName("of() 성공")
-    void of_Success() {
+    @DisplayName("of() 성공 - 정상입력")
+    void of_성공() {
         assertInstanceOf(LentHistory.class, LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -26,8 +26,8 @@ class LentHistoryUnitTest {
     }
 
     @Test
-    @DisplayName("of() 실패 대여시작시간 NULL")
-    void of_FAILURE_startedAt_NULL() {
+    @DisplayName("of() 실패 - 대여시작시간 NULL")
+    void of_실패_startedAt_NULL() {
         DomainException domainException = assertThrows(DomainException.class, () -> LentHistory.of(
                 null,
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -38,8 +38,8 @@ class LentHistoryUnitTest {
     }
 
     @Test
-    @DisplayName("of() 실패 만료시간 NULL")
-    void of_FAILURE_expiredAt_NULL() {
+    @DisplayName("of() 실패 - 만료시간 NULL")
+    void of_실패_expiredAt_NULL() {
         DomainException domainException = assertThrows(DomainException.class, () -> LentHistory.of(
                 DateUtil.getNow(),
                 null,
@@ -63,7 +63,7 @@ class LentHistoryUnitTest {
 
     @Test
     @DisplayName("of() 실패 유저ID NULL")
-    void of_FAILURE_userId_NULL() {
+    void of_실패_userId_NULL() {
         DomainException domainException = assertThrows(DomainException.class, () -> LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -74,8 +74,8 @@ class LentHistoryUnitTest {
     }
 
     @Test
-    @DisplayName("of() 실패 사물함ID NULL")
-    void of_FAILURE_cabinetId_NULL() {
+    @DisplayName("of() 실패 - 사물함ID NULL")
+    void of_실패_cabinetId_NULL() {
         DomainException domainException = assertThrows(DomainException.class, () -> LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -86,8 +86,8 @@ class LentHistoryUnitTest {
     }
 
     @Test
-    @DisplayName("캐비넷 아이디 비교 성공")
-    void isCabinetIdEqual_SUCCESS() {
+    @DisplayName("캐비넷 아이디 비교 성공 - ID 동일")
+    void isCabinetIdEqual_성공_ID_같음() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -98,8 +98,8 @@ class LentHistoryUnitTest {
     }
 
     @Test
-    @DisplayName("캐비넷 아이디 비교 실패")
-    void isCabinetIdEqual_FAILURE() {
+    @DisplayName("캐비넷 아이디 비교 실패 - ID 다름")
+    void isCabinetIdEqual_실패_ID_다름() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -110,8 +110,8 @@ class LentHistoryUnitTest {
     }
 
     @Test
-    @DisplayName("만료시간 설정여부 true ")
-    void isSetExpiredAt_TRUE() {
+    @DisplayName("만료시간 설정 성공 - 대여일자보다 3일 이후")
+    void isSetExpiredAt_성공_3일이후() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -121,21 +121,21 @@ class LentHistoryUnitTest {
         assertTrue(lentHistory.isSetExpiredAt());
     }
 
-    @Test
-    @DisplayName("만료시간 설정 false - 만료시간 무한")
-    void isSetExpiredAt_False() {
-        LentHistory lentHistory = LentHistory.of(
-                DateUtil.getNow(),
-                DateUtil.getInfinityDate(),
-                1L,
-                1L);
+//    @Test
+//    @DisplayName("만료시간 설정 false - 만료시간 무한")
+//    void isSetExpiredAt_실패() {
+//        LentHistory lentHistory = LentHistory.of(
+//                DateUtil.getNow(),
+//                DateUtil.getInfinityDate(),
+//                1L,
+//                1L);
+//
+//        assertFalse(lentHistory.isSetExpiredAt());
+//    }
 
-        assertFalse(lentHistory.isSetExpiredAt());
-    }
-
     @Test
-    @DisplayName("반납시간 설정 성공")
-    void isSetEndedAt_TRUE() {
+    @DisplayName("만료일자 설정 성공 - 3일이후만료")
+    void isSetEndedAt_성공_3일일후만료() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -147,21 +147,21 @@ class LentHistoryUnitTest {
         assertTrue(lentHistory.isSetEndedAt());
     }
 
-    @Test
-    @DisplayName("반납시간 설정 실패")
-    void isSetEndedAt_FALSE() {
-        LentHistory lentHistory = LentHistory.of(
-                DateUtil.getNow(),
-                DateUtil.getInfinityDate(),
-                1L,
-                1L);
+//    @Test
+//    @DisplayName("반납시간 설정 실패")
+//    void isSetEndedAt_실패_만료시간_무한() {
+//        LentHistory lentHistory = LentHistory.of(
+//                DateUtil.getNow(),
+//                DateUtil.getInfinityDate(),
+//                1L,
+//                1L);
+//
+//        assertFalse(lentHistory.isSetEndedAt());
+//    }
 
-        assertFalse(lentHistory.isSetEndedAt());
-    }
-
     @Test
-    @DisplayName("남은만료일자와 반납일자 차이 - 조기 반납 음수값")
-    void getDaysDiffEndedAndExpired_PRE_RETURN() {
+    @DisplayName("남은만료일자-반납일자 성공 - 3일 조기반납")
+    void getDaysDiffEndedAndExpired_성공_조기반납() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -175,8 +175,8 @@ class LentHistoryUnitTest {
 
 
     @Test
-    @DisplayName("남은만료일자와 반납일자 차이 - 연체 반납 양수값")
-    void getDaysDiffEndedAndExpired_DELAYED_RETURN() {
+    @DisplayName("남은만료일자-반납일자차이 성공 - 5일 연체반납")
+    void getDaysDiffEndedAndExpired_성공_연체반납() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
@@ -191,34 +191,21 @@ class LentHistoryUnitTest {
 
     @Test
     @DisplayName("남은만료일자와 반납일자 차이 - 잔여 0일 반납")
-    void getDaysDiffEndedAndExpired_PROPER_RETURN() {
+    void getDaysDiffEndedAndExpired_성공_정상반납() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
                 1L,
                 1L);
 
-        lentHistory.endLent(DateUtil.addDaysToDate(DateUtil.getNow(), 3)); // 2일 연체 반납
-
-        assertEquals( 0, lentHistory.getDaysDiffEndedAndExpired());
-    }
-
-    @Test
-    @DisplayName("남은만료일자와 반납일자 차이 - 잔여 0일 반납")
-    void getDaysDiffEndedAndExpired_FAILURE() {
-        LentHistory lentHistory = LentHistory.of(
-                DateUtil.getNow(),
-                DateUtil.addDaysToDate(DateUtil.getNow(), 3),
-                1L,
-                1L);
-
-        DomainException domainException = assertThrows(DomainException.class, () -> lentHistory.endLent(null));
+        DomainException domainException = assertThrows(DomainException.class,
+                () -> lentHistory.endLent(null));
         assertEquals(ExceptionStatus.INVALID_STATUS, domainException.getStatus());
     }
 
     @Test
-    @DisplayName("반납 함수 성공")
-    void endLent_SUCCESS() {
+    @DisplayName("반납 함수 성공 - 반납일자가 대여일자보다 이후")
+    void endLent_성공_대여일자_이후에_반납() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.getNow(),
                 DateUtil.getNow(),
@@ -233,14 +220,15 @@ class LentHistoryUnitTest {
 
     @Test
     @DisplayName("반납 함수 실패 - 대여일자가 반납일자보다 이후")
-    void endLent_FAILURE_startDate_BIGGER_THEN_endDate() {
+    void endLent_실패_대여일자_이전에_반납() {
         LentHistory lentHistory = LentHistory.of(
                 DateUtil.addDaysToDate(DateUtil.getNow(), 3),
                 DateUtil.addDaysToDate(DateUtil.getNow(), 5),
                 1L,
                 1L);
 
-        DomainException domainException = assertThrows(DomainException.class, () -> lentHistory.endLent(DateUtil.getNow()));
+        DomainException domainException = assertThrows(DomainException.class,
+                () -> lentHistory.endLent(DateUtil.getNow()));
         assertEquals(ExceptionStatus.INVALID_STATUS, domainException.getStatus());
     }
 }
