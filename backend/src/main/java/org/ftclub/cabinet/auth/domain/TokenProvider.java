44716253657a20adef055b64efa275f863b495eb
Ssuamje <side2850@naver.com>
2023-06-15 19:30:41 +0900

    [BE] HOTFIX: 멤버인 경우 BlackHoledAt 파싱 안되던 점 수정

--- backend/src/main/java/org/ftclub/cabinet/auth/domain/TokenProvider.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/domain/TokenProvider.java
@@ -61,11 +61,17 @@ public class TokenProvider {
 			}
 			claims.put("email", email);
 			claims.put("name", profile.get("login").asText());
-			claims.put("blackholedAt",
-					profile.get("cursus_users").get(1).get("blackholed_at") != null ?
-							profile.get("cursus_users").get(1).get("blackholed_at").asText()
-									.substring(0, 10)
-							: null);
+			JsonNode blackholedAt = profile.get("cursus_users").get(1).get("blackholed_at");
+			if (blackholedAt.equals(null) || blackholedAt.asText().equals("null")) {
+				claims.put("blackholedAt", null);
+			} else {
+				claims.put("blackholedAt", blackholedAt.asText().substring(0, 10));
+			}
+//			claims.put("blackholedAt",
+//					profile.get("cursus_users").get(1).get("blackholed_at") != null ?
+//							profile.get("cursus_users").get(1).get("blackholed_at").asText()
+//									.substring(0, 10)
+//							: null);
 			claims.put("role", UserRole.USER);
 		}
 		return claims;
