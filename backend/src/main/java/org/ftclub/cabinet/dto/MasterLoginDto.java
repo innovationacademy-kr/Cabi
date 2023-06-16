f836551012cb7476112afbf5a6c0fdd2f78d93f9
Woo Joo Chae <enaenen@naver.com>
2023-06-16 17:25:06 +0900

    [BE] FIX: MasterLoginDto NoArgsConstructor

--- backend/src/main/java/org/ftclub/cabinet/dto/MasterLoginDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/MasterLoginDto.java
@@ -2,11 +2,13 @@ package org.ftclub.cabinet.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class MasterLoginDto {
 
-	String id;
-	String password;
+    String id;
+    String password;
 }
