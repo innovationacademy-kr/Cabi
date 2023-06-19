da0f50b60609bd2d4497d6efab6e634107e6c7ba
eunbi9n <gombi9_9@naver.com>
2023-06-19 16:32:34 +0900

    [BE] FEAT: search 관련 파일 logger 추가

--- backend/src/main/java/org/ftclub/cabinet/search/controller/SearchController.java
+++ backend/src/main/java/org/ftclub/cabinet/search/controller/SearchController.java
@@ -3,11 +3,14 @@ package org.ftclub.cabinet.search.controller;
 import static org.ftclub.cabinet.auth.domain.AuthLevel.ADMIN_ONLY;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.ftclub.cabinet.auth.domain.AuthGuard;
 import org.ftclub.cabinet.cabinet.service.CabinetFacadeService;
 import org.ftclub.cabinet.dto.CabinetInfoPaginationDto;
 import org.ftclub.cabinet.dto.UserCabinetPaginationDto;
 import org.ftclub.cabinet.dto.UserProfilePaginationDto;
+import org.ftclub.cabinet.statistics.controller.StatisticsController;
 import org.ftclub.cabinet.user.service.UserFacadeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v4/admin/search")
 public class SearchController {
+	private static final Logger logger = LogManager.getLogger(SearchController.class);
 	private final CabinetFacadeService cabinetFacadeService;
 	private final UserFacadeService userFacadeService;
 
@@ -26,6 +30,7 @@ public class SearchController {
 	public CabinetInfoPaginationDto getCabinetsInfo(
 			@RequestParam("visibleNum") Integer visibleNum
 	) {
+		logger.info("Called getCabinetsInfo");
 		return cabinetFacadeService.getCabinetsInfo(visibleNum);
 	}
 
@@ -36,6 +41,7 @@ public class SearchController {
 			@RequestParam("page") Integer page,
 			@RequestParam("size") Integer size
 	) {
+		logger.info("Called getUsersProfile");
 		return userFacadeService.getUserProfileListByPartialName(name, page, size);
 	}
 
@@ -46,6 +52,7 @@ public class SearchController {
 			@RequestParam("page") Integer page,
 			@RequestParam("size") Integer size
 	) {
+		logger.info("Called getCabinetsLentInfo");
 		return userFacadeService.findUserCabinetListByPartialName(name, page, size);
 	}
 
