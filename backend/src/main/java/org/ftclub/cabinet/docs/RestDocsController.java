31dbf2f25a773686048e83da5b82cfeae1dcecec
donggyu <leedong1848@gmail.com>
2023-06-25 18:39:27 +0900

    [BE] DOCS: docs 아티팩트 매핑

--- backend/src/main/java/org/ftclub/cabinet/docs/RestDocsController.java
+++ backend/src/main/java/org/ftclub/cabinet/docs/RestDocsController.java
@@ -1,15 +1,18 @@
 package org.ftclub.cabinet.docs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/docs")
+@RequestMapping("/v4/docs")
+@Slf4j
 public class RestDocsController {
 
 	@GetMapping
 	public String index() {
-		return "index";
+		log.info("get /v4/docs");
+		return "docs/index.html";
 	}
 }
