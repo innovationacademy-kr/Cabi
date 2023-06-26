7bf4ebb1b32ad5743b2ecf25083eabea2c62f9f4
donggyu <leedong1848@gmail.com>
2023-06-26 14:43:00 +0900

    [BE] TEST: junit에 맞게 고치고 테스트 코드 단순화

--- backend/src/test/java/org/ftclub/cabinet/auth/domain/TokenValidatorTest.java
+++ backend/src/test/java/org/ftclub/cabinet/auth/domain/TokenValidatorTest.java
@@ -1,5 +1,9 @@
 package org.ftclub.cabinet.auth.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Date;
@@ -10,7 +14,6 @@ import org.ftclub.cabinet.utils.DateUtil;
 import org.ftclub.testutils.TestControllerUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -96,7 +99,7 @@ public class TokenValidatorTest {
 	void 토큰_페이로드_가져오기() throws JsonProcessingException {
 		String userToken = TestControllerUtils.getTestUserToken(jwtProperties.getSigningKey(),
 				DateUtil.getNow());
-		Assert.assertEquals("testUserName",
+		assertEquals("testUserName",
 				tokenValidator.getPayloadJson(userToken).get("name").asText());
 	}
 
@@ -109,23 +112,14 @@ public class TokenValidatorTest {
 		String masterToken = TestControllerUtils.getTestMasterToken(jwtProperties.getSigningKey(),
 				DateUtil.getNow());
 
-		Assert.assertEquals(true,
-				tokenValidator.isAdminRoleValid(userToken, AuthLevel.USER_OR_ADMIN));
-		Assert.assertEquals(true,
-				tokenValidator.isAdminRoleValid(adminToken, AuthLevel.USER_OR_ADMIN));
-		Assert.assertEquals(true,
-				tokenValidator.isAdminRoleValid(adminToken, AuthLevel.ADMIN_ONLY));
-		Assert.assertEquals(true,
-				tokenValidator.isAdminRoleValid(masterToken, AuthLevel.MASTER_ONLY));
-
-		Assert.assertEquals(false,
-				tokenValidator.isAdminRoleValid(userToken, AuthLevel.USER_ONLY));
-		Assert.assertEquals(false,
-				tokenValidator.isAdminRoleValid(userToken, AuthLevel.ADMIN_ONLY));
-		Assert.assertEquals(false,
-				tokenValidator.isAdminRoleValid(userToken, AuthLevel.MASTER_ONLY));
-		Assert.assertEquals(false,
-				tokenValidator.isAdminRoleValid(adminToken, AuthLevel.MASTER_ONLY));
+		assertTrue(tokenValidator.isAdminRoleValid(userToken, AuthLevel.USER_OR_ADMIN));
+		assertTrue(tokenValidator.isAdminRoleValid(adminToken, AuthLevel.USER_OR_ADMIN));
+		assertTrue(tokenValidator.isAdminRoleValid(adminToken, AuthLevel.ADMIN_ONLY));
+		assertTrue(tokenValidator.isAdminRoleValid(masterToken, AuthLevel.MASTER_ONLY));
+		assertFalse(tokenValidator.isAdminRoleValid(userToken, AuthLevel.USER_ONLY));
+		assertFalse(tokenValidator.isAdminRoleValid(userToken, AuthLevel.ADMIN_ONLY));
+		assertFalse(tokenValidator.isAdminRoleValid(userToken, AuthLevel.MASTER_ONLY));
+		assertFalse(tokenValidator.isAdminRoleValid(adminToken, AuthLevel.MASTER_ONLY));
 	}
 
 }
