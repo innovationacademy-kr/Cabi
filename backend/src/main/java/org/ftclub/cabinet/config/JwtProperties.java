185078683fe5f2131a729ef51776c7a110ae6f84
Ssuamje <side2850@naver.com>
2023-06-28 15:38:02 +0900

    [BE] FIX: yaml 파일에서 active하는 profile별로 auth 다르게 설정되도록 수정

--- backend/src/main/java/org/ftclub/cabinet/config/JwtProperties.java
+++ backend/src/main/java/org/ftclub/cabinet/config/JwtProperties.java
@@ -1,37 +1,38 @@
 package org.ftclub.cabinet.config;
 
 import io.jsonwebtoken.SignatureAlgorithm;
-import java.security.Key;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
 
 @Component
 @Getter
 public class JwtProperties {
 
-	@Value("${spring.oauth2.jwt.token.secret-key}")
-	private String secret;
+    @Value("${spring.auth.jwt-secret-key}")
+    private String secret;
 
-	@Value("${spring.oauth2.jwt.token.main-token-name}")
-	private String mainTokenName;
+    @Value("${spring.oauth2.jwt.token.main-token-name}")
+    private String mainTokenName;
 
-	@Value("${spring.oauth2.jwt.token.main-provider}")
-	private String mainProviderName;
+    @Value("${spring.oauth2.jwt.token.main-provider}")
+    private String mainProviderName;
 
-	@Value("${spring.oauth2.jwt.token.admin-token-name}")
-	private String adminTokenName;
+    @Value("${spring.oauth2.jwt.token.admin-token-name}")
+    private String adminTokenName;
 
-	@Value("${spring.oauth2.jwt.token.admin-provider}")
-	private String adminProviderName;
+    @Value("${spring.oauth2.jwt.token.admin-provider}")
+    private String adminProviderName;
 
-	@Value("${spring.oauth2.jwt.token.expiry}")
-	private Integer expiry;
+    @Value("${spring.oauth2.jwt.token.expiry}")
+    private Integer expiry;
 
-	public Key getSigningKey() {
-		byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(this.secret);
-		return new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS256.getJcaName());
-	}
+    public Key getSigningKey() {
+        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(this.secret);
+        return new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS256.getJcaName());
+    }
 }
