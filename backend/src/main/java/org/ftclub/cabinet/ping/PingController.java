644ae2c07beae0ce25fb6c04a58c6e43a8e9e64b
donggyu <leedong1848@gmail.com>
2023-07-01 02:33:59 +0900

    [BE] FIX: DI 확인을 위한 ping url 복구

--- backend/src/main/java/org/ftclub/cabinet/ping/PingController.java
+++ backend/src/main/java/org/ftclub/cabinet/ping/PingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v4/ping")
+@RequestMapping("/ping")
 public class PingController {
 
 	@RequestMapping
