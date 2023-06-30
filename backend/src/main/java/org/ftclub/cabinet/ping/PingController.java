6dd133f3088f9a332bd34d288e96187e9a28d251
donggyu <leedong1848@gmail.com>
2023-07-01 02:06:48 +0900

    [BE] FIX: change ping url

--- backend/src/main/java/org/ftclub/cabinet/ping/PingController.java
+++ backend/src/main/java/org/ftclub/cabinet/ping/PingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ping")
+@RequestMapping("/v4/ping")
 public class PingController {
 
 	@RequestMapping
