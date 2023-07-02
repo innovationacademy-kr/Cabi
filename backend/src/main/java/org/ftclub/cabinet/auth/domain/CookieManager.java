8702123e4d50b6427e4362e6043a62c049194694
Ssuamje <side2850@naver.com>
2023-07-02 10:30:45 +0900

    [BE] FIX: DeleteCookie MaxAge 0으로 변경, 즉각 삭제하게끔 재수정

--- backend/src/main/java/org/ftclub/cabinet/auth/domain/CookieManager.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/domain/CookieManager.java
@@ -16,60 +16,60 @@ import javax.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class CookieManager {
 
-    private final DomainProperties domainProperties;
-    private final JwtProperties jwtProperties;
+	private final DomainProperties domainProperties;
+	private final JwtProperties jwtProperties;
 
-    /**
-     * 쿠키를 가져옵니다.
-     *
-     * @param req  요청 시의 서블렛 {@link HttpServletRequest}
-     * @param name 쿠키 이름
-     * @return 쿠키 값, 없는 경우 null
-     */
-    public String getCookieValue(HttpServletRequest req, String name) {
-        Cookie[] cookies = req.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(name)) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
-    }
+	/**
+	 * 쿠키를 가져옵니다.
+	 *
+	 * @param req  요청 시의 서블렛 {@link HttpServletRequest}
+	 * @param name 쿠키 이름
+	 * @return 쿠키 값, 없는 경우 null
+	 */
+	public String getCookieValue(HttpServletRequest req, String name) {
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(name)) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
+	}
 
-    /**
-     * 쿠키를 설정합니다.
-     *
-     * @param res        요청 시의 서블렛 {@link HttpServletResponse}
-     * @param name       쿠키 이름
-     * @param value      쿠키 값
-     * @param path       쿠키 사용 경로
-     * @param serverName 쿠키 도메인
-     */
-    public void setCookieToClient(HttpServletResponse res, Cookie cookie, String path,
-                                  String serverName) {
-        cookie.setMaxAge(60 * 60 * 24 * jwtProperties.getExpiryDays());
-        cookie.setPath(path);
-        if (serverName.equals(domainProperties.getLocal())) {
-            cookie.setDomain(domainProperties.getLocal());
-        } else {
-            cookie.setDomain(domainProperties.getCookieDomain());
-        }
-        res.addCookie(cookie);
-    }
+	/**
+	 * 쿠키를 설정합니다.
+	 *
+	 * @param res        요청 시의 서블렛 {@link HttpServletResponse}
+	 * @param name       쿠키 이름
+	 * @param value      쿠키 값
+	 * @param path       쿠키 사용 경로
+	 * @param serverName 쿠키 도메인
+	 */
+	public void setCookieToClient(HttpServletResponse res, Cookie cookie, String path,
+	                              String serverName) {
+		cookie.setMaxAge(60 * 60 * 24 * jwtProperties.getExpiryDays());
+		cookie.setPath(path);
+		if (serverName.equals(domainProperties.getLocal())) {
+			cookie.setDomain(domainProperties.getLocal());
+		} else {
+			cookie.setDomain(domainProperties.getCookieDomain());
+		}
+		res.addCookie(cookie);
+	}
 
-    /**
-     * 쿠키를 삭제합니다.
-     * <p>
-     * 해당 쿠키의 만료시간을 0으로 설정하는 방식으로 삭제합니다.
-     *
-     * @param res  요청 시의 서블렛 {@link HttpServletResponse}
-     * @param name 쿠키 이름
-     */
-    public void deleteCookie(HttpServletResponse res, String name) {
-        Cookie cookie = new Cookie(name, null);
-        cookie.setMaxAge(-1);
-        res.addCookie(cookie);
-    }
+	/**
+	 * 쿠키를 삭제합니다.
+	 * <p>
+	 * 해당 쿠키의 만료시간을 0으로 설정하는 방식으로 삭제합니다.
+	 *
+	 * @param res  요청 시의 서블렛 {@link HttpServletResponse}
+	 * @param name 쿠키 이름
+	 */
+	public void deleteCookie(HttpServletResponse res, String name) {
+		Cookie cookie = new Cookie(name, null);
+		cookie.setMaxAge(0);
+		res.addCookie(cookie);
+	}
 }
