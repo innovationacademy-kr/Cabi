d97edc633a83e4a9ecd6844efed4437ead33e16b
Ssuamje <side2850@naver.com>
2023-07-03 20:00:48 +0900

    [BE] FIX: AuthService에서 사용하지 않는 멤버 제거

--- backend/src/main/java/org/ftclub/cabinet/auth/service/AuthService.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/service/AuthService.java
@@ -1,10 +1,7 @@
 package org.ftclub.cabinet.auth.service;
 
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.ftclub.cabinet.auth.domain.TokenProvider;
 import org.ftclub.cabinet.config.DomainProperties;
-import org.ftclub.cabinet.config.JwtProperties;
 import org.ftclub.cabinet.config.MasterProperties;
 import org.ftclub.cabinet.dto.MasterLoginDto;
 import org.ftclub.cabinet.exception.ExceptionStatus;
@@ -13,6 +10,8 @@ import org.ftclub.cabinet.user.domain.UserRole;
 import org.ftclub.cabinet.user.service.UserService;
 import org.ftclub.cabinet.utils.DateUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * Cabi 자체의 인증 서비스입니다.
@@ -23,9 +22,7 @@ public class AuthService {
 
 	private final MasterProperties masterProperties;
 	private final DomainProperties domainProperties;
-	private final JwtProperties jwtProperties;
 	private final UserService userService;
-	private final TokenProvider tokenProvider;
 
 	public boolean validateMasterLogin(MasterLoginDto masterLoginDto) {
 		return masterLoginDto.getId().equals(masterProperties.getId())
