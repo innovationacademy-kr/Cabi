993ec8137a16d57e76d16e6d63e8e8c91752c064
Ssuamje <side2850@naver.com>
2023-07-04 15:59:22 +0900

    [BE] FEAT: OauthService 유닛 테스트 생략

--- backend/src/test/java/org/ftclub/cabinet/auth/service/OauthServiceUnitTest.java
+++ backend/src/test/java/org/ftclub/cabinet/auth/service/OauthServiceUnitTest.java
@@ -1,6 +1,8 @@
 package org.ftclub.cabinet.auth.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,20 +16,26 @@ public class OauthServiceUnitTest {
 
 	@InjectMocks
 	OauthService oauthService;
-    
+
 	@Mock
 	ObjectMapper objectMapper = mock(ObjectMapper.class);
 
 	@Test
+	@DisplayName("외부 API 의존으로 생략")
+	@Disabled
 	void sendToApi() {
 
 	}
 
 	@Test
+	@DisplayName("외부 API 의존으로 생략")
+	@Disabled
 	void getTokenByCode() {
 	}
 
 	@Test
+	@DisplayName("외부 API 의존으로 생략")
+	@Disabled
 	void getProfileByToken() {
 	}
 }
