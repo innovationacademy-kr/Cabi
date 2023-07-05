ae04b215e4c72a6feb4414120b0c029d43101938
space <enaenen@naver.com>
2023-07-05 17:31:06 +0900

    [BE] REFACTOR: Cabinet NULL 검사는 객체 isValid 에 위임

--- backend/src/main/java/org/ftclub/cabinet/lent/domain/LentPolicyImpl.java
+++ backend/src/main/java/org/ftclub/cabinet/lent/domain/LentPolicyImpl.java
@@ -55,17 +55,15 @@ public class LentPolicyImpl implements LentPolicy {
 		}
 
 		LentType lentType = cabinet.getLentType();
-		if (lentType != null) {
-			switch (lentType) {
-				case PRIVATE:
-					return now.plusDays(getDaysForLentTermPrivate());
-				case SHARE:
-					LentHistory lentHistory = activeLentHistories.get(0);
-					return generateSharedCabinetExpirationDate(now,
-							cabinet.getStatus(), lentHistory);
-				case CLUB:
-					return DateUtil.getInfinityDate();
-			}
+		switch (lentType) {
+			case PRIVATE:
+				return now.plusDays(getDaysForLentTermPrivate());
+			case SHARE:
+				LentHistory lentHistory = activeLentHistories.get(0);
+				return generateSharedCabinetExpirationDate(now,
+						cabinet.getStatus(), lentHistory);
+			case CLUB:
+				return DateUtil.getInfinityDate();
 		}
 		throw new IllegalArgumentException("대여 상태가 잘못되었습니다.");
 	}
