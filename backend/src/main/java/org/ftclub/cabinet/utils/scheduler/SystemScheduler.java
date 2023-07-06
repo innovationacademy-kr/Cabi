5bd38b61b39493ab8252defbb5d490aae65dcbe5
sichoi42 <42.4.sichoi@gmail.com>
2023-07-06 17:19:59 +0900

    [BE] FIX: 시스템 스케줄러 자정마다 실행되도록 수정

--- backend/src/main/java/org/ftclub/cabinet/utils/scheduler/SystemScheduler.java
+++ backend/src/main/java/org/ftclub/cabinet/utils/scheduler/SystemScheduler.java
@@ -27,9 +27,7 @@ public class SystemScheduler {
 	/**
 	 * 매일 자정마다 대여 기록을 확인하여, 연체 메일 발송 및 휴학생 처리를 트리거하는 메소드
 	 */
-	// Every Midnight
-//	@Scheduled(cron = "0 0 0 * * *")
-	@Scheduled(initialDelay = 3000, fixedDelay = 1000 * 60 * 60 * 24)
+	@Scheduled(cron = "0 0 0 * * *")
 	public void checkAllLents() {
 		log.info("called checkAllLents");
 		List<ActiveLentHistoryDto> activeLents = this.lentService.getAllActiveLentHistories();
