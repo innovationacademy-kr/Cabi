f758655e9971c56bf42497ad91e13f02f7dc1be1
sichoi42 <42.4.sichoi@gmail.com>
2023-07-06 18:01:06 +0900

    [BE] TEST: gmailProperties 생성 BeforeAll로 이동

--- backend/src/test/java/org/ftclub/cabinet/utils/OverdueManagerUnitTest.java
+++ backend/src/test/java/org/ftclub/cabinet/utils/OverdueManagerUnitTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import javax.mail.MessagingException;
 import org.ftclub.cabinet.cabinet.domain.CabinetStatus;
 import org.ftclub.cabinet.cabinet.service.CabinetService;
+import org.ftclub.cabinet.config.GmailProperties;
 import org.ftclub.cabinet.config.MailOverdueProperties;
 import org.ftclub.cabinet.dto.ActiveLentHistoryDto;
 import org.ftclub.cabinet.utils.overdue.manager.OverdueManager;
@@ -27,29 +28,34 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class OverdueManagerUnitTest {
 
-	@Mock(lenient = true)
-	private MailOverdueProperties mailOverdueProperties = mock(MailOverdueProperties.class);
-
 	@Mock
 	private CabinetService cabinetService = mock(CabinetService.class);
-
 	@Mock
 	private EmailSender emailSender = mock(EmailSender.class);
-
+	@Mock(lenient = true)
+	private MailOverdueProperties mailOverdueProperties = mock(MailOverdueProperties.class);
 	@InjectMocks
 	private OverdueManager overdueManager;
-
 	private static ActiveLentHistoryDto activeLentHistoryDto;
-
+	private static GmailProperties gmailProperties;
 
 	@BeforeAll
-	@DisplayName("테스트 전에 activeLentHistoryDto를 생성한다.")
+	@DisplayName("테스트 전에 공용 객체를 생성합니다.")
 	static void setupBeforeAll() {
 		activeLentHistoryDto = mock(ActiveLentHistoryDto.class);
 		given(activeLentHistoryDto.getUserId()).willReturn(1L);
 		given(activeLentHistoryDto.getName()).willReturn("동글동글동그리");
 		given(activeLentHistoryDto.getEmail()).willReturn("동글레차.student.42seoul.kr");
 		given(activeLentHistoryDto.getCabinetId()).willReturn(1L);
+
+		gmailProperties = mock(GmailProperties.class);
+		given(gmailProperties.getHost()).willReturn("smtp.gmail.com");
+		given(gmailProperties.getPort()).willReturn(587);
+		given(gmailProperties.getUsername()).willReturn("까비의부장님사난.gmail.com");
+		given(gmailProperties.getPassword()).willReturn("비밀입니다.");
+		given(gmailProperties.getUseAuth()).willReturn(true);
+		given(gmailProperties.getUseStartTls()).willReturn(true);
+		given(gmailProperties.getIsProduction()).willReturn(false);
 	}
 
 	@BeforeEach
@@ -150,7 +156,7 @@ public class OverdueManagerUnitTest {
 
 	@Test
 	@DisplayName("실패: NONE 상태에서는 연체 처리를 안함")
-	void 실패_handleOverdue_SOON_NONE() throws MessagingException, IOException {
+	void 실패_handleOverdue_NONE() throws MessagingException, IOException {
 		given(activeLentHistoryDto.getIsExpired()).willReturn(false);
 		given(activeLentHistoryDto.getDaysLeftFromExpireDate()).willReturn(0L);
 
