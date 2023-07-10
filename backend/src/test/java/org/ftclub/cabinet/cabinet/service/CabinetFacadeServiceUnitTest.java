de8e9940a73174d0cd616e02003346f08b3bb0ef
space <enaenen@naver.com>
2023-07-10 12:13:06 +0900

    [BE] TEST: getCabinetInfo 성공 & 실패 테스트, null테스트 fix

--- backend/src/test/java/org/ftclub/cabinet/cabinet/service/CabinetFacadeServiceUnitTest.java
+++ backend/src/test/java/org/ftclub/cabinet/cabinet/service/CabinetFacadeServiceUnitTest.java
@@ -4,16 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.ftclub.cabinet.cabinet.domain.Cabinet;
+import org.ftclub.cabinet.cabinet.domain.CabinetStatus;
+import org.ftclub.cabinet.cabinet.domain.LentType;
+import org.ftclub.cabinet.cabinet.domain.Location;
 import org.ftclub.cabinet.cabinet.repository.CabinetOptionalFetcher;
-import org.ftclub.cabinet.cabinet.service.CabinetFacadeServiceImpl;
 import org.ftclub.cabinet.dto.BuildingFloorsDto;
+import org.ftclub.cabinet.dto.CabinetInfoResponseDto;
+import org.ftclub.cabinet.dto.LentDto;
+import org.ftclub.cabinet.lent.domain.LentHistory;
 import org.ftclub.cabinet.lent.repository.LentOptionalFetcher;
 import org.ftclub.cabinet.mapper.CabinetMapper;
 import org.ftclub.cabinet.mapper.LentMapper;
+import org.ftclub.cabinet.user.domain.User;
 import org.ftclub.cabinet.user.repository.UserOptionalFetcher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +29,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cglib.core.Local;
 
 @ExtendWith(MockitoExtension.class)
 class CabinetFacadeServiceUnitTest {
@@ -38,9 +47,9 @@ class CabinetFacadeServiceUnitTest {
 	@Mock
 	LentMapper lentMapper;
 
-	private ArrayList<Integer> generateFloors(int limit){
+	private ArrayList<Integer> generateFloors(int limit) {
 		ArrayList<Integer> floors = new ArrayList<>();
-		for(int i = 1; i <= limit; i++) {
+		for (int i = 1; i <= limit; i++) {
 			floors.add(i);
 		}
 		return floors;
@@ -58,7 +67,6 @@ class CabinetFacadeServiceUnitTest {
 		ArrayList<Integer> floors2 = generateFloors(8);
 		ArrayList<Integer> floors3 = generateFloors(21);
 
-
 		List<String> buildings = new ArrayList<>();
 		buildings.add(building1);
 		buildings.add(building2);
@@ -70,9 +78,12 @@ class CabinetFacadeServiceUnitTest {
 		given(cabinetOptionalFetcher.findAllFloorsByBuilding(building2)).willReturn(floors2);
 		given(cabinetOptionalFetcher.findAllFloorsByBuilding(building3)).willReturn(floors3);
 
-		given(cabinetMapper.toBuildingFloorsDto(building1, floors1)).willReturn(new BuildingFloorsDto(building1, floors1));
-		given(cabinetMapper.toBuildingFloorsDto(building2, floors2)).willReturn(new BuildingFloorsDto(building2, floors2));
-		given(cabinetMapper.toBuildingFloorsDto(building3, floors3)).willReturn(new BuildingFloorsDto(building3, floors3));
+		given(cabinetMapper.toBuildingFloorsDto(building1, floors1)).willReturn(
+				new BuildingFloorsDto(building1, floors1));
+		given(cabinetMapper.toBuildingFloorsDto(building2, floors2)).willReturn(
+				new BuildingFloorsDto(building2, floors2));
+		given(cabinetMapper.toBuildingFloorsDto(building3, floors3)).willReturn(
+				new BuildingFloorsDto(building3, floors3));
 
 		//when
 		List<BuildingFloorsDto> buildingFloorsResponse = cabinetFacadeService.getBuildingFloorsResponse();
@@ -92,24 +103,112 @@ class CabinetFacadeServiceUnitTest {
 		then(cabinetMapper).should(times(3)).toBuildingFloorsDto(any(), any());
 	}
 
-	//WIP
 	@Test
 	@DisplayName("성공: 건물과 층 조회 - 정보 없음")
 	void 성공_NULL_getBuildingFloorsResponse() {
-		//setups
-
 		//given
-		given(cabinetOptionalFetcher.findAllBuildings()).willReturn(null);
+		given(cabinetOptionalFetcher.findAllBuildings()).willReturn(new ArrayList<>());
 
 		//when
 		List<BuildingFloorsDto> buildingFloorsResponse = cabinetFacadeService.getBuildingFloorsResponse();
 		//then
-		assertNull(buildingFloorsResponse);
+		assertNotNull(buildingFloorsResponse);
 		then(cabinetOptionalFetcher).should().findAllBuildings();
 	}
 
 	@Test
-	void getCabinetInfo() {
+	@DisplayName("성공: 캐비넷 정보 조회")
+	void 성공_getCabinetInfo() {
+		//given
+		Long cabinetId = 123L;
+
+		User findUser1 = mock(User.class);
+		User findUser2 = mock(User.class);
+		User findUser3 = mock(User.class);
+
+		LentHistory lentHistory1 = mock(LentHistory.class);
+		LentHistory lentHistory2 = mock(LentHistory.class);
+		LentHistory lentHistory3 = mock(LentHistory.class);
+		given(lentHistory1.getUser()).willReturn(findUser1);
+		given(lentHistory2.getUser()).willReturn(findUser2);
+		given(lentHistory3.getUser()).willReturn(findUser3);
+
+		List<LentHistory> lentHistories = new ArrayList<>();
+		lentHistories.add(lentHistory1);
+		lentHistories.add(lentHistory2);
+		lentHistories.add(lentHistory3);
+
+		LentDto lentDto1 = mock(LentDto.class);
+		LentDto lentDto2 = mock(LentDto.class);
+		LentDto lentDto3 = mock(LentDto.class);
+		Location location = mock(Location.class);
+		Cabinet cabinet = mock(Cabinet.class);
+
+		given(lentOptionalFetcher.findAllActiveLentByCabinetId(cabinetId))
+				.willReturn(lentHistories);
+
+		given(lentMapper.toLentDto(findUser1, lentHistory1))
+				.willReturn(lentDto1);
+		given(lentMapper.toLentDto(findUser2, lentHistory2))
+				.willReturn(lentDto2);
+		given(lentMapper.toLentDto(findUser3, lentHistory3))
+				.willReturn(lentDto3);
+
+		given(cabinetOptionalFetcher.findCabinet(cabinetId))
+				.willReturn(cabinet);
+
+		given(cabinetMapper.toCabinetInfoResponseDto(cabinet,
+				List.of(lentDto1, lentDto2, lentDto3)))
+				.willReturn(
+						new CabinetInfoResponseDto(
+								cabinetId,
+								21,
+								LentType.PRIVATE,
+								3,
+								"뭐가이렇게많냐",
+								CabinetStatus.FULL,
+								"THIS IS STATUS",
+								location,
+								List.of(lentDto1, lentDto2, lentDto3)
+						)
+				);
+
+		// when
+		CabinetInfoResponseDto cabinetInfo = cabinetFacadeService.getCabinetInfo(cabinetId);
+
+		// then
+		then(lentOptionalFetcher).should().findAllActiveLentByCabinetId(cabinetId);
+		then(lentMapper).should(times(3)).toLentDto(any(), any());
+		then(cabinetOptionalFetcher).should().findCabinet(cabinetId);
+
+		assertEquals(cabinetInfo.getCabinetId(), cabinetId);
+		assertEquals(cabinetInfo.getVisibleNum(), 21);
+		assertEquals(cabinetInfo.getLentType(), LentType.PRIVATE);
+		assertEquals(cabinetInfo.getMaxUser(), 3);
+		assertEquals(cabinetInfo.getTitle(), "뭐가이렇게많냐");
+		assertEquals(cabinetInfo.getStatus(), CabinetStatus.FULL);
+		assertEquals(cabinetInfo.getStatusNote(), "THIS IS STATUS");
+		assertEquals(cabinetInfo.getLocation(), location);
+		assertEquals(cabinetInfo.getLents(), List.of(lentDto1, lentDto2, lentDto3));
+	}
+
+
+	@Test
+	@DisplayName("성공: 캐비넷 정보 조회 - NULL")
+	void 성공_NULL_getCabinetInfo() {
+		//given
+		Long cabinetId = -1L;
+
+		given(lentOptionalFetcher.findAllActiveLentByCabinetId(cabinetId))
+				.willReturn(new ArrayList<>());
+
+		CabinetInfoResponseDto cabinetInfo = cabinetFacadeService.getCabinetInfo(cabinetId);
+
+		// then
+		then(lentOptionalFetcher).should().findAllActiveLentByCabinetId(cabinetId);
+		then(lentMapper).should(times(0)).toLentDto(any(), any());
+		then(cabinetOptionalFetcher).should().findCabinet(cabinetId);
+		assertNull(cabinetInfo);
 	}
 
 	@Test
