cb84e872557427caf49b04e0569ec6ad70a2988e
moonseonghui <msh9980@naver.com>
2023-07-12 13:33:03 +0900

    [FE]:FIX  좌우 버튼 추가_수정 필요

--- frontend/src/components/LentLog/AdminCabinetLentLog.tsx
+++ frontend/src/components/LentLog/AdminCabinetLentLog.tsx
@@ -20,7 +20,11 @@ const AdminCabinetLentLog = ({
           type="prev"
           onClick={onClickPrev}
         >
-          이전
+          <ImgCenterStyled>
+            <ImageStyled>
+              <img src="/src/assets/images/LeftSectionButton.svg" alt="" />
+            </ImageStyled>
+          </ImgCenterStyled>
         </PageButtonStyled>
         <PageButtonStyled
           page={page}
@@ -28,12 +32,35 @@ const AdminCabinetLentLog = ({
           type="next"
           onClick={onClickNext}
         >
-          다음
+          <ImgCenterStyled>
+            <ImageStyled>
+              <img src="/src/assets/images/LeftSectionButton.svg" alt="" />
+            </ImageStyled>
+          </ImgCenterStyled>
         </PageButtonStyled>
       </ButtonContainerStyled>
     </AdminLentLogStyled>
   );
 };
+
+const AdminLentLogStyled = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const ButtonContainerStyled = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.3;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  overflow: hidden;
+`;
 
 const PageButtonStyled = styled.div<{
   page: number;
@@ -41,8 +68,14 @@ const PageButtonStyled = styled.div<{
   type: string;
 }>`
   cursor: pointer;
-  color: var(--main-color);
+  width: 40px;
+  height: 100%;
+  border-radius: 10px;
   position: absolute;
+  background: linear-gradient(to left, transparent, rgba(0, 0, 0, 0.7));
+  /*&:hover {
+    background: linear-gradient(to left, transparent, rgba(0, 0, 0, 0.5));
+  }*/
   display: ${({ page, totalPage, type }) => {
     if (type == "prev" && page == 0) return "none";
     if (type == "next" && (totalPage == 0 || page == totalPage - 1))
@@ -56,22 +89,23 @@ const PageButtonStyled = styled.div<{
         `
       : css`
           right: 0;
+          transform: rotate(-180deg);
         `}
 `;
 
-const ButtonContainerStyled = styled.div`
-  position: relative;
-  width: 80%;
-  height: 50px;
+const ImgCenterStyled = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  margin: 0 auto;
-  margin-top: 25px;
+  justify-content: center;
+  height: 100%;
+  //padding-right: 15px;
 `;
 
-const AdminLentLogStyled = styled.div`
-  width: 100%;
+const ImageStyled = styled.div`
+  width: 40px;
+  margin-right: 4px;
+  filter: brightness(0%);
+  border-radius: 50%;
 `;
 
 export default AdminCabinetLentLog;
--- frontend/src/components/LentLog/LogTable/AdminCabinetLogTable.tsx
+++ frontend/src/components/LentLog/LogTable/AdminCabinetLogTable.tsx
@@ -76,12 +76,14 @@ const TheadStyled = styled.thead`
 
 const TbodyStyled = styled.tbody`
   & > tr {
+    //font-size: 11px;
     text-align: center;
     height: 50px;
   }
   & > tr > td {
     height: 50px;
     line-height: 50px;
+    //padding: 0 22px;
     width: 33.3%;
   }
   & > tr:nth-child(2n) {
