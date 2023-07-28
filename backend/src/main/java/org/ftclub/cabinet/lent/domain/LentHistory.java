9fb6c5426b1dc18cd6d897636b44c3a4d0591e09
donggyu <leedong1848@gmail.com>
2023-07-28 16:28:09 +0900

    hotfix

--- backend/src/main/java/org/ftclub/cabinet/lent/domain/LentHistory.java
+++ backend/src/main/java/org/ftclub/cabinet/lent/domain/LentHistory.java
@@ -163,7 +163,7 @@ public class LentHistory {
      * @return 설정이 되어있으면 true 아니면 false
      */
     public boolean isSetExpiredAt() {
-        return (getExpiredAt() != null || getExpiredAt() != DateUtil.getInfinityDate());
+        return (getExpiredAt() != DateUtil.getInfinityDate() || getExpiredAt() != null);
     }
 
     /**
