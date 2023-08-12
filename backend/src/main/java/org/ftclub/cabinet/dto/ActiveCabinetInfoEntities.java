1ef73e6af1c9b45c6d434a129189d042a6734394
space <enaenen@naver.com>
2023-08-12 18:42:55 +0900

    [BE] Feature: 캐시 추가 & DSL Repository 추가

--- backend/src/main/java/org/ftclub/cabinet/dto/ActiveCabinetInfoEntities.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/ActiveCabinetInfoEntities.java
@@ -1,7 +1,6 @@
 package org.ftclub.cabinet.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import org.ftclub.cabinet.cabinet.domain.Cabinet;
@@ -9,7 +8,6 @@ import org.ftclub.cabinet.lent.domain.LentHistory;
 import org.ftclub.cabinet.user.domain.User;
 
 @Getter
-//@AllArgsConstructor
 @ToString
 public class ActiveCabinetInfoEntities {
 	private final Cabinet cabinet;
