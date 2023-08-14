ec2595c7d5b2beb45af0bc4415b71378c1e2f939
sichoi42 <42.4.sichoi@gmail.com>
2023-08-14 19:31:19 +0900

    [BE] FIX: contextLoads 테스트코드 diabled

--- backend/src/test/java/org/ftclub/cabinet/CabinetApplicationTests.java
+++ backend/src/test/java/org/ftclub/cabinet/CabinetApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class CabinetApplicationTests {
 
 	@Autowired
