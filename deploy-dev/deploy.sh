ff7ce7e04df1f555382385c0fa801a11361143ca
sichoi42 <42.4.sichoi@gmail.com>
2023-08-28 16:32:01 +0900

    [COMMON] FIX: deploy 스크립트 수정

--- deploy-dev/deploy.sh
+++ deploy-dev/deploy.sh
@@ -3,10 +3,7 @@
 mkdir -p /home/ec2-user/deploy/zip
 cd /home/ec2-user/deploy/zip/
 
-docker stop cabi_dev
-docker rm cabi_dev
-docker rmi cabi_dev
+cd pinpoint-application
 
-docker build -t cabi_dev ./          # Docker Image 생성
-mkdir -p /home/ec2-user/logs
-docker run --name cabi_dev -d -p 4242:4242 -v /home/ec2-user/logs:/logs cabi_dev  # Docker Container 생성
+docker compose down --rmi all
+docker compose up -d
