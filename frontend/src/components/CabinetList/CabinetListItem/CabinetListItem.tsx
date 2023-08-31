cab9fab7a25c8d5a9c718ab54a3ac6c2dd5afe0d
moonseonghui <msh9980@naver.com>
2023-08-31 13:46:12 +0900

    [FE] DESIGN pending_box_shadow 삭제

--- frontend/src/components/CabinetList/CabinetListItem/CabinetListItem.tsx
+++ frontend/src/components/CabinetList/CabinetListItem/CabinetListItem.tsx
@@ -180,7 +180,6 @@ const CabinetListItemStyled = styled.div<{
     status === "PENDING" &&
     css`
       border: 2px solid var(--main-color);
-      box-shadow: 0 -0.2em 1em #9747ff45;
     `}
     
   .cabinetLabelTextWrap {
--- frontend/src/components/LeftNav/CabinetColorTable/CabinetColorTable.tsx
+++ frontend/src/components/LeftNav/CabinetColorTable/CabinetColorTable.tsx
@@ -22,10 +22,10 @@ const CabinetColorTable = () => {
     <ColorTableStyled>
       <ColorTableItemContainer color={"var(--mine)"} text={"내 사물함"} />
       <ColorTableItemContainer color={"var(--available)"} text={"사용가능"} />
+      <ColorTableItemContainer color={"var(--pending)"} text={"오픈예정"} />
       <ColorTableItemContainer color={"var(--full)"} text={"사용 중"} />
       <ColorTableItemContainer color={"var(--expired)"} text={"반납지연"} />
       <ColorTableItemContainer color={"var(--banned)"} text={"사용불가"} />
-      <ColorTableItemContainer color={"var(--pending)"} text={"오픈예정"} />
     </ColorTableStyled>
   );
 };
