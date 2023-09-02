86c6303961e644ff7455f65debe0f1bdfe4ec72f
donggyu <leedong1848@gmail.com>
2023-09-02 14:49:19 +0900

    [BE] main 명령어 추가

--- cron/src/main/kotlin/Main.kt
+++ cron/src/main/kotlin/Main.kt
@@ -1,10 +1,19 @@
-import jobs.blackhole.BlackholeChecker
+import jobs.FtTokenFetcher
+import jobs.blackhole.BlackholeUpdater
 import kotlin.system.exitProcess
 
+private val mapping = mapOf(
+    "update-blackhole" to BlackholeUpdater::create,
+    "get-ft-token" to FtTokenFetcher::create,
+)
+
 fun main(args: Array<String>) {
-    println("Start!")
+    if (args.size != 1) {
+        println("실행하려는 명령어를 입력하세요")
+        exitProcess(1)
+    }
     try {
-        BlackholeChecker.create().sprint().let { println(it) }
+        mapping[args[0]]?.let { it().sprint() }?: throw Exception("해당 명령어는 존재하지 않습니다.")
     } catch (e: Exception) {
         println(e.message)
         exitProcess(1)
