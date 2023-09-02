ae926c6fbe7e35a190f1c3b20bc4bf033f39b858
donggyu <leedong1848@gmail.com>
2023-09-02 15:23:34 +0900

    [BE] FIX: 파싱 고침

--- cron/src/main/kotlin/jobs/blackhole/CursusUsersDeserializer.kt
+++ cron/src/main/kotlin/jobs/blackhole/CursusUsersDeserializer.kt
@@ -1,8 +1,11 @@
 package jobs.blackhole
 
 import com.fasterxml.jackson.databind.JsonNode
+import mu.KotlinLogging
 import java.time.LocalDateTime
 import java.time.format.DateTimeParseException
+
+private val log = KotlinLogging.logger {}
 
 private const val PROFILE = "user"
 private const val USERNAME = "login"
@@ -10,16 +13,17 @@ private const val EMAIL = "email"
 private const val BLACKHOLED_AT = "blackholed_at"
 
 object CursusUsersDeserializer {
-    fun toUsers(node: JsonNode): List<UserProfile> {
+    fun toUsers(node: List<JsonNode>): List<UserProfile> {
+        log.info { "called toUsers" }
         return node.map { toUser(it) }
     }
 
     fun toUser(node: JsonNode): UserProfile {
-        val profile = node.get(PROFILE)
+        log.info { "called toUser username: ${node[PROFILE][USERNAME]}" }
         return UserProfile(
-            name = profile.get(USERNAME).asText(),
-            email = profile.get(EMAIL).asText(),
-            blackholedAt = parseDate(profile.get(BLACKHOLED_AT).asText())
+            name = node[PROFILE][USERNAME].asText(),
+            email = node[PROFILE][EMAIL].asText(),
+            blackholedAt = parseDate(node[BLACKHOLED_AT].asText())
         )
     }
 
