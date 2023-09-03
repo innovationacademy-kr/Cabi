0c743ff27449bdb3d0bdb97785ec4aaa12199bae
donggyu <leedong1848@gmail.com>
2023-09-03 20:17:56 +0900

    [BE] COMMENT: 주석 추가

--- cron/src/main/kotlin/http/OkhttpExtension.kt
+++ cron/src/main/kotlin/http/OkhttpExtension.kt
@@ -12,19 +12,35 @@ private val TOTAL_INDEX = "X-Total"
 private val PAGE_INDEX = "X-Page"
 private val PAGE_SIZE_INDEX = "X-Per-Page"
 
+/**
+ * OkhttpExtension
+ * - token을 Authorization header에 추가한다.
+ */
 fun Request.Builder.addToken(token: String): Request.Builder =
     this.header(AUTHORIZATION, BEARER_PREFIX + token)
 
+/**
+ * OkhttpExtension
+ * - response header에서 total page를 가져온다. (X-Total header를 이용)
+ */
 fun Response.ftGetTotalPages(): Int {
     val total = this.header(TOTAL_INDEX)?.toInt() ?: throw Exception("total page is not found")
     val perPage = this.header(PAGE_SIZE_INDEX)?.toInt() ?: throw Exception("per page is not found")
     return total / perPage + 1
 }
 
+/**
+ * OkhttpExtension
+ * - response header에서 current page를 가져온다. (X-Page header를 이용)
+ */
 fun Response.ftGetCurrentPage(): Int {
     return this.header(PAGE_INDEX)?.toInt() ?: throw Exception("current page is not found")
 }
 
+/**
+ * OkhttpExtension
+ * - response가 401(Unauthorized)인지 확인한다.
+ */
 fun Response.isUnauthorized(): Boolean {
     return this.code() == 401
 }
