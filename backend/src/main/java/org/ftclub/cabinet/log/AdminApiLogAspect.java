b913f1470c0ec47dbdd69ec1da6378f3578c58ea
jiwon <jwpark0119@gmail.com>
2023-09-15 16:38:15 +0900

    [BE] FEAT: admin api AOP 구현 & LogPaser 적용

--- backend/src/main/java/org/ftclub/cabinet/log/AdminApiLogAspect.java
+++ backend/src/main/java/org/ftclub/cabinet/log/AdminApiLogAspect.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.ftclub.cabinet.auth.domain.AuthGuard;
@@ -31,21 +32,39 @@ public class AdminApiLogAspect {
 	private final CookieManager cookieManager;
 	private final TokenValidator tokenValidator;
 	private final JwtProperties jwtProperties;
+	private final LogParser logParser;
 
 	@AfterReturning(
 			pointcut = "@annotation(authGuard) && !@annotation(org.springframework.web.bind.annotation.GetMapping))",
-			returning = "ret", argNames = "authGuard,joinPoint,ret")
-	public void adminApiSuccessLog(AuthGuard authGuard, JoinPoint joinPoint, Object ret)
+			returning = "ret", argNames = "joinPoint,authGuard,ret")
+	public void adminApiSuccessLog(JoinPoint joinPoint, AuthGuard authGuard, Object ret)
 			throws JsonProcessingException {
-		System.out.println("AdminApiLogAspect.adminApiSuccessLog");
 		if (authGuard.level().equals(AuthLevel.USER_ONLY) || authGuard.level().equals(AuthLevel.USER_OR_ADMIN)) {
 			return;
 		}
+		String responseString = (ret == null) ? "void" : ret.toString();
+		sendLogMessage(joinPoint, responseString);
+	}
+
+	@AfterThrowing(
+			pointcut = "@annotation(authGuard) && !@annotation(org.springframework.web.bind.annotation.GetMapping))",
+			throwing = "exception", argNames = "joinPoint,authGuard, exception")
+	public void adminApiThrowingLog(JoinPoint joinPoint, AuthGuard authGuard, Exception exception)
+			throws JsonProcessingException {
+		if (authGuard.level().equals(AuthLevel.ADMIN_ONLY) || authGuard.level().equals(AuthLevel.MASTER_ONLY)) {
+			return;
+		}
+		String responseString = exception.getClass().getName() + ":{" + exception.getMessage() + "}";
+		sendLogMessage(joinPoint, responseString);
+	}
+
+	private void sendLogMessage(JoinPoint joinPoint, String responseString)
+			throws JsonProcessingException {
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
 				.getRequest();
 
 		String name = tokenValidator.getPayloadJson(
-						cookieManager.getCookieValue(request, jwtProperties.getMainTokenName())).get("name")
+						cookieManager.getCookieValue(request, jwtProperties.getAdminTokenName())).get("email")
 				.asText();
 		Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
 		String className = joinPoint.getTarget().getClass().getName();
@@ -70,21 +89,8 @@ public class AdminApiLogAspect {
 			sb.setLength(sb.length() - 1);
 			sb.append("#");
 		}
-		// 어떤 결과를 반환 했는지
-//		if (ret != null) {
-//			sb.append(ret.toString());
-//		} else {
-//			sb.append("void");
-//		}
-
-		String result = sb.toString();
-		System.out.println(result);
+		String message = sb.toString();
+		logParser.parseToDiscordAlarmMessage(message);
+		log.info(message);
 	}
-
-//	@AfterThrowing
-//	public void adminApiThrowingLog(
-//			AuthGuard authGuard,
-//			JoinPoint joinPoint,
-//			Object ret) {
-//	}
 }
