883fc82beaec26542607ccb99253ee84dab47896
Woo Joo Chae <enaenen@naver.com>
2023-09-22 22:25:15 +0900

    [BE] TEST: Disable SpringBootTest

--- backend/src/test/java/org/ftclub/cabinet/CabinetApplicationTests.java
+++ backend/src/test/java/org/ftclub/cabinet/CabinetApplicationTests.java
@@ -3,12 +3,14 @@ package org.ftclub.cabinet;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.ftclub.cabinet.cabinet.domain.Cabinet;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@Disabled
 class CabinetApplicationTests {
 
 	@PersistenceContext
