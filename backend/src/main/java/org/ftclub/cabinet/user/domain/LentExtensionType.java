a2102cf9d05af5dc1319b68308affa9c4d76d6b2
Woo Joo Chae <enaenen@naver.com>
2023-10-13 23:54:40 +0900

    [BE]REFACTOR : 줄정리

--- backend/src/main/java/org/ftclub/cabinet/user/domain/LentExtensionType.java
+++ backend/src/main/java/org/ftclub/cabinet/user/domain/LentExtensionType.java
@@ -4,9 +4,9 @@ package org.ftclub.cabinet.user.domain;
  * 연장권 종류
  */
 public enum LentExtensionType {
-	ALL, PRIVATE_ONLY, SHARE_ONLY;
+    ALL, PRIVATE_ONLY, SHARE_ONLY;
 
-	public boolean isValid() {
-		return this.equals(ALL) || this.equals(PRIVATE_ONLY) || this.equals(SHARE_ONLY);
-	}
+    public boolean isValid() {
+        return this.equals(ALL) || this.equals(PRIVATE_ONLY) || this.equals(SHARE_ONLY);
+    }
 }
