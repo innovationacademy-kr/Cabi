733e919c728eb16bdfb13eaa582763e94f78ca9f
Ssuamje <side2850@naver.com>
2023-10-19 17:58:03 +0900

    [BE] DOCS: TODO 추가

--- backend/src/main/java/org/ftclub/cabinet/alarm/domain/LentExpirationAlarm.java
+++ backend/src/main/java/org/ftclub/cabinet/alarm/domain/LentExpirationAlarm.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
  */
 @AllArgsConstructor
 public class LentExpirationAlarm implements Alarm {
+	//TODO : CabinetPlace는 Dto로 전환이 필요함(엔티티임)
 	private final CabinetPlace cabinetPlace;
 	private final LocalDateTime lentExpirationDate;
 	private final int daysAfterExpiration;
