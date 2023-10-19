7cfe3be5806079b661052b5ae1754cfa5dead10f
Woo Joo Chae <enaenen@naver.com>
2023-10-19 20:50:25 +0900

    [BE] FEAT: Slack api management added

--- backend/src/main/java/org/ftclub/cabinet/alarm/slack/SlackApiManager.java
+++ backend/src/main/java/org/ftclub/cabinet/alarm/slack/SlackApiManager.java
@@ -1,6 +1,11 @@
 package org.ftclub.cabinet.alarm.slack;
 
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import feign.FeignException.FeignClientException;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.ftclub.cabinet.alarm.slack.config.SlackProperties;
@@ -18,7 +23,6 @@ public class SlackApiManager {
     private final SlackProperties slackProperties;
     private final SlackFeignClient slackFeignClient;
 
-
     public SlackUserInfo requestSlackUserInfo(String email) {
         log.info("Called requestSlackUserInfo email={}", email);
         try {
@@ -33,8 +37,23 @@ public class SlackApiManager {
             return slackResponse.getSlackUserInfo();
         } catch (FeignClientException e) {
             log.error("{}", e.getMessage());
-            throw new ServiceException(ExceptionStatus.OAUTH_BAD_GATEWAY);
+            throw new ServiceException(ExceptionStatus.SLACK_BAD_GATEWAY);
         }
     }
 
+    public void sendMessage(String channelId, String message) {
+        log.info("Called sendMessage channelId={}, message={}", channelId, message);
+        try {
+            MethodsClient methods = Slack.getInstance().methods(slackProperties.getAppToken());
+
+            ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                    .channel(channelId) // DM & channel
+                    .text(message)
+                    .build();
+            methods.chatPostMessage(request);
+        } catch (SlackApiException | IOException e) {
+            log.error("{}", e.getMessage());
+            throw new ServiceException(ExceptionStatus.SLACK_BAD_GATEWAY);
+        }
+    }
 }
