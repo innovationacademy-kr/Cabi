1452cb84d994d3e253b5fe629807c3de2cc95a26
Woo Joo Chae <enaenen@naver.com>
2023-10-19 19:58:53 +0900

    [BE] FEAT: feignclient enable

--- backend/src/main/java/org/ftclub/cabinet/CabinetApplication.java
+++ backend/src/main/java/org/ftclub/cabinet/CabinetApplication.java
@@ -1,18 +1,20 @@
-	package org.ftclub.cabinet;
+package org.ftclub.cabinet;
 
 import org.ftclub.cabinet.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
+@EnableFeignClients
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Import({CorsConfig.class})
 // @CrossOrigin(origins = {"*"}, allowedHeaders = {"*"}, originPatterns = {"*"})
 public class CabinetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CabinetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetApplication.class, args);
+    }
 
 }
