b6bd46477a441398256f30f809a45dfd5e1e2e85
sichoi42 <42.4.sichoi@gmail.com>
2023-11-07 20:09:56 +0900

    [BE] FIX: FCM 기능 restore

---
+++ backend/src/main/java/org/ftclub/cabinet/firebase/FCMInitializer.java
@@ -0,0 +1,43 @@
+package org.ftclub.cabinet.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FCMInitializer {
+
+
+	@Value("${firebase.messaging.credentials.path}")
+	private String credentialsPath;
+	private final ResourceLoader resourceLoader;
+
+	@PostConstruct
+	public void initialize() throws IOException {
+		Path currentPath = Paths.get("").toAbsolutePath().normalize();
+		Resource resource = resourceLoader.getResource("file:" + currentPath + credentialsPath);
+		try (InputStream inputStream = resource.getInputStream()) {
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(inputStream))
+					.build();
+			if (FirebaseApp.getApps().isEmpty()) {
+				FirebaseApp.initializeApp(options);
+				log.info("Firebase application has been initialized");
+			}
+		}
+	}
+}
+
