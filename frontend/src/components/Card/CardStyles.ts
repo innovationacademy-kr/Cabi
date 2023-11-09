f918faf140f9cd6fe6efb85216d0f7c4e3c8ee48
jusohn <jusohn@student.42seoul.kr>
2023-11-09 10:28:51 +0900

    [FE] REFACTOR: CardStyles 에서 Card 관련 styled 를 Card 로 이전

--- frontend/src/components/Card/CardStyles.ts
+++ frontend/src/components/Card/CardStyles.ts
@@ -1,59 +1,5 @@
 import styled from "styled-components";
 
-export const CardStyled = styled.div<{
-  width: string;
-  height: string;
-  gridArea: string;
-}>`
-  width: ${(props) => props.width};
-  border-radius: 10px;
-  background-color: var(--lightgary-color);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: ${(props) => props.height};
-  grid-area: ${(props) => props.gridArea};
-`;
-
-export const CardHeaderStyled = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 20px 10px 30px;
-`;
-
-export const CardTitleStyled = styled.div`
-  font-size: 1.2em;
-  font-weight: bold;
-  margin-right: auto;
-`;
-
-export const CardButtonWrapper = styled.div`
-  display: flex;
-`;
-
-export const CardButtonStyled = styled.div<{
-  backgroundColor?: string;
-  color?: string;
-  isClickable?: boolean;
-  isExtensible?: boolean;
-}>`
-  background-color: ${(props) =>
-    props.backgroundColor ? props.backgroundColor : "var(--white)"};
-  color: ${(props) =>
-    props.color
-      ? props.color
-      : props.isExtensible
-      ? "var(--main-color)"
-      : "var(--gray-color)"};
-  padding: 5px 15px;
-  border: none;
-  border-radius: 5px;
-  cursor: ${(props) => (props.isClickable ? "pointer" : "default")};
-  margin-left: 10px;
-`;
-
 export const CardContentWrapper = styled.div`
   background-color: var(--white);
   border-radius: 10px;
