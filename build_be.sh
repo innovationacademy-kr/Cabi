7e1e777b234d5dc4c1e5e7dc03a2076734d02009
sichoi42 <42.4.sichoi@gmail.com>
2023-11-20 10:26:44 +0900

    [BE] FIX: build_be.sh 프로젝트 루트에서 실행되도록 변경

--- build_be.sh
+++ build_be.sh
@@ -8,9 +8,7 @@ CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 RESET='\033[0m'
 
-echo -e $GREEN"이 스크립트는 ./backend 디렉토리에서 실행되어야 합니다!!"
-echo -e $YELLOW"노션에 있는 application.yml 파일들을 전부 올바른 디렉토리에 넣어주세요."
-echo -e $GREEN".env 파일 또한 필요합니다!"
+echo -e $GREEN"이 스크립트는 까비 프로젝트 홈 디렉토리에서 실행되어야 합니다."
 echo -e $RESET"1초 뒤 실행됩니다."
 sleep 1
 
@@ -19,30 +17,24 @@ arg=$1
 echo -en $CYAN
 if [ "$arg" == "docker" ] || [ "$arg" == "all" ]; then
     echo "Local DB Compose Down"
-    docker compose down
+    docker compose -f ./backend/docker-compose.yaml down
 
     echo "Local DB Compose Up with daemon"
-    docker compose up --build -d
-
-    echo "Test DB Compose Down"
-    cd ./src/test/resources/
-    docker compose down
-
-    echo "Test DB Compose Up with daemon"
-    docker compose up --build -d
-    cd ../../../
+    docker compose -f ./backend/docker-compose.yaml up --build -d
 fi
 
 echo -en $CYAN
 if [ "$arg" == "build" ] || [ "$arg" == "re" ] || [ "$arg" == "all" ]; then
     echo "Build with Gradle"
+    cd backend/
     ./gradlew build -x test
+    cd ..
 fi
 
 echo -en $CYAN
 if [ "$arg" == "run" ] || [ "$arg" == "re" ] || [ "$arg" == "all" ]; then
     echo "Spring Boot Run"
-    java -jar -Dspring.profiles.active=local ./build/libs/cabinet-0.0.1-SNAPSHOT.jar
+    java -jar -Dspring.profiles.active=local ./backend/build/libs/cabinet-0.0.1-SNAPSHOT.jar
 fi
 
 echo -en $RESET
