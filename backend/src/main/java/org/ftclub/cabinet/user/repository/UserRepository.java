80a6ad4a7d60bc15f4b8f82d8bb36a42af17ae95
Woo Joo Chae <enaenen@naver.com>
2023-12-21 17:33:06 +0900

    [BE] FIX: AlarmOptOut 레코드가 없는 상태에서 User 를 가져오도록 수정

--- backend/src/main/java/org/ftclub/cabinet/user/repository/UserRepository.java
+++ backend/src/main/java/org/ftclub/cabinet/user/repository/UserRepository.java
@@ -56,14 +56,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	 */
 	Page<User> findAllByRoleAndDeletedAtIsNull(@Param("role") UserRole role, Pageable pageable);
 
-	 /**
+	/**
 	 * 블랙홀에 빠질 위험이 있는 유저들의 정보를 조회합니다. blackholedAt이 현재 시간보다 과거인 유저들을 블랙홀에 빠질 위험이 있는 유저로 판단합니다.
 	 *
 	 * @return {@link User} 리스트
 	 */
 	@Query("SELECT u FROM User u WHERE u.blackholedAt IS NOT NULL OR u.blackholedAt <= CURRENT_TIMESTAMP")
 	List<User> findByRiskOfFallingIntoBlackholeUsers();
-	
+
 	/**
 	 * 블랙홀에 빠질 위험이 없는 유저들의 정보를 조회합니다. blackholedAt이 null이거나 현재 시간보다 미래인 유저들을 블랙홀에 빠질 위험이 없는 유저로
 	 * 판단합니다.
@@ -77,7 +77,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	 * 유저의 id로 OptOut 테이블과 결합된 유저 정보를 찾습니다.
 	 */
 	@Query("SELECT u FROM User u "
-			+ "JOIN AlarmOptOut o ON u.userId = o.user.userId "
+			+ "LEFT JOIN AlarmOptOut o ON u.userId = o.user.userId "
 			+ "WHERE u.userId = :id")
 	Optional<User> findUserWithOptOutById(@Param("id") Long id);
 
