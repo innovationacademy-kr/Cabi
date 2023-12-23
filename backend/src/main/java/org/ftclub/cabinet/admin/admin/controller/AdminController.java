9e10cafa6b056bd17f1da77ad52ebfc2d4eab32d
Ssuamje <side2850@naver.com>
2023-12-23 17:09:16 +0900

    fix: admin promote 라우트 지정

--- backend/src/main/java/org/ftclub/cabinet/admin/admin/controller/AdminController.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/admin/controller/AdminController.java
@@ -3,8 +3,9 @@ package org.ftclub.cabinet.admin.admin.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.ftclub.cabinet.admin.admin.service.AdminFacadeService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,6 +19,13 @@ public class AdminController {
 
 	private final AdminFacadeService adminFacadeService;
 
-	public void promoteUserToAdmin(@RequestParam("email") String email) {
+	/**
+	 * 유저를 관리자로 승격시킵니다.
+	 *
+	 * @param email 유저의 이메일
+	 */
+	@PostMapping("/{email}/promote")
+	public void promoteUserToAdmin(@PathVariable String email) {
+		adminFacadeService.promoteAdminByEmail(email);
 	}
 }
