3bb643322525806bd87f365064ba2ade15499e37
space <enaenen@naver.com>
2024-01-02 02:25:32 +0900

    [BE] HOTFIX: 연장권 발급로직 수동

--- backend/src/main/java/org/ftclub/cabinet/ping/PingController.java
+++ backend/src/main/java/org/ftclub/cabinet/ping/PingController.java
@@ -1,14 +1,24 @@
 package org.ftclub.cabinet.ping;
 
+import lombok.RequiredArgsConstructor;
+import org.ftclub.cabinet.user.service.LentExtensionService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ping")
+@RequiredArgsConstructor
 public class PingController {
+	private final LentExtensionService lentExtensionService;
 
 	@RequestMapping
 	public String ping() {
 		return "pong";
+	}
+
+	@RequestMapping("/pong")
+	public String ok(){
+		lentExtensionService.issueLentExtension();
+		return "ok";
 	}
 }
