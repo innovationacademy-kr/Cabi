21155472bf7fda780789ef823ccc03685c40f34f
Ssuamje <side2850@naver.com>
2024-01-09 21:36:51 +0900

    DOCS : AuthPolicyService 주석 추가

--- backend/src/main/java/org/ftclub/cabinet/auth/service/AuthPolicyService.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/service/AuthPolicyService.java
@@ -5,12 +5,22 @@ import org.ftclub.cabinet.config.DomainProperties;
 import org.ftclub.cabinet.config.MasterProperties;
 import org.springframework.stereotype.Service;
 
+/**
+ * 인증 정책을 관리하는 서비스
+ */
 @Service
 @RequiredArgsConstructor
 public class AuthPolicyService {
 	private final MasterProperties masterProperties;
 	private final DomainProperties domainProperties;
 
+	/**
+	 * 마스터 계정 정보와 일치하는지 확인
+	 *
+	 * @param id       아이디
+	 * @param password 비밀번호
+	 * @return 일치 여부
+	 */
 	public boolean isMatchWithMasterAuthInfo(String id, String password) {
 		return masterProperties.getId().equals(id)
 				&& masterProperties.getPassword().equals(password);
