e41960c03c78ad68c0aad79f44634f5eb1dbb68c
space <enaenen@naver.com>
2024-01-15 14:12:30 +0900

    [BE] HOTFIX: AuthAspect 500 -> 401 error

--- backend/src/main/java/org/ftclub/cabinet/auth/domain/AuthAspect.java
+++ backend/src/main/java/org/ftclub/cabinet/auth/domain/AuthAspect.java
@@ -1,6 +1,13 @@
 package org.ftclub.cabinet.auth.domain;
 
+import static org.ftclub.cabinet.auth.domain.AuthLevel.ADMIN_ONLY;
+import static org.ftclub.cabinet.auth.domain.AuthLevel.MASTER_ONLY;
+import static org.ftclub.cabinet.auth.domain.AuthLevel.USER_ONLY;
+import static org.ftclub.cabinet.auth.domain.AuthLevel.USER_OR_ADMIN;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,11 +19,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import static org.ftclub.cabinet.auth.domain.AuthLevel.*;
 
 /**
  * {@link AuthGuard} 어노테이션이 붙은 메소드나 클래스에 대해 인증을 검사하는 클래스입니다.
@@ -64,27 +66,29 @@ public class AuthAspect {
 			case ADMIN_ONLY:
 				if (!tokenValidator.isValidTokenWithLevel(token, ADMIN_ONLY)) {
 					cookieManager.deleteCookie(response, adminTokenName);
-					throw ExceptionStatus.UNAUTHORIZED_ADMIN.asServiceException();
+					throw ExceptionStatus.UNAUTHORIZED_ADMIN.asControllerException();
 				}
 				break;
 			case USER_ONLY:
 				if (!tokenValidator.isValidTokenWithLevel(token, USER_ONLY)) {
 					cookieManager.deleteCookie(response, mainTokenName);
-					throw ExceptionStatus.UNAUTHORIZED_USER.asServiceException();
+					throw ExceptionStatus.UNAUTHORIZED_USER.asControllerException();
 				}
 				break;
 			case USER_OR_ADMIN:
 				if (!tokenValidator.isValidTokenWithLevel(token, USER_OR_ADMIN)) {
 					cookieManager.deleteCookie(response, mainTokenName);
 					cookieManager.deleteCookie(response, adminTokenName);
-					throw ExceptionStatus.UNAUTHORIZED.asServiceException();
+					throw ExceptionStatus.UNAUTHORIZED.asControllerException();
 				}
 				break;
 			case MASTER_ONLY:
 				if (!tokenValidator.isValidTokenWithLevel(token, MASTER_ONLY)) {
 					cookieManager.deleteCookie(response, adminTokenName);
-					throw ExceptionStatus.UNAUTHORIZED_ADMIN.asServiceException();
+					throw ExceptionStatus.UNAUTHORIZED_ADMIN.asControllerException();
 				}
+			default:
+				throw ExceptionStatus.UNAUTHORIZED.asControllerException();
 		}
 	}
 
