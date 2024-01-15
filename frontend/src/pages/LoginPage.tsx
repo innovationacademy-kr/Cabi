933cecc9c2fee4307caa06e95d7bd802f8515479
sichoi42 <42.4.sichoi@gmail.com>
2024-01-15 18:41:09 +0900

    [COMMON] FIX: 로그인 후 콜백 주소를 프론트에서 결정하도록 변경

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,11 +2,12 @@ import LoginTemplate from "@/components/Login/LoginTemplate";
 import "@/assets/css/loginPage.css";
 
 const LoginPage = () => {
+  const ORIGIN_URL = window.location.origin;
   const url = `${import.meta.env.VITE_BE_HOST}/v4/auth/login`;
 
   return (
     <LoginTemplate
-      url={url}
+      url={`${url}?redirect=${ORIGIN_URL}/post-login`}
       pageTitle="Cabi"
       pageSubTitle="여러분의 일상을 가볍게"
       imgSrc="/src/assets/images/loginImg.svg"
