86ae296f0e429908f60024f458ce9d908064628f
jiwon <jwpark0119@gmail.com>
2024-01-20 12:42:49 +0900

    merge

--- backend/src/main/java/org/ftclub/cabinet/user/service/LentExtensionQueryService.java
+++ backend/src/main/java/org/ftclub/cabinet/user/service/LentExtensionQueryService.java
@@ -16,24 +16,27 @@ import org.springframework.stereotype.Service;
 @Logging(level = LogLevel.DEBUG)
 public class LentExtensionQueryService {
 
-    private final LentExtensionRepository lentExtensionRepository;
+	private final LentExtensionRepository lentExtensionRepository;
 
 	/**
 	 * 유저의 사용 가능한 연장권 중 사용 기한이 가장 임박한 연장권을 가져옵니다.
+	 *
 	 * @param userId
 	 * @return 사용 기한이 가장 임박한 연장권을 반환합니다.
 	 */
-    public LentExtension findActiveLentExtension(Long userId) {
+	public LentExtension findActiveLentExtension(Long userId) {
 
-        return LentExtensions.builder()
-                .lentExtensions(lentExtensionRepository.findAll(userId))
-                .build()
-                .findImminentActiveLentExtension()
-                .orElse(null);
-    }
+		List<LentExtension> lentExtensions = lentExtensionRepository.findAllByUserId(userId);
+		return LentExtensions.builder()
+				.lentExtensions(lentExtensions)
+				.build()
+				.findImminentActiveLentExtension()
+				.orElse(null);
+	}
 
 	/**
 	 * 유저의 사용 가능한 연장권을 모두 가져옵니다.
+	 *
 	 * @param userId
 	 * @return 사용 가능한 연장권을 모두 반환합니다.
 	 */
@@ -45,11 +48,12 @@ public class LentExtensionQueryService {
 
 	/**
 	 * 유저의 모든 연장권을 사용 기한 기준 최신 순서로 가져옵니다.
+	 *
 	 * @param userId
 	 * @return 사용 기한을 기준으로 최신 순서로 정렬된 모든 연장권을 반환합니다.
 	 */
 	public List<LentExtension> findLentExtensionsInLatestOrder(Long userId) {
-		return lentExtensionRepository.findAll(userId)
+		return lentExtensionRepository.findAllByUserId(userId)
 				.stream()
 				.sorted(Comparator.comparing(LentExtension::getExpiredAt,
 						Comparator.reverseOrder()))
