4c540bf1a9373baa6fbb63425e1e390c9df618a9
Woo Joo Chae <enaenen@naver.com>
2024-01-22 13:28:29 +0900

    [BE] FEAT: 동아리 관련 controller

---
+++ backend/src/main/java/org/ftclub/cabinet/club/controller/ClubController.java
@@ -0,0 +1,15 @@
+package org.ftclub.cabinet.club.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.ftclub.cabinet.log.Logging;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v4/clubs")
+@Logging
+public class ClubController {
+
+
+}
