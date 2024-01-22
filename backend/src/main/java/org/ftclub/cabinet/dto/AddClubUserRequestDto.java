53594cb36eddf3739843b032da1e9f182d9deac0
Woo Joo Chae <enaenen@naver.com>
2024-01-22 20:04:50 +0900

    [BE] FIX: 생성자 추가

--- backend/src/main/java/org/ftclub/cabinet/dto/AddClubUserRequestDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/AddClubUserRequestDto.java
@@ -1,13 +1,17 @@
 package org.ftclub.cabinet.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.ToString;
 
 @Getter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class AddClubUserRequestDto {
 
+	@NonNull
 	private String name;
 }
