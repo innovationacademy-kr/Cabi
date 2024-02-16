e254e31977260dfe7c547d3a9eb4fbe0f87e9630
jusohn <jusohn@student.42seoul.kr>
2024-02-16 16:57:29 +0900

    [FE] FIX: Available Page 롤백 #1556

--- frontend/src/pages/AvailablePage.tsx
+++ frontend/src/pages/AvailablePage.tsx
@@ -126,13 +126,7 @@ const AvailablePage = () => {
 
   return (
     <WrapperStyled>
-      <UtilsSectionStyled>
-        <MultiToggleSwitch
-          initialState={toggleType}
-          setState={setToggleType}
-          toggleList={toggleList}
-        />
-      </UtilsSectionStyled>
+      <UtilsSectionStyled></UtilsSectionStyled>
       <HeaderStyled>사용 가능 사물함</HeaderStyled>
       <SubHeaderStyled>
         <h2>
@@ -142,11 +136,22 @@ const AvailablePage = () => {
           {isRefreshing ? (
             <LoadingAnimation />
           ) : (
-            <img src="/src/assets/images/refresh.svg" alt="새로고침" />
+            <>
+              <img src="/src/assets/images/rotateRight.svg" alt="새로고침" />
+              <AvailableCountdown observeOpenTime={() => setIsOpenTime(true)} />
+            </>
           )}
         </RefreshButtonStyled>
+        {/*  */}
       </SubHeaderStyled>
-      <AvailableCountdown observeOpenTime={() => setIsOpenTime(true)} />
+      <MultiToggleSwitchStyled>
+        <MultiToggleSwitch
+          initialState={toggleType}
+          setState={setToggleType}
+          toggleList={toggleList}
+        />
+      </MultiToggleSwitchStyled>
+
       {isLoaded && cabinets ? (
         Object.entries(cabinets).map(([key, value]) => (
           <FloorContainer
@@ -201,14 +206,18 @@ const SubHeaderStyled = styled.div`
 `;
 
 const RefreshButtonStyled = styled.button`
-  margin-top: 40px;
-  background-color: initial;
-  width: 35px;
-  height: 35px;
+  margin-top: 60px;
+  width: 280px;
+  height: 47px;
+  background-color: var(--main-color);
   padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   img {
-    width: 35px;
-    height: 35px;
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
   }
   &:hover {
     opacity: 0.7;
@@ -217,6 +226,11 @@ const RefreshButtonStyled = styled.button`
     transform: scale(0.8);
   }
   transition: all 0.3s ease;
+`;
+
+const MultiToggleSwitchStyled = styled.div`
+  width: 70%;
+  margin-top: 58px;
 `;
 
 const FooterStyled = styled.footer`
