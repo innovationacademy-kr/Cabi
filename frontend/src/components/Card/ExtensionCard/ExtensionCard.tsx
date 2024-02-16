c02144e952064a19a81a3fff45ad8268ff6decbb
jusohn <jusohn@student.42seoul.kr>
2024-02-16 23:42:06 +0900

    [FE] FIX: 연장권 안내 문구 변수로 분리 #1540

--- frontend/src/components/Card/ExtensionCard/ExtensionCard.tsx
+++ frontend/src/components/Card/ExtensionCard/ExtensionCard.tsx
@@ -15,11 +15,12 @@ interface ExtensionProps {
   button: IButtonProps;
 }
 
+const NotificationModalDetail = `연장권은 매월 2일 제공되며,<br/>이전에 받은 연장권은 사용이 불가능 합니다.<br/>24HANE 기준 160시간을 출석한 경우,<br/> 연장권이 부여됩니다.`;
+
 const ExtensionCard = ({ extensionInfo, button }: ExtensionProps) => {
   const [showNotificationModal, setShowNotificationModal] =
     useState<boolean>(false);
   return (
-<<<<<<< HEAD
     <>
       <Card
         title={"연장권"}
@@ -34,56 +35,28 @@ const ExtensionCard = ({ extensionInfo, button }: ExtensionProps) => {
         <CardContentWrapper>
           <CardContentStyled>
             <ContentInfoStyled>사용 기한</ContentInfoStyled>
-            <ContentDeatilStyled>
+            <ContentDetailStyled>
               {!!extensionInfo
                 ? formatDate(new Date(extensionInfo.expiredAt), ".")
                 : "-"}
-            </ContentDeatilStyled>
+            </ContentDetailStyled>
           </CardContentStyled>
           <CardContentStyled>
             <ContentInfoStyled>연장 기간</ContentInfoStyled>
-            <ContentDeatilStyled>
+            <ContentDetailStyled>
               {!!extensionInfo ? extensionInfo.extensionPeriod + "일" : "-"}
-            </ContentDeatilStyled>
+            </ContentDetailStyled>
           </CardContentStyled>
         </CardContentWrapper>
       </Card>
       {showNotificationModal && (
         <NotificationModal
           title={"연장권 안내"}
-          detail={
-            "연장권은 매월 2일 제공되며,<br/>이전에 받은 연장권은 사용이 불가능 합니다.<br/>24HANE 기준 160시간을 출석한 경우,<br/> 연장권이 부여됩니다."
-          }
+          detail={NotificationModalDetail}
           closeModal={() => setShowNotificationModal(false)}
         />
       )}
     </>
-=======
-    <Card
-      title={"연장권"}
-      gridArea={"extension"}
-      width={"350px"}
-      height={"183px"}
-      buttons={[button]}
-    >
-      <CardContentWrapper>
-        <CardContentStyled>
-          <ContentInfoStyled>사용 기한</ContentInfoStyled>
-          <ContentDetailStyled>
-            {!!extensionInfo
-              ? formatDate(new Date(extensionInfo.expiredAt), ".")
-              : "-"}
-          </ContentDetailStyled>
-        </CardContentStyled>
-        <CardContentStyled>
-          <ContentInfoStyled>연장 기간</ContentInfoStyled>
-          <ContentDetailStyled>
-            {!!extensionInfo ? extensionInfo.extensionPeriod + "일" : "-"}
-          </ContentDetailStyled>
-        </CardContentStyled>
-      </CardContentWrapper>
-    </Card>
->>>>>>> dc7a099f69df811ffb8fb9ec7bc7e6801ac3f723
   );
 };
 
