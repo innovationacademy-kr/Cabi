ff1540793457fffa1d0d2e47c69b6f24fb016100
jnkeniaem <jeanne.kim9@gmail.com>
2024-02-21 13:20:14 +0900

    [FIX]: 1920 layout fix #1559

--- frontend/src/pages/Wednesday/DetailTable.tsx
+++ frontend/src/pages/Wednesday/DetailTable.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
+// 170px
+
 const DetailTable = () => {
   return (
     <TableStyled>
       <TableHeadStyled>
-        <th>날짜</th>
-        <th>ID</th>
-        <th>제목</th>
-        <th>시간</th>
-        <th>카테고리</th>
+        <th id="date">날짜</th>
+        <th id="intraId">ID</th>
+        <th id="title">제목</th>
+        <th id="time">시간</th>
+        <th id="category">카테고리</th>
       </TableHeadStyled>
       <TableBodyStyled>
         <td className="leftEnd">
@@ -32,12 +34,16 @@ const DetailTable = () => {
           <span>1월 20일</span>
         </td>
         <td className="rightEnd" colSpan={4} id="noEvent">
-          <span>
-            다양한 주제와 관심사를 함께 나누고 싶으신 분은 지금 바로 발표를
-            신청해보세요
-          </span>
-          <img src="/src/assets/images/happyCcabi.svg" />
-          <button>신청하기</button>
+          <div>
+            <NoEventPhraseStyled>
+              <span>
+                다양한 주제와 관심사를 함께 나누고 싶으신 분은 지금 바로 발표를
+                신청해보세요
+              </span>
+              <img src="/src/assets/images/happyCcabi.svg" />
+            </NoEventPhraseStyled>
+            <button>신청하기</button>
+          </div>
         </td>
       </TableBodyStyled>
     </TableStyled>
@@ -50,13 +56,6 @@ const TableStyled = styled.table`
   width: 100%;
   border-spacing: 0 24px;
   border-collapse: separate;
-
-  & > #noEvent {
-    background-color: white;
-    /* display: flex;
-    align-items: center;
-    justify-content: center; */
-  }
 `;
 
 const TableHeadStyled = styled.tr`
@@ -65,6 +64,26 @@ const TableHeadStyled = styled.tr`
 
   & > th {
     font-size: 20px;
+  }
+
+  & > #date {
+    width: 170px;
+  }
+
+  & > #intraId {
+    width: 180px;
+  }
+
+  & > #title {
+    width: 740px;
+  }
+
+  & > #time {
+    width: 100px;
+  }
+
+  & > #category {
+    width: 120px;
   }
 `;
 
@@ -83,19 +102,42 @@ const TableBodyStyled = styled.tr`
     background-color: white;
   }
 
+  & > td > div {
+    /* background-color: green; */
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   & button {
     width: 120px;
     height: 40px;
+    margin-left: 100px;
+    margin-right: 60px;
   }
-  & img {
-    width: 30px;
-    height: 30px;
-    margin-left: 20px;
-  }
+
   & .leftEnd {
     border-radius: 10px 0 0 10px;
   }
   & .rightEnd {
     border-radius: 0 10px 10px 0;
+  }
+`;
+
+const NoEventPhraseStyled = styled.div`
+  /* background-color: yellow; */
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 850px;
+  & > img {
+    width: 30px;
+    height: 30px;
+    margin-left: 20px;
+  }
+
+  & > span {
+    font-weight: bold;
   }
 `;
