924c17499b64eea4e0b4104a27d226a8607b643b
최지민 <choijimin@choejihyeong-ui-MacBook-Air.local>
2024-02-26 14:09:56 +0900

    [FE] FEAT: 날짜 선택 드롭다운 메뉴 수정 및 이동 #1559

---
+++ frontend/src/components/Wednesday/Registers/DropdownTimeMenu.tsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+const DropdownTimeMenu = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsVisible(false);
+        setIsFocused(false); // 이 부분이 수정되었습니다.
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  enum PresentationTime {
+    HALF = "30분",
+    HOUR = "1시간",
+    HOUR_HALF = "1시간 30분",
+    TWO_HOUR = "2시간",
+  }
+
+  const handleOptionSelect = (option: PresentationTime) => {
+    setSelectedOption(option);
+    setIsVisible(false);
+    setIsFocused(false);
+  };
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+    setIsFocused(!isVisible);
+  };
+  return (
+    <DropdownContainer ref={dropdownRef}>
+      <RegisterTimeInputStyled onClick={toggleVisibility} isFocused={isFocused}>
+        {/* <RegisterTimeInputStyled onClick={() => setIsVisible(!isVisible)}> */}
+        {selectedOption || "시간을 선택해주세요"}
+      </RegisterTimeInputStyled>
+      {isVisible && (
+        <DropdownOptions>
+          {Object.values(PresentationTime).map((time) => (
+            <DropdownOption key={time} onClick={() => handleOptionSelect(time)}>
+              {time}
+            </DropdownOption>
+          ))}
+        </DropdownOptions>
+      )}
+    </DropdownContainer>
+  );
+};
+
+const DropdownContainer = styled.div`
+  position: relative;
+`;
+
+const DropdownOptions = styled.ul`
+  margin-top: 10px;
+  position: absolute;
+  top: 52px;
+  left: 0;
+  width: 310px;
+  border: 1px solid var(--white);
+  border-radius: 10px;
+  text-align: left;
+  padding: 10px;
+  color: var(--black);
+  background-color: var(--white);
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const DropdownOption = styled.li`
+  font-size: 0.9rem;
+  color: var(--gray-color);
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #f0f0f0;
+    color: var(--main-color);
+  }
+`;
+
+const RegisterTimeInputStyled = styled.div<{ isFocused: boolean }>`
+  height: 46px;
+  width: 310px;
+  border-radius: 10px;
+  background-color: var(--white);
+  border: 2px solid
+    ${(props) => (props.isFocused ? "var(--main-color)" : "var(--white)")}; // 이 부분이 수정되었습니다.
+  resize: none;
+  outline: none;
+  color: var(--gray-color);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: 0.9rem;
+  padding-left: 10px;
+`;
+
+export default DropdownTimeMenu;
