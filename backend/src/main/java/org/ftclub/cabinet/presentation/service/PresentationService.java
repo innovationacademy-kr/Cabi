c3728587984a1513241aed426b7d09a4f5b54f92
saewoo1 <wooozooo1123@icloud.com>
2024-02-26 20:14:49 +0900

    [BE] FEAT: 신청서 저장 기능

--- backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationService.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.dto.PresentationFormRequestDto;
 import org.ftclub.cabinet.presentation.domain.Presentation;
 import org.ftclub.cabinet.presentation.repository.PresentationRepository;
+import org.ftclub.cabinet.user.domain.User;
+import org.ftclub.cabinet.user.service.UserQueryService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +14,25 @@ import org.springframework.stereotype.Service;
 public class PresentationService {
 
 	private final PresentationRepository presentationRepository;
+	private final PresentationPolicyService presentationPolicyService;
+	private final UserQueryService userQueryService;
 
+	/**
+	 * 수요 지식회 폼 저장
+	 *
+	 * @param userId 토큰 파싱을 통해 받아온 userId
+	 * @param dto    신청서 작성에 필요한 정보들
+	 */
 	@Transactional
-	public void createPresentationFromByInformation(PresentationFormRequestDto dto) {
+	public void createPresentationFrom(Long userId, PresentationFormRequestDto dto) {
+		presentationPolicyService.verifyReservationDate(dto.getDateTime());
+
 		Presentation presentation = Presentation.of(dto.getCategory(), dto.getDateTime(),
-				dto.getPresentationTime(), dto.getSubject(), dto.getSummary(), dto.getDetail());
+			dto.getPresentationTime(), dto.getSubject(), dto.getSummary(), dto.getDetail());
+		User user = userQueryService.getUser(userId);
+
+		presentation.setUser(user);
+
 		presentationRepository.save(presentation);
 	}
 }
