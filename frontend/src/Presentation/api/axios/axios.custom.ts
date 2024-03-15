48cd71da45f8faad37994153bbab7f83d8d5642d
jusohn <jusohn@student.42seoul.kr>
2024-03-15 18:38:19 +0900

    [FE] FIX: Presentation axios.custom.ts 이전 #1563

--- frontend/src/Presentation/api/axios/axios.custom.ts
+++ frontend/src/Presentation/api/axios/axios.custom.ts
@@ -100,3 +100,17 @@ export const axiosUpdatePresentationStatus = async (
     throw error;
   }
 };
+
+const axiosGetAdminPresentationScheduleURL = "/v5/admin/presentation/schedule/";
+export const getAdminPresentationSchedule = async (
+  yearMonth: string
+): Promise<any> => {
+  try {
+    const response = await instance.get(axiosGetAdminPresentationScheduleURL, {
+      params: { yearMonth },
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
