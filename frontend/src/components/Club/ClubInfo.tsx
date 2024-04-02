8b91365491e0f810ffc928aa243b2dbe580e1624
junyoung2015 <jusohn@student.42seoul.kr>
2024-04-02 15:24:23 +0900

    [FE] FIX: 동아리 페이지 버그 및 사용성 수정 #1562 (#1570)
    * [FE] FIX: 다른 동아리 클릭 시 ClubMemberInfoArea 가 계속 열려있고, 부정확한 정보를 표시하는 문제 수정 #1562
    
    * [FE] FIX: 변수명을 명료하게 수정 #1562
    
    * [FE] FIX: 동아리 메모 (공지) line-spacing 조절 #1562

--- frontend/src/components/Club/ClubInfo.tsx
+++ frontend/src/components/Club/ClubInfo.tsx
@@ -8,17 +8,20 @@ import ClubMemberListContainer from "@/components/Club/ClubMemberList/ClubMember
 import LoadingAnimation from "@/components/Common/LoadingAnimation";
 import { ClubInfoResponseDto } from "@/types/dto/club.dto";
 import useClubInfo from "@/hooks/useClubInfo";
+import useMenu from "@/hooks/useMenu";
 import { STATUS_400_BAD_REQUEST } from "@/constants/StatusCode";
 
 const ClubInfo = () => {
   const myInfo = useRecoilValue(userState);
   const { clubState, clubInfo, setPage } = useClubInfo();
-  const [imMaster, setImMaster] = useState<boolean>(false);
+  const [isMaster, setIsMaster] = useState<boolean>(false);
+  const { closeAll } = useMenu();
 
   useEffect(() => {
+    closeAll();
     if (clubInfo && clubInfo !== STATUS_400_BAD_REQUEST) {
       let clubInfoTest = clubInfo as ClubInfoResponseDto;
-      if (clubInfoTest.clubMaster.userName === myInfo.name) setImMaster(true);
+      if (clubInfoTest.clubMaster.userName === myInfo.name) setIsMaster(true);
     }
     // console.log(clubInfo);
   }, [clubInfo]);
@@ -38,8 +41,8 @@ const ClubInfo = () => {
         <ClubInfoWrapperStyled>
           <TitleStyled>동아리 정보</TitleStyled>
           <CardGridWrapper>
-            <ClubCabinetInfoCard clubInfo={clubInfo} isMaster={imMaster} />
-            <ClubNoticeCard notice={clubInfo.clubNotice} isMaster={imMaster} />
+            <ClubCabinetInfoCard clubInfo={clubInfo} isMaster={isMaster} />
+            <ClubNoticeCard notice={clubInfo.clubNotice} isMaster={isMaster} />
           </CardGridWrapper>
           <ClubMemberListContainer
             clubInfo={clubInfo}
