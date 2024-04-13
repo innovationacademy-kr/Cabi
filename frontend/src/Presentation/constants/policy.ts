578cd0da8b141145ca7f7092233bf438688e1ca9
jusohn <jusohn@student.42seoul.kr>
2024-04-13 17:01:20 +0900

    [COMMON] FIX: 정책에 따라 AVAILABLE_WEEKS 를 2,4 주로 변경

--- frontend/src/Presentation/constants/policy.ts
+++ frontend/src/Presentation/constants/policy.ts
@@ -2,9 +2,7 @@
 export const FUTURE_MONTHS_TO_DISPLAY = 3;
 
 // NOTE: 발표 가능한 주차
-export const AVAILABLE_WEEKS = (
-  import.meta.env.VITE_AVAILABLE_WEEKS ?? "1,2,3,4,5"
-)
+export const AVAILABLE_WEEKS = (import.meta.env.VITE_AVAILABLE_WEEKS ?? "2,4")
   .split(",")
   .map(Number);
 
