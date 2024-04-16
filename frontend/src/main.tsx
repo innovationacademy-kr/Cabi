d00e48d39d00cb9cd44ef9342554f943127d8674
jnkeniaem <jeanne.kim9@gmail.com>
2024-04-16 16:38:41 +0900

    [FE] FEAT: GlobalStyle 이용해서 모드 바꿈#1551

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import { GlobalStyle } from "@/components/TopNav/DarkMode/DarkMode";
 import App from "./App";
 import "./assets/css/media.css";
 import "./assets/css/reset.css";
@@ -9,6 +10,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
+      <GlobalStyle />
       <App />
     </RecoilRoot>
   </React.StrictMode>
