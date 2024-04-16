cd0aadd32a8b9964972c7a8576b45510878fe4c7
sichoi42 <42.4.sichoi@gmail.com>
2024-04-16 23:26:48 +0900

    [BE] FEAT: 오류 알림 디스코드 메시지 #1593

--- backend/src/main/java/org/ftclub/cabinet/alarm/discord/DiscordWebHookMessenger.java
+++ backend/src/main/java/org/ftclub/cabinet/alarm/discord/DiscordWebHookMessenger.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 @Component
 public class DiscordWebHookMessenger {
+
 	private final static String DISCORD_WEBHOOK_MESSAGE_KEY = "content";
 	private final String discordWebHookUrl;
 
@@ -17,6 +18,18 @@ public class DiscordWebHookMessenger {
 	}
 
 	public void sendMessage(DiscordAlarmMessage message) {
+		Map<String, String> body = new HashMap<>();
+		body.put(DISCORD_WEBHOOK_MESSAGE_KEY, message.toString());
+
+		WebClient.create().post()
+				.uri(discordWebHookUrl)
+				.bodyValue(body)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+	}
+
+	public void sendMessage(DiscordWebAlarmMessage message) {
 		Map<String, String> body = new HashMap<>();
 		body.put(DISCORD_WEBHOOK_MESSAGE_KEY, message.toString());
 
