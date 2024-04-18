adda16d06fa34c6f6eb80bddfefd5da3272f96f0
jnkeniaem <jeanne.kim9@gmail.com>
2024-04-18 18:30:37 +0900

    [FE] FIX: colorThemeIconComponentMap에 Toggle을 붙여 컬러 테마 토글과 컬러 모드를 분리 #1551

--- frontend/src/types/enum/colorTheme.type.enum.ts
+++ frontend/src/types/enum/colorTheme.type.enum.ts
@@ -1,7 +1,12 @@
-enum ColorThemeType {
+// NOTE : 라이트, 다크, 디바이스 토글
+export enum ColorThemeToggleType {
   LIGHT = "LIGHT",
   DARK = "DARK",
   DEVICE = "DEVICE",
 }
 
-export default ColorThemeType;
+// NOTE : 라이트 모드 / 다크 모드
+export enum ColorThemeType {
+  LIGHT = "LIGHT",
+  DARK = "DARK",
+}
