37c527232a8925e10f3076df549c4e1875e449ca
sichoi42 <42.4.sichoi@gmail.com>
2024-04-19 17:09:58 +0900

    [BE] FIX: 연체 처리 NPE 오류 수정 #1597

--- backend/src/main/java/org/ftclub/cabinet/dto/ActiveLentHistoryDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/ActiveLentHistoryDto.java
@@ -1,5 +1,6 @@
 package org.ftclub.cabinet.dto;
 
+import javax.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -14,5 +15,6 @@ public class ActiveLentHistoryDto {
 	private final String email;
 	private final Long cabinetId;
 	private final Boolean isExpired;
+	@Nullable
 	private final Long daysFromExpireDate;
 }
