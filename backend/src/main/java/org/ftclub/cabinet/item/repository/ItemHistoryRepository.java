ef43dd593a9eb36e229f29daae41d6a06978bdfa
jiwon <jwpark0119@gmail.com>
2024-04-23 15:55:23 +0900

    [BE] FEAT: ItemHistory 관련 Repository, Service 추가

---
+++ backend/src/main/java/org/ftclub/cabinet/item/repository/ItemHistoryRepository.java
@@ -0,0 +1,10 @@
+package org.ftclub.cabinet.item.repository;
+
+import org.ftclub.cabinet.item.domain.ItemHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemHistoryRepository extends JpaRepository<Long, ItemHistory> {
+
+}
