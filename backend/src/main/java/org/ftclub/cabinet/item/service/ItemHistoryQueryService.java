2054f26d86fe3bccdcdbf65270d435e0a79ee7f6
Woo Joo Chae <enaenen@naver.com>
2024-04-23 20:11:38 +0900

    [BE] FEAT: 인벤토리 관련 query service 추가

--- backend/src/main/java/org/ftclub/cabinet/item/service/ItemHistoryQueryService.java
+++ backend/src/main/java/org/ftclub/cabinet/item/service/ItemHistoryQueryService.java
@@ -1,5 +1,7 @@
 package org.ftclub.cabinet.item.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.item.domain.ItemHistory;
 import org.ftclub.cabinet.item.repository.ItemHistoryRepository;
@@ -7,21 +9,17 @@ import org.ftclub.cabinet.log.LogLevel;
 import org.ftclub.cabinet.log.Logging;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Logging(level = LogLevel.DEBUG)
 public class ItemHistoryQueryService {
 
-    private final ItemHistoryRepository itemHistoryRepository;
+	private final ItemHistoryRepository itemHistoryRepository;
 
+	public List<ItemHistory> findAllItemHistoryByUser(Long userId) {
+		return itemHistoryRepository.getAllUnusedItemHistoryByUser(userId);
+	}
 
-
-    public List<ItemHistory> findItemHistory(Long userId, LocalDateTime start, LocalDateTime end) {
-        return itemHistoryRepository.findItemHistory(userId, start, end);
-    }
 	public List<ItemHistory> getItemHistory(Long userId, LocalDateTime start, LocalDateTime end) {
 		return null;
 	}
