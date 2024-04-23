0b90de8516c17a3dae94473ca07b655e75e252f3
Woo Joo Chae <enaenen@naver.com>
2024-04-23 18:46:54 +0900

    [BE] FEAT: myitem - controller & facade, query service

--- backend/src/main/java/org/ftclub/cabinet/item/service/ItemFacadeService.java
+++ backend/src/main/java/org/ftclub/cabinet/item/service/ItemFacadeService.java
@@ -1,12 +1,7 @@
 package org.ftclub.cabinet.item.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.ftclub.cabinet.dto.CoinHistoryResponseDto;
-import org.ftclub.cabinet.dto.ItemHistoryResponseDto;
-import org.ftclub.cabinet.dto.ItemPaginationDto;
-import org.ftclub.cabinet.dto.UserSessionDto;
+import org.ftclub.cabinet.dto.*;
 import org.ftclub.cabinet.item.domain.CoinHistoryType;
 import org.ftclub.cabinet.item.domain.Item;
 import org.ftclub.cabinet.log.LogLevel;
@@ -14,37 +9,41 @@ import org.ftclub.cabinet.log.Logging;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 @Logging(level = LogLevel.DEBUG)
 public class ItemFacadeService {
 
-	private final ItemQueryService itemQueryService;
-	private final ItemCommandService itemCommandService;
-	private final ItemHistoryQueryService itemHistoryQueryService;
-	private final ItemHistoryCommandService itemHistoryCommandService;
+    private final ItemQueryService itemQueryService;
+    private final ItemCommandService itemCommandService;
+    private final ItemHistoryQueryService itemHistoryQueryService;
+    private final ItemHistoryCommandService itemHistoryCommandService;
 
-	@Transactional
-	public ItemPaginationDto getItems() {
-		List<Item> allItems = itemQueryService.getAllItems();
-		return null;
-	}
+    @Transactional
+    public ItemPaginationDto getItems() {
+        List<Item> allItems = itemQueryService.getAllItems();
+        return null;
+    }
 
-	@Transactional(readOnly = true)
-	public void getMyItems(UserSessionDto user) {
+    @Transactional(readOnly = true)
+    public MyItemResponseDto getMyItems(UserSessionDto user) {
+        itemHistoryQueryService.getItemHistory(user.getUserId(), LocalDateTime.now(), LocalDateTime.now());
+        return null;
+    }
 
-	}
+    @Transactional(readOnly = true)
+    public ItemHistoryResponseDto getItemHistory(Long userId,
+                                                 LocalDateTime start, LocalDateTime end) {
+        return null;
+    }
 
-	@Transactional(readOnly = true)
-	public ItemHistoryResponseDto getItemHistory(Long userId,
-			LocalDateTime start, LocalDateTime end) {
-		return null;
-	}
-
-	@Transactional(readOnly = true)
-	public CoinHistoryResponseDto getCoinHistory(Long userId, CoinHistoryType type,
-			LocalDateTime start, LocalDateTime end) {
-		return null;
-	}
+    @Transactional(readOnly = true)
+    public CoinHistoryResponseDto getCoinHistory(Long userId, CoinHistoryType type,
+                                                 LocalDateTime start, LocalDateTime end) {
+        return null;
+    }
 }
--- backend/src/main/java/org/ftclub/cabinet/item/service/ItemHistoryQueryService.java
+++ backend/src/main/java/org/ftclub/cabinet/item/service/ItemHistoryQueryService.java
@@ -1,7 +1,5 @@
 package org.ftclub.cabinet.item.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.item.domain.ItemHistory;
 import org.ftclub.cabinet.item.repository.ItemHistoryRepository;
@@ -9,15 +7,22 @@ import org.ftclub.cabinet.log.LogLevel;
 import org.ftclub.cabinet.log.Logging;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Logging(level = LogLevel.DEBUG)
 public class ItemHistoryQueryService {
 
-	private final ItemHistoryRepository itemHistoryRepository;
+    private final ItemHistoryRepository itemHistoryRepository;
 
 
-	public List<ItemHistory> getItemHistory(Long userId, LocalDateTime start, LocalDateTime end) {
-		return null;
-	}
+    public List<ItemHistory> getItemHistory(Long userId, LocalDateTime start, LocalDateTime end) {
+        return null;
+    }
+
+    public List<ItemHistory> findItemHistory(Long userId, LocalDateTime start, LocalDateTime end) {
+        return itemHistoryRepository.findItemHistory(userId, start, end);
+    }
 }
