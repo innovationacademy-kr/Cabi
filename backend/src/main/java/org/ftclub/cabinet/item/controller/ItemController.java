27bc2a3c4627feff2a56adaa7eff250aa491a227
Woo Joo Chae <enaenen@naver.com>
2024-04-23 15:40:03 +0900

    [BE] FEAT: Essential field added

--- backend/src/main/java/org/ftclub/cabinet/item/controller/ItemController.java
+++ backend/src/main/java/org/ftclub/cabinet/item/controller/ItemController.java
@@ -3,7 +3,10 @@ package org.ftclub.cabinet.item.controller;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.auth.domain.AuthGuard;
 import org.ftclub.cabinet.auth.domain.AuthLevel;
+import org.ftclub.cabinet.dto.UserSessionDto;
+import org.ftclub.cabinet.item.service.ItemFacadeService;
 import org.ftclub.cabinet.log.Logging;
+import org.ftclub.cabinet.user.domain.UserSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v5/items")
 @Logging
 public class ItemController {
+    private final ItemFacadeService itemFacadeService;
 
     @GetMapping("/")
     @AuthGuard(level = AuthLevel.USER_ONLY)
--- backend/src/main/java/org/ftclub/cabinet/item/service/ItemFacadeService.java
+++ backend/src/main/java/org/ftclub/cabinet/item/service/ItemFacadeService.java
@@ -1,7 +1,22 @@
 package org.ftclub.cabinet.item.service;
 
+import lombok.RequiredArgsConstructor;
+import org.ftclub.cabinet.dto.UserSessionDto;
+import org.ftclub.cabinet.log.LogLevel;
+import org.ftclub.cabinet.log.Logging;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
+@RequiredArgsConstructor
+@Logging(level = LogLevel.DEBUG)
 public class ItemFacadeService {
+    private final ItemQueryService itemQueryService;
+    private final ItemCommandService itemCommandService;
+
+    @Transactional(readOnly = true)
+    public void getMyItems(UserSessionDto user) {
+
+    }
 }
