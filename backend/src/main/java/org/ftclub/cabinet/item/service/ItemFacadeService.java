43b81dfbb15c19846dd8e1c76821e2faf9ade382
Woo Joo Chae <enaenen@naver.com>
2024-04-23 20:12:09 +0900

    [BE] FEAT: 인벤토리 관련 facade WIP

--- backend/src/main/java/org/ftclub/cabinet/item/service/ItemFacadeService.java
+++ backend/src/main/java/org/ftclub/cabinet/item/service/ItemFacadeService.java
@@ -1,9 +1,5 @@
 package org.ftclub.cabinet.item.service;
 
-import static org.ftclub.cabinet.item.domain.CoinHistoryType.ALL;
-import static org.ftclub.cabinet.item.domain.CoinHistoryType.EARN;
-import static org.ftclub.cabinet.item.domain.CoinHistoryType.USE;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,23 +8,20 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.dto.CoinHistoryDto;
 import org.ftclub.cabinet.dto.CoinHistoryResponseDto;
-import org.ftclub.cabinet.dto.CoinInformationDto;
 import org.ftclub.cabinet.dto.ItemDto;
 import org.ftclub.cabinet.dto.ItemHistoryResponseDto;
 import org.ftclub.cabinet.dto.ItemResponseDto;
+import org.ftclub.cabinet.dto.MyItemResponseDto;
 import org.ftclub.cabinet.dto.UserSessionDto;
-import org.ftclub.cabinet.dto.*;
 import org.ftclub.cabinet.item.domain.CoinHistoryType;
 import org.ftclub.cabinet.item.domain.Item;
 import org.ftclub.cabinet.item.domain.ItemHistory;
 import org.ftclub.cabinet.log.LogLevel;
 import org.ftclub.cabinet.log.Logging;
 import org.ftclub.cabinet.mapper.ItemMapper;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 
 @Service
@@ -36,21 +29,12 @@ import java.util.List;
 @Logging(level = LogLevel.DEBUG)
 public class ItemFacadeService {
 
-    private final ItemQueryService itemQueryService;
-    private final ItemCommandService itemCommandService;
-    private final ItemHistoryQueryService itemHistoryQueryService;
-    private final ItemHistoryCommandService itemHistoryCommandService;
 	private final ItemQueryService itemQueryService;
 	private final ItemCommandService itemCommandService;
 	private final ItemHistoryQueryService itemHistoryQueryService;
 	private final ItemHistoryCommandService itemHistoryCommandService;
 	private final ItemMapper itemMapper;
 
-    @Transactional
-    public ItemPaginationDto getItems() {
-        List<Item> allItems = itemQueryService.getAllItems();
-        return null;
-    }
 	@Transactional
 	public ItemResponseDto getAllItems() {
 		List<Item> allItems = itemQueryService.getAllItems();
@@ -60,26 +44,49 @@ public class ItemFacadeService {
 		return new ItemResponseDto(itemDtos);
 	}
 
-    @Transactional(readOnly = true)
-    public MyItemResponseDto getMyItems(UserSessionDto user) {
-        itemHistoryQueryService.getItemHistory(user.getUserId(), LocalDateTime.now(), LocalDateTime.now());
-        return null;
-    }
-
 	@Transactional(readOnly = true)
-	public ItemHistoryResponseDto getItemHistory(Long userId,
-		LocalDateTime start, LocalDateTime end) {
+	public MyItemResponseDto getMyItems(UserSessionDto user) {
+//		List<ItemHistory> userItemHistories = itemHistoryQueryService.findAllItemHistoryByUser(user.getUserId());
+//
+//		List<ItemDto> extensionItems = getItemDtos(userItemHistories);
+//
+//		List<ItemDto> exchangeItems = userItemHistories.stream()
+//				.filter(ih -> ih.getItem())
+//				.map(ih -> itemMapper.toItemDto(ih.getItem()))
+//				.collect(Collectors.toList());
+//
+//		List<ItemDto> alarmItems = userItemHistories.stream()
+//				.filter(ih -> ih.getItem().getSku().isAlarmType())
+//				.map(ih -> itemMapper.toItemDto(ih.getItem()))
+//				.collect(Collectors.toList());
+//
+//		List<ItemDto> penaltyItems = userItemHistories.stream()
+//				.filter(ih -> ih.getItem().getSku().isPenaltyType())
+//				.map(ih -> itemMapper.toItemDto(ih.getItem()))
+//				.collect(Collectors.toList());
+//
+//		return itemMapper.toMyItemResponseDto(extensionItems, exchangeItems, alarmItems, penaltyItems);
 		return null;
 	}
 
-    @Transactional(readOnly = true)
-    public CoinHistoryResponseDto getCoinHistory(Long userId, CoinHistoryType type,
-                                                 LocalDateTime start, LocalDateTime end) {
-        return null;
-    }
+	private List<ItemDto> getItemDtos(List<ItemHistory> userItemHistories, ) {
+		List<ItemDto> extensionItems = userItemHistories.stream()
+				.filter(ih -> ih.getItem().getSku().isExtensionType())
+				.map(ih -> itemMapper.toItemDto(ih.getItem()))
+				.collect(Collectors.toList());
+		return extensionItems;
+	}
+
+
+	@Transactional(readOnly = true)
+	public ItemHistoryResponseDto getItemHistory(Long userId,
+			LocalDateTime start, LocalDateTime end) {
+		return null;
+	}
+
 	@Transactional(readOnly = true)
 	public CoinHistoryResponseDto getCoinHistory(Long userId, CoinHistoryType type,
-		LocalDateTime start, LocalDateTime end) {
+			LocalDateTime start, LocalDateTime end) {
 		List<Item> items = new ArrayList<>();
 		if (type.equals(CoinHistoryType.EARN) || type.equals(CoinHistoryType.ALL)) {
 			items.addAll(itemQueryService.getEarnItemIds());
@@ -89,13 +96,13 @@ public class ItemFacadeService {
 		}
 		List<Long> itemIds = items.stream().map(Item::getId).collect(Collectors.toList());
 		List<ItemHistory> coinHistories =
-			itemHistoryQueryService.getCoinHistoryOnItem(userId, start, end, itemIds);
+				itemHistoryQueryService.getCoinHistoryOnItem(userId, start, end, itemIds);
 
 		Map<Long, Item> itemMap = items.stream()
-			.collect(Collectors.toMap(Item::getId, item -> item));
+				.collect(Collectors.toMap(Item::getId, item -> item));
 		List<CoinHistoryDto> result = coinHistories.stream()
-			.map(ih -> itemMapper.toCoinHistoryDto(ih, itemMap.get(ih.getItemId())))
-			.collect(Collectors.toList());
+				.map(ih -> itemMapper.toCoinHistoryDto(ih, itemMap.get(ih.getItemId())))
+				.collect(Collectors.toList());
 		return itemMapper.toCoinHistoryResponseDto(result);
 	}
 
@@ -116,15 +123,4 @@ public class ItemFacadeService {
 //		}
 //	}
 
-	/**
-	 * 해당 월의 총 Coin 아이템을 획득한 횟수, 요청일의 출석체크 해당 여부를 반환
-	 *
-	 * @param userId
-	 * @param itemId
-	 * @return
-	 */
-	public CoinInformationDto getCoinInformation(Long userId, Long itemId) {
-		LocalDateTime now = LocalDateTime.now();
-		itemHistoryQueryService.get
-	}
 }
