1cbe7dc7c06d85be904e3c9985d19587060a93cc
Woo Joo Chae <enaenen@naver.com>
2024-04-23 15:30:01 +0900

    [BE] FEAT: annotation added to service

--- backend/src/main/java/org/ftclub/cabinet/item/service/ItemQueryService.java
+++ backend/src/main/java/org/ftclub/cabinet/item/service/ItemQueryService.java
@@ -1,7 +1,13 @@
 package org.ftclub.cabinet.item.service;
 
+import lombok.RequiredArgsConstructor;
+import org.ftclub.cabinet.log.LogLevel;
+import org.ftclub.cabinet.log.Logging;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@RequiredArgsConstructor
+@Logging(level = LogLevel.DEBUG)
 public class ItemQueryService {
 }
