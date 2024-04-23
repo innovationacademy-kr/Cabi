9cb4782fee3ebf14237368ca25f0a7535126a5e2
Woo Joo Chae <enaenen@naver.com>
2024-04-23 15:15:26 +0900

    [BE] DOCS: 아이템 엔티티 주석

--- backend/src/main/java/org/ftclub/cabinet/item/domain/Item.java
+++ backend/src/main/java/org/ftclub/cabinet/item/domain/Item.java
@@ -1,55 +1,76 @@
 package org.ftclub.cabinet.item.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.ftclub.cabinet.exception.ExceptionStatus;
 
+import javax.persistence.*;
+
 @Entity
 @Table(name = "ITEM")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString()
+@ToString
 public class Item {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
 
-	@Column(name = "NAME", unique = true, nullable = false)
-	private String name;
+    /**
+     * 상품명
+     */
+    @Column(name = "NAME", unique = true, nullable = false)
+    private String name;
 
-	@Column(name = "PRICE", nullable = false)
-	private Long price;
 
-	@Column(name = "SKU", unique = true, nullable = false)
-	private Long sku;
+    /**
+     * 상품 가격
+     */
+    @Column(name = "PRICE", nullable = false)
+    private Long price;
 
-	@Column(name = "DESCRIPTION", nullable = false)
-	private String description;
+    /**
+     * 상품 고유 코드
+     */
+    @Column(name = "SKU", unique = true, nullable = false)
+    private Long sku;
 
-	protected Item(String name, long price, long sku, String description){
-		this.name = name;
-		this.price = price;
-		this.sku = sku;
-		this.description = description;
-	}
+    /**
+     * 상품 설명
+     */
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
 
-	public static Item of(String name, long price, long sku, String description){
-		Item item = new Item(name, price, sku, description);
-		if (!item.isValid()){
-			throw ExceptionStatus.INVALID_ARGUMENT.asDomainException();
-		}
-		return item;
-	}
+    protected Item(String name, long price, long sku, String description) {
+        this.name = name;
+        this.price = price;
+        this.sku = sku;
+        this.description = description;
+    }
 
-	private boolean isValid() {
-		return this.name != null && this.sku != null && this.description != null;
-	}
+    /**
+     * @param name        상품 이름
+     * @param price       상품 가격
+     * @param sku         상품 코드
+     * @param description 상품 설명
+     * @return 상품 객체	{@link Item}
+     */
+    public static Item of(String name, long price, long sku, String description) {
+        Item item = new Item(name, price, sku, description);
+        if (!item.isValid()) {
+            throw ExceptionStatus.INVALID_ARGUMENT.asDomainException();
+        }
+        return item;
+    }
+
+    /**
+     * name, sku, description 의 null 이 아닌지 확인합니다.
+     *
+     * @return 유효한 인스턴스 여부
+     */
+    private boolean isValid() {
+        return this.name != null && this.sku != null && this.description != null;
+    }
 }
