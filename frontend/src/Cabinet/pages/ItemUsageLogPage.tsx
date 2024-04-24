e1c8d76cb7442904a51488afa3daf78ed1350563
gykoh42 <karmakoh42@naver.com>
2024-04-24 17:34:34 +0900

    [FE] FEAT: 아이템 사용내역 제목 Home 서식에 맞춰 적용 #1604

--- frontend/src/Cabinet/pages/ItemUsageLogPage.tsx
+++ frontend/src/Cabinet/pages/ItemUsageLogPage.tsx
@@ -1,4 +1,49 @@
+import useState from "react";
+import styled from "styled-components";
+
 const ItemUsageLogPage = () => {
-  return <div>아이템 사용 내역</div>;
+  return (
+    <WrapperStyled id="infoWrap">
+      <TitleContainerStyled className="titleContainer">
+        <h1 className="title">
+          <span>아이템 사용 내역</span>
+        </h1>
+      </TitleContainerStyled>
+    </WrapperStyled>
+  );
 };
+
+const WrapperStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 60px 0;
+`;
+
+const TitleContainerStyled = styled.div`
+  width: 80%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid #d9d9d9;
+  margin-bottom: 70px;
+  color: var(--main-color);
+  font-weight: 700;
+  .logo {
+    width: 35px;
+    height: 35px;
+    margin-bottom: 20px;
+  }
+  .title {
+    font-size: 2.5rem;
+    letter-spacing: -0.02rem;
+    margin-bottom: 20px;
+  }
+  .title > span {
+    color: black;
+  }
+`;
+
 export default ItemUsageLogPage;
