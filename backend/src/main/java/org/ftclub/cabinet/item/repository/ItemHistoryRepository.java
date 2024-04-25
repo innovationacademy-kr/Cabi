1bac97493498f43f7dcf408ae453a12385180440
saewoo1 <wooozooo1123@icloud.com>
2024-04-25 13:04:44 +0900

    [BE] FEAT: 유저가 아이템을 기간 내에 사용한 횟수를 반환하는 기능

--- backend/src/main/java/org/ftclub/cabinet/item/repository/ItemHistoryRepository.java
+++ backend/src/main/java/org/ftclub/cabinet/item/repository/ItemHistoryRepository.java
@@ -12,34 +12,44 @@ import org.springframework.stereotype.Repository;
 public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> {
 
 	@Query("SELECT ih "
-			+ "FROM ItemHistory ih "
-			+ "WHERE ih.userId = :userId "
-			+ "AND ih.purchaseAt BETWEEN :start AND :end "
-			+ "AND ih.itemId IN (:itemIds)")
+		+ "FROM ItemHistory ih "
+		+ "WHERE ih.userId = :userId "
+		+ "AND ih.purchaseAt BETWEEN :start AND :end "
+		+ "AND ih.itemId IN (:itemIds)")
 	List<ItemHistory> findAllByUserIdAndPurchaseAtAndItemIdIn(@Param("userId") Long userId,
-			@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
-			@Param("itemIds") List<Long> itemIds);
+		@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
+		@Param("itemIds") List<Long> itemIds);
 
 	@Query("SELECT ih "
-			+ "FROM ItemHistory ih "
-			+ "JOIN FETCH ih.item "
-			+ "WHERE ih.userId = :userId "
-			+ "AND ih.usedAt BETWEEN :start AND :end")
+		+ "FROM ItemHistory ih "
+		+ "JOIN FETCH ih.item "
+		+ "WHERE ih.userId = :userId "
+		+ "AND ih.usedAt BETWEEN :start AND :end")
 	List<ItemHistory> findAllByUserIdAndUsedAtJoinItem(@Param("userId") Long userId,
-			@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+		@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
 	@Query("SELECT ih "
-			+ "FROM ItemHistory ih "
-			+ "JOIN FETCH ih.item "
-			+ "WHERE ih.userId = :userId")
+		+ "FROM ItemHistory ih "
+		+ "JOIN FETCH ih.item "
+		+ "WHERE ih.userId = :userId")
 	List<ItemHistory> findAllByUserId(@Param("userId") Long userId);
 
 
 	@Query("SELECT ih "
-			+ "FROM ItemHistory ih "
-			+ "JOIN FETCH ih.item "
-			+ "WHERE ih.userId = :userId "
-			+ "AND ih.usedAt IS NULL "
+		+ "FROM ItemHistory ih "
+		+ "JOIN FETCH ih.item "
+		+ "WHERE ih.userId = :userId "
+		+ "AND ih.usedAt IS NULL "
 	)
 	List<ItemHistory> getAllUnusedItemHistoryByUser(@Param("userId") Long userId);
+
+	@Query("SELECT COUNT(ih) "
+		+ "FROM ItemHistory ih "
+		+ "WHERE ih.userId = :userId "
+		+ "AND ih.itemId = :itemId "
+		+ "AND ih.purchaseAt BETWEEN :start AND :end")
+	Long getCountByUserIdAndItemIdBetween(@Param("userId") Long userId,
+		@Param("itemId") Long itemId,
+		@Param("start") LocalDateTime start,
+		@Param("end") LocalDateTime end);
 }
