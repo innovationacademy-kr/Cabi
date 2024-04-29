607dba28589d76da71e5874a4f31623b63222ab5
Minkyu01 <koryum30@gmail.com>
2024-04-29 15:57:05 +0900

    [FE] FEAT: 무한 스크롤 분석 #1604

--- frontend/src/Cabinet/pages/ItemUsageLogPage.tsx
+++ frontend/src/Cabinet/pages/ItemUsageLogPage.tsx
@@ -11,98 +11,203 @@ interface IItemUsageLog {
   logo: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 }
 
-const test: IItemUsageLog[] = [
-  {
-    date: new Date(
-      Number(new Date().getFullYear()),
-      Number(new Date().getMonth()),
-      Number(new Date().getDate()),
-      Number(new Date().getHours()),
-      Number(new Date().getMinutes())
-    ),
-    title: "이사권",
-    logo: MoveImg,
-  },
-  {
-    date: new Date(
-      Number(new Date().getFullYear()),
-      Number(new Date().getMonth()),
-      Number(new Date().getDate()),
-      Number(new Date().getHours()),
-      Number(new Date().getMinutes())
-    ),
-    title: "알림 등록권",
-    logo: AlarmImg,
-  },
-  {
-    date: new Date(
-      Number(new Date().getFullYear()),
-      Number(new Date().getMonth()),
-      Number(new Date().getDate() + 1),
-      Number(new Date().getHours()),
-      Number(new Date().getMinutes())
-    ),
-    title: "연장권 - 3일",
-    logo: ExtensionImg,
-  },
-  {
-    date: new Date(
-      Number(new Date().getFullYear()),
-      Number(new Date().getMonth()),
-      Number(new Date().getDate() + 1),
-      Number(new Date().getHours()),
-      Number(new Date().getMinutes())
-    ),
-    title: "페널티 축소권",
-    logo: PenaltyImg,
-  },
-  {
-    date: new Date(
-      Number(new Date().getFullYear()),
-      Number(new Date().getMonth()),
-      Number(new Date().getDate() + 2),
-      Number(new Date().getHours()),
-      Number(new Date().getMinutes())
-    ),
-    title: "연장권 - 15일", // type 이 있는 아이템의 경우 따로 처리 필요함
-    logo: ExtensionImg,
-  },
-];
+// interface IItemDto {
+//   Sku: string;
+//   ItemName: string;
+//   ItemPrice: number;
+//   ItemType: string;
+// }
+// interface IItemHistoryDto {
+//   date: Date;
+//   ItemDto: IItemDto;
+// }
+
+// const dummyData: { itemHistories: IItemHistoryDto[]; totalLength: number } = {
+//   itemHistories: [
+//     {
+//       date: "2024-04-01T10:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-02T11:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-03T12:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-04T13:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-05T14:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-06T15:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-07T16:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-08T17:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-09T18:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//     {
+//       date: "2024-04-10T19:00:00",
+//       itemDto: {
+//         Sku: "extension_31",
+//         ItemName: "연장권",
+//         ItemPrice: -2000,
+//         ItemType: "3일",
+//       },
+//     },
+//   ],
+//   totalLength: 10,
+// };
+
+// function groupByMonthAndLimitItems(
+//   itemHistories: IItemHistoryDto[],
+//   limit: number = 4
+// ): { [key: string]: IItemHistoryDto[] } {
+//   const groupedByMonth: { [key: string]: IItemHistoryDto[] } = {};
+
+//   itemHistories.forEach((itemHistory) => {
+//     const month = itemHistory.date.substring(0, 7); // YYYY-MM 형식으로 월을 추출합니다.
+//     if (!groupedByMonth[month]) {
+//       groupedByMonth[month] = [];
+//     }
+//     if (groupedByMonth[month].length < limit) {
+//       groupedByMonth[month].push(itemHistory);
+//     }
+//   });
+
+//   return groupedByMonth;
+// }
 
 const ItemUsageLogPage = () => {
-  const [itemUsageLogs, setItemUsageLogs] = useState<IItemUsageLog[]>([]);
-  const [groupedLogs, setGroupedLogs] = useState<{
-    [key: string]: IItemUsageLog[];
-  }>({});
+  // const [items, setItems] = useState();
+  // const [hasMore, setHasMore] = useState(true);
+  // // page 조회 페이지 수 -> 한페이지에 4개씩
+  // const [page, setPage] = useState(1);
+  // // size 보여주는 컴포넌트 개수
+  // const [size, setSize] = useState(1);
 
-  useEffect(() => {
-    setItemUsageLogs(test);
-    const grouped = test.reduce((acc, item) => {
-      const dateKey = item.date.toISOString().split("T")[0]; // 날짜 부분까지만 추출
-      if (!acc[dateKey]) {
-        acc[dateKey] = [];
-      }
-      acc[dateKey].push(item); // dateKey 에 해당 dateKey 가 없으면 빈 배열 할당 후 push
-      return acc;
-    }, {} as { [key: string]: IItemUsageLog[] });
-    setGroupedLogs(grouped);
-  }, []);
+  // // 아이템을 불러오는 함수
+  // const loadItems = () => {
+  //   // 페이지당 아이템 수를 정의합니다. 예: 10
+  //   const perPage = 4;
+  //   // 현재 페이지에 해당하는 아이템을 계산합니다.
+  //   // 나중에 더미데이터를 API 호출로 대체
+  //   const newItems = dummyData.itemHistories.slice(0, page * perPage);
 
-  const formatDate = (date: Date) => {
-    return new Intl.DateTimeFormat("ko-KR", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: false,
-    }).format(date);
-  };
+  //   setItems(newItems);
+  //   // 모든 아이템을 로드했는지 확인합니다.
+  //   if (newItems.length >= dummyData.totalLength) {
+  //     setHasMore(false);
+  //   }
+  //   const groupedData = groupByMonthAndLimitItems(dummyData.itemHistories);
+  // };
+
+  // useEffect(() => {
+  //   // 컴포넌트 마운트 시 첫 페이지 로드
+  //   loadItems();
+  // }, [page]);
+
+  // // 스크롤 이벤트를 처리하는 함수
+  // const handleScroll = (e) => {
+  //   const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+  //   // 스크롤이 끝에 도달했는지 확인
+  //   if (scrollHeight - scrollTop === clientHeight) {
+  //     // 추가 아이템이 더 있는지 확인 후 페이지를 업데이트
+  //     if (hasMore) {
+  //       setPage((prevPage) => prevPage + 1);
+  //     }
+  //   }
+  // };
+
+  // const formatDate = (date: Date) => {
+  //   return new Intl.DateTimeFormat("ko-KR", {
+  //     year: "numeric",
+  //     month: "2-digit",
+  //     day: "2-digit",
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //     hour12: false,
+  //   }).format(date);
+  // };
+  // console.log("formatDate", formatDate("2024-04-01T10:00:00"));
 
   return (
     <WrapperStyled>
       <TitleWrapperStyled>아이템 사용 내역</TitleWrapperStyled>
-      <ItemUsageLogWrapperStyled>
+      {/* <div
+        onScroll={handleScroll}
+        style={{ overflowY: "auto", height: "100vh" }}
+      >
+        {items.map((item, index) => (
+          <div key={index}>
+            <p>{item.date}</p>
+            <p>{ItemLogoType[item.itemDto.ItemName]}</p>
+            <p>{item.itemDto.ItemPrice}</p>
+            <p>{item.itemDto.ItemType}</p>
+          </div>
+        ))}
+        {!hasMore && <p>더 이상 로드할 아이템이 없습니다.</p>}
+      </div> */}
+      {/* <ItemUsageLogWrapperStyled>
         {Object.entries(groupedLogs).map(([date, itemUsageLogs]) => (
           <DateSectionStyled key={date}>
             <DateTitleStyled>{date.replace(/-/g, ". ")}</DateTitleStyled>
@@ -121,7 +226,7 @@ const ItemUsageLogPage = () => {
             ))}
           </DateSectionStyled>
         ))}
-      </ItemUsageLogWrapperStyled>
+      </ItemUsageLogWrapperStyled> */}
     </WrapperStyled>
   );
 };
