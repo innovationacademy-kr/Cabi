e3e84f2885b474403c5c2db64085a715d3585806
saewoo1 <wooozooo1123@icloud.com>
2024-04-30 23:19:48 +0900

    [BE] REFACTOR: 불필요한 어노테이션 제거
    DTO에서 사용하지 않는 NoArgsConstructor 어노테이션 제거

--- backend/src/main/java/org/ftclub/cabinet/dto/CoinMonthlyCollectionDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/CoinMonthlyCollectionDto.java
@@ -2,12 +2,10 @@ package org.ftclub.cabinet.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
-@NoArgsConstructor
 @AllArgsConstructor
 public class CoinMonthlyCollectionDto {
 
--- backend/src/main/java/org/ftclub/cabinet/dto/ItemUseRequestDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/ItemUseRequestDto.java
@@ -2,10 +2,8 @@ package org.ftclub.cabinet.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ItemUseRequestDto {
 
