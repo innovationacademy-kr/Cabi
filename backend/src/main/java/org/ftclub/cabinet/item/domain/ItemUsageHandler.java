8dc37636020116587952d8fd5bae74fac74298eb
saewoo1 <wooozooo1123@icloud.com>
2024-05-01 15:47:07 +0900

    [BE] FIX: EventListener -> TransactionalEventListener
    Transactional 전파를 통해 이벤트 로직 수행 도중 예외가 발생할 경우 이후 로직을 수행하지 않도록 롤백

--- backend/src/main/java/org/ftclub/cabinet/item/domain/ItemUsageHandler.java
+++ backend/src/main/java/org/ftclub/cabinet/item/domain/ItemUsageHandler.java
@@ -3,8 +3,8 @@ package org.ftclub.cabinet.item.domain;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.lent.service.LentFacadeService;
 import org.ftclub.cabinet.user.service.UserFacadeService;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -13,7 +13,7 @@ public class ItemUsageHandler {
 	private final LentFacadeService lentFacadeService;
 	private final UserFacadeService userFacadeService;
 
-	@EventListener
+	@TransactionalEventListener
 	public void handleItemUsage(ItemUsage itemUsage) {
 		if (itemUsage instanceof SwapItem) {
 			SwapItem swapItem = (SwapItem) itemUsage;
