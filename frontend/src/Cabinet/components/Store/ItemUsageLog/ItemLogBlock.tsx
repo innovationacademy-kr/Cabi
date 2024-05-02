81a23d90b0e94bfb2d6213795fae1ce719e2fb58
gykoh42 <karmakoh42@naver.com>
2024-05-02 15:02:35 +0900

    [FE] FEAT: ItemLogBlock 컴포넌트로 분리 #1604

---
+++ frontend/src/Cabinet/components/Store/ItemUsageLog/ItemLogBlock.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+interface IItemUsageLog {
+  date: Date;
+  title: string;
+  logo: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
+
+const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(date);
+};
+
+const ItemLogBlock = ({ log }: { log: IItemUsageLog }) => {
+  return (
+    <ItemUsageLogStyled>
+      <IconBlockStyled>
+        <log.logo />
+      </IconBlockStyled>
+      <ItemUsageInfoStyled>
+        <ItemDateStyled>{formatDate(log.date)}</ItemDateStyled>
+        <ItemTitleStyled>{log.title}</ItemTitleStyled>
+      </ItemUsageInfoStyled>
+    </ItemUsageLogStyled>
+  );
+};
+
+const ItemUsageLogStyled = styled.div`
+  margin-top: 10px;
+  border-radius: 10px;
+  height: 90px;
+  border: 1px solid #d9d9d9;
+  display: flex;
+  align-items: center;
+`;
+
+const IconBlockStyled = styled.div`
+  display: flex;
+  width: 60px;
+  height: 60px;
+  border-radius: 10px;
+  background-color: var(--main-color);
+  justify-content: center;
+  align-items: center;
+  margin-left: 30px;
+  margin-right: 20px;
+  svg {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const ItemUsageInfoStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: flex-start;
+`;
+
+const ItemDateStyled = styled.div`
+  font-size: 16px;
+  word-spacing: -2px;
+  color: var(--gray-color);
+`;
+
+const ItemTitleStyled = styled.div`
+  font-size: 16px;
+  font-weight: 800;
+`;
+
+export default ItemLogBlock;
