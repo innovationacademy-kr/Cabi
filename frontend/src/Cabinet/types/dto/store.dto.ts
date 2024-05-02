3e3eafffe6c85bc268c381ed0494ca789bf8b8c2
jnkeniaem <jeanne.kim9@gmail.com>
2024-05-02 14:03:55 +0900

    [FE] FEAT: store 메인에서 쓰는 dto와 다른 형태의 IItem 추가#1604

--- frontend/src/Cabinet/types/dto/store.dto.ts
+++ frontend/src/Cabinet/types/dto/store.dto.ts
@@ -1,4 +1,12 @@
 export interface IItem {
+  Sku: string;
+  ItemName: string;
+  ItemPrice: number;
+  ItemType: string;
+  // ex) "3일"
+}
+
+export interface IItemStore {
   sku: string;
   // ex) "extension_31"
   itemPrice: number;
@@ -12,7 +20,7 @@ export interface IItemDetail {
   // ex) "연장권"
   itemTypes: string;
   // ex) "현재 대여 중인 사물함의 반납 기한을 3일, 15일 또는 30일 연장할 수 있습니다."
-  description: IItem[];
+  description: IItemStore[];
   // List<ItemDetailsDto>
   // TODO : 받는 데이터 형태 확인해서 필요시 description type 변경
 }
