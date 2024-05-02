4adb3f88a193ff8c146bac076549e59247854649
jnkeniaem <jeanne.kim9@gmail.com>
2024-05-02 13:54:11 +0900

    [FE] FEAT: 변경된 필드에 따라 store dto 변경 #1604

---
+++ frontend/src/Cabinet/types/dto/store.dto.ts
@@ -0,0 +1,18 @@
+export interface IItem {
+  sku: string;
+  // ex) "extension_31"
+  itemPrice: number;
+  // ex) -2000
+  itemDetails: string;
+  // ex) "3일"
+}
+
+export interface IItemDetail {
+  itemName: string;
+  // ex) "연장권"
+  itemTypes: string;
+  // ex) "현재 대여 중인 사물함의 반납 기한을 3일, 15일 또는 30일 연장할 수 있습니다."
+  description: IItem[];
+  // List<ItemDetailsDto>
+  // TODO : 받는 데이터 형태 확인해서 필요시 description type 변경
+}
