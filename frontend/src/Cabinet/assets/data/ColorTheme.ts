a4219678cc807d669ba9bd059b838f33cd9c47c6
jnkeniaem <jeanne.kim9@gmail.com>
2024-05-10 20:53:50 +0900

    [FE] REFACT: 컴포넌트 변수 중 gray 계열은 lightValues기준 아래로 갈수록 어두운 색으로 정렬#1604

--- frontend/src/Cabinet/assets/data/ColorTheme.ts
+++ frontend/src/Cabinet/assets/data/ColorTheme.ts
@@ -9,28 +9,29 @@ const lightValues = css`
 
   /* component variable */
   --bg-color: var(--ref-white);
-  --line-color: var(--ref-gray-400);
-  --normal-text-color: var(--ref-black);
   --white-text-with-bg-color: var(--ref-white);
   --card-content-bg-color: var(--ref-white);
-  --button-line-color: var(--sys-main-color);
-  --capsule-btn-border-color: var(--ref-gray-200);
-  --capsule-btn-hover-bg-color: var(--ref-transparent-purple-100);
-  --presentation-no-event-past-color: var(--ref-gray-200);
-  --presentation-no-event-cur-color: var(--bg-color);
-  --color-picker-bg-color: var(--bg-color);
-  --color-picker-hash-bg-color: var(--ref-gray-200);
-  --color-picker-input-color: var(--ref-gray-500);
-  --extension-card-active-btn-color: var(--sys-main-color);
-  --light-gray-line-btn-color: var(--ref-gray-400);
   --card-bg-color: var(--ref-gray-100);
+  --capsule-btn-border-color: var(--ref-gray-200);
+  --presentation-no-event-past-color: var(--ref-gray-200);
+  --color-picker-hash-bg-color: var(--ref-gray-200);
   --map-floor-color: var(--ref-gray-200);
   --service-man-title-border-btm-color: var(--ref-gray-300);
+  --line-color: var(--ref-gray-400);
+  --light-gray-line-btn-color: var(--ref-gray-400);
   --toggle-switch-off-bg-color: var(--ref-gray-400);
-  --presentation-card-speaker-name-color: var(--ref-gray-450);
+  --color-picker-input-color: var(--ref-gray-500);
   --gray-line-btn-color: var(--ref-gray-500);
   --pie-chart-label-text-color: var(--ref-gray-600);
   --notion-btn-text-color: var(--ref-gray-800);
+  --normal-text-color: var(--ref-black);
+
+  --button-line-color: var(--sys-main-color);
+  --capsule-btn-hover-bg-color: var(--ref-transparent-purple-100);
+  --presentation-no-event-cur-color: var(--bg-color);
+  --color-picker-bg-color: var(--bg-color);
+  --extension-card-active-btn-color: var(--sys-main-color);
+  --presentation-card-speaker-name-color: var(--ref-gray-450);
   --table-even-row-bg-color: var(--ref-purple-100);
   --presentation-table-even-row-bg-color: var(--ref-blue-100);
   --presentation-dropdown-select-color: var(--ref-blue-150);
@@ -57,43 +58,49 @@ const lightValues = css`
 
 // 다크모드 변수
 const darkValues = css`
-  --bg-color: var(--ref-gray-900);
-  --line-color: var(--ref-gray-500);
-  --normal-text-color: var(--ref-gray-100);
-  --white-text-with-bg-color: var(--ref-gray-100);
-  --card-content-bg-color: var(--ref-gray-550);
-  --button-line-color: var(--sys-main-color);
-  --capsule-btn-border-color: var(--ref-gray-600);
-  --capsule-btn-hover-bg-color: var(--ref-transparent-purple-200);
-  --presentation-no-event-past-color: var(--bg-color);
-  --presentation-no-event-cur-color: var(--ref-gray-600);
-  --color-picker-bg-color: var(--ref-gray-530);
-  --color-picker-hash-bg-color: var(--ref-gray-550);
-  --color-picker-input-color: var(--ref-gray-400);
-  --extension-card-active-btn-color: var(--gray-line-btn-color);
-  --light-gray-line-btn-color: var(--service-man-title-border-btm-color);
-  --card-bg-color: var(--ref-gray-700);
-  --map-floor-color: var(--ref-gray-700);
-  --service-man-title-border-btm-color: var(--ref-gray-600);
-  --presentation-card-speaker-name-color: var(--ref-gray-450);
-  --toggle-switch-off-bg-color: var(--ref-gray-500);
-  --gray-line-btn-color: var(--ref-gray-400);
-  --pie-chart-label-text-color: var(--ref-gray-300);
-  --notion-btn-text-color: var(--ref-gray-200);
-  --table-even-row-bg-color: var(--ref-purple-700);
-  --presentation-table-even-row-bg-color: var(--ref-blue-500);
-  --presentation-dropdown-select-color: var(--ref-blue-470);
-
+  /* system variable */
   --sys-main-color: var(--ref-purple-600);
   --sys-default-main-color: var(--ref-purple-600);
   --sys-default-mine-color: var(--ref-green-200);
   --sys-presentation-main-color: var(--ref-blue-430);
 
+  /* component variable */
+  --bg-color: var(--ref-gray-900);
+  --white-text-with-bg-color: var(--ref-gray-100);
+  --card-content-bg-color: var(--ref-gray-550);
+  --card-bg-color: var(--ref-gray-700);
+  --capsule-btn-border-color: var(--ref-gray-600);
+  --presentation-no-event-past-color: var(--bg-color);
+  --color-picker-hash-bg-color: var(--ref-gray-550);
+  --map-floor-color: var(--ref-gray-700);
+  --service-man-title-border-btm-color: var(--ref-gray-600);
+  --line-color: var(--ref-gray-500);
+  --light-gray-line-btn-color: var(--service-man-title-border-btm-color);
+  --toggle-switch-off-bg-color: var(--ref-gray-500);
+  --color-picker-input-color: var(--ref-gray-400);
+  --gray-line-btn-color: var(--ref-gray-400);
+  /* TODO : --color-picker-input-color랑 합치기 */
+  --pie-chart-label-text-color: var(--ref-gray-300);
+  --notion-btn-text-color: var(--ref-gray-200);
+  --normal-text-color: var(--ref-gray-100);
+
+  --button-line-color: var(--sys-main-color);
+  --capsule-btn-hover-bg-color: var(--ref-transparent-purple-200);
+  --presentation-no-event-cur-color: var(--ref-gray-600);
+  --color-picker-bg-color: var(--ref-gray-530);
+  --extension-card-active-btn-color: var(--gray-line-btn-color);
+  --presentation-card-speaker-name-color: var(--ref-gray-450);
+  --table-even-row-bg-color: var(--ref-purple-700);
+  --presentation-table-even-row-bg-color: var(--ref-blue-500);
+  --presentation-dropdown-select-color: var(--ref-blue-470);
+
+  /* cabinet */
   --mine-color: var(--ref-green-200);
   --available-color: var(--sys-main-color);
   --pending-color: var(--sys-main-color);
   --expired-color: var(--ref-red-200);
 
+  /* shadow */
   --modal-bg-shadow-color: var(--ref-black-shadow-300);
   --hover-box-shadow-color: var(--ref-black-shadow-400);
   --login-card-border-shadow-color: var(--ref-black-shadow-300);
