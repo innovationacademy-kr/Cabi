f948ee0f799cfe9a35f7232a7d570a235b41e5e0
jnkeniaem <jeanne.kim9@gmail.com>
2024-05-28 09:41:46 +0900

    [FE] FEAT: mockData로 코인 통계 띄우기#1604

--- frontend/src/Cabinet/components/AdminInfo/Chart/StoreHalfPieChart.tsx
+++ frontend/src/Cabinet/components/AdminInfo/Chart/StoreHalfPieChart.tsx
@@ -1,55 +1,23 @@
 import { ResponsivePie } from "@nivo/pie";
 import styled from "styled-components";
 
-interface IRentInfo {
-  floor: number;
-  total: number;
-  used: number;
-  overdue: number;
-  unused: number;
-  disabled: number;
+interface ICoinCollectInfo {
+  coinCount: number;
+  userCount: number;
 }
 
-type CabinetStatus = {
-  [used: string]: number;
-  overdue: number;
-  unused: number;
-  disabled: number;
+const convert = (data: ICoinCollectInfo[]) => {
+  let total = 0;
+  const obj = data.map((cur) => {
+    total += cur.userCount;
+    return { id: cur.coinCount + "회", value: cur.userCount };
+  });
+
+  obj.push({ id: "전체", value: total });
+  return obj;
 };
 
-type TextMap = {
-  [used: string]: string;
-  overdue: string;
-  unused: string;
-  disabled: string;
-};
-
-const convert = (data: IRentInfo[]) => {
-  const textMap: TextMap = {
-    used: "5회",
-    overdue: "10회",
-    unused: "15회",
-    disabled: "20회",
-    // disabled: "전체",
-  };
-  const obj: CabinetStatus = data.reduce(
-    ({ used, overdue, unused, disabled }, cur) => ({
-      used: used + cur.used,
-      overdue: overdue + cur.overdue,
-      unused: unused + cur.unused,
-      disabled: disabled + cur.disabled,
-    }),
-    { used: 0, overdue: 0, unused: 0, disabled: 0 }
-  );
-  return Object.keys(obj).map((key: string) => ({
-    id: textMap[key],
-    value: 30,
-    // value: obj[key],
-  }));
-};
-
-const StoreHalfPieChart = ({ data }: { data: IRentInfo[] }) => {
-  console.log("data : ", data);
+const StoreHalfPieChart = ({ data }: { data: ICoinCollectInfo[] }) => {
   return (
     <HalfPieChartStyled>
       <ResponsivePie
@@ -71,7 +39,7 @@ const StoreHalfPieChart = ({ data }: { data: IRentInfo[] }) => {
           "var(--ref-purple-300)",
           "var(--sys-main-color)",
           "var(--ref-purple-650)",
-          //   "var(--ref-purple-690)",
+          "var(--ref-purple-690)",
         ]}
         innerRadius={0.5}
         padAngle={0.7}
