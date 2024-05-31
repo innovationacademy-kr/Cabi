3861cca30ec2e4f5c39a764810321b62d2dcd520
chyo1 <hyowon3856@gmail.com>
2024-05-31 13:34:05 +0900

    [BE] admin controller 생성

--- backend/src/main/java/org/ftclub/cabinet/admin/statistics/controller/AdminStatisticsControllerV5.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/statistics/controller/AdminStatisticsControllerV5.java
@@ -1,5 +1,15 @@
 package org.ftclub.cabinet.admin.statistics.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.ftclub.cabinet.log.Logging;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v5/admin/statistics")
+@RequiredArgsConstructor
+@Logging
 public class AdminStatisticsControllerV5 {
+
 
 }
