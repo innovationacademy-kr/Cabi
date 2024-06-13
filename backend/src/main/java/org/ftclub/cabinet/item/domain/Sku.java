38196b935c7be940af358d791690c05c52dad33d
chyo1 <hyowon3856@gmail.com>
2024-06-13 14:03:03 +0900

    [BE] FEAT: 관리자 지급 보상 추가

--- backend/src/main/java/org/ftclub/cabinet/item/domain/Sku.java
+++ backend/src/main/java/org/ftclub/cabinet/item/domain/Sku.java
@@ -25,7 +25,12 @@ public enum Sku {
 	COIN_REWARD_1000("동전 줍기 20일 보상"),
 	COIN_REWARD_2000("동전 줍기 20일 보상"),
 
-	ADMIN_REWARD("관리자 보상");
+	ADMIN_REWARD_100("관리자 보상"),
+	ADMIN_REWARD_200("관리자 보상"),
+	ADMIN_REWARD_500("관리자 보상"),
+	ADMIN_REWARD_1000("관리자 보상"),
+	ADMIN_REWARD_2000("관리자 보상"),
+	;
 
 	private final String details;
 
