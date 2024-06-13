2e1770317aa480fe9eb0dfa6f54636c8bb9d8939
jiwon <dongchen119@naver.com>
2024-06-13 15:06:32 +0900

    [BE] FIX : 어드민으로 코인 지급 시 redis 반영 안되는 현상 수정:

--- backend/src/main/java/org/ftclub/cabinet/admin/item/service/AdminItemFacadeService.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/item/service/AdminItemFacadeService.java
@@ -15,9 +15,10 @@ import org.ftclub.cabinet.item.domain.Sku;
 import org.ftclub.cabinet.item.service.ItemCommandService;
 import org.ftclub.cabinet.item.service.ItemHistoryCommandService;
 import org.ftclub.cabinet.item.service.ItemHistoryQueryService;
-import org.ftclub.cabinet.item.service.ItemPolicyService;
 import org.ftclub.cabinet.item.service.ItemQueryService;
+import org.ftclub.cabinet.item.service.ItemRedisService;
 import org.ftclub.cabinet.mapper.ItemMapper;
+import org.ftclub.cabinet.utils.lock.LockUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -31,8 +32,9 @@ public class AdminItemFacadeService {
 	private final ItemCommandService itemCommandService;
 	private final ItemHistoryCommandService itemHistoryCommandService;
 	private final ItemHistoryQueryService itemHistoryQueryService;
-	private final ItemPolicyService itemPolicyService;
 	private final ItemMapper itemMapper;
+
+	private final ItemRedisService itemRedisService;
 
 	@Transactional
 	public void createItem(Integer Price, Sku sku, ItemType type) {
@@ -45,6 +47,13 @@ public class AdminItemFacadeService {
 		LocalDateTime now = null;
 		if (item.getPrice() > 0) {
 			now = LocalDateTime.now();
+			userIds.forEach(userId -> LockUtil.lockRedisCoin(userId, () -> {
+				long coinAmount = itemRedisService.getCoinAmount(userId);
+				itemRedisService.saveCoinCount(userId, coinAmount + item.getPrice());
+
+				long totalCoinSupply = itemRedisService.getTotalCoinSupply();
+				itemRedisService.saveTotalCoinSupply(totalCoinSupply + item.getPrice());
+			}));
 		}
 		itemHistoryCommandService.createItemHistories(userIds, item.getId(), now);
 	}
