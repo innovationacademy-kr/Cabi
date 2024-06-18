538a5f4160aab4deadb0b593128ddc625c1e596e
chyo1 <hyowon3856@gmail.com>
2024-06-18 15:36:58 +0900

    [BE] FIX: 아이템 사용 기록에 출석 연장권 사용 내역 보이게 수정

--- backend/src/main/java/org/ftclub/cabinet/item/repository/ItemHistoryRepository.java
+++ backend/src/main/java/org/ftclub/cabinet/item/repository/ItemHistoryRepository.java
@@ -27,7 +27,7 @@ public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> 
 			+ "JOIN FETCH ih.item i "
 			+ "WHERE ih.userId = :userId "
 			+ "AND ih.usedAt IS NOT NULL "
-			+ "AND i.price < 0 "
+			+ "AND i.price <= 0 "
 			+ "ORDER BY ih.usedAt DESC",
 			countQuery = "SELECT COUNT(ih) "
 					+ "FROM ItemHistory ih "
