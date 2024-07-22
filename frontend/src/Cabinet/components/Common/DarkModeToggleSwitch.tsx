22593b5fcaabd03fcc187c262fb82952118d2b58
gykoh42 <karmakoh42@naver.com>
2024-07-23 05:32:31 +0900

    [FE] FEAT: DarkModeToggleSwitch 의 초기값을 기기 설정값으로 적용하는 로직 추가 #1649

--- frontend/src/Cabinet/components/Common/DarkModeToggleSwitch.tsx
+++ frontend/src/Cabinet/components/Common/DarkModeToggleSwitch.tsx
@@ -1,53 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as MoonIcon } from "@/Cabinet/assets/images/moonAdmin.svg";
 import { ReactComponent as SunIcon } from "@/Cabinet/assets/images/sunAdmin.svg";
+import {
+  DisplayStyleToggleType,
+  DisplayStyleType,
+} from "@/Cabinet/types/enum/displayStyle.type.enum";
 
 interface ToggleSwitchInterface {
   id: string;
-  onChange?: (checked: boolean) => void;
-  checked: boolean;
-  disabled?: boolean;
 }
 
-const DarkModeToggleSwitch = ({
-  id,
-  onChange,
-  checked = false,
-  disabled,
-}: ToggleSwitchInterface) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newState = event.target.checked;
-    if (onChange) onChange(newState);
+const getInitialDisplayStyle = (
+  savedDisplayStyleToggle: DisplayStyleToggleType,
+  darkModeQuery: MediaQueryList
+) => {
+  if (savedDisplayStyleToggle === DisplayStyleToggleType.LIGHT)
+    return DisplayStyleType.LIGHT;
+  else if (savedDisplayStyleToggle === DisplayStyleToggleType.DARK)
+    return DisplayStyleType.DARK;
+
+  if (darkModeQuery.matches) {
+    return DisplayStyleType.DARK;
+  }
+  return DisplayStyleType.LIGHT;
+};
+
+const DarkModeToggleSwitch = ({ id }: ToggleSwitchInterface) => {
+  const savedDisplayStyleToggle =
+    (localStorage.getItem("display-style-toggle") as DisplayStyleToggleType) ||
+    DisplayStyleToggleType.DEVICE;
+  const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const initialDisplayStyle = getInitialDisplayStyle(
+    savedDisplayStyleToggle,
+    darkModeQuery
+  );
+  const [darkMode, setDarkMode] = useState<DisplayStyleType>(
+    initialDisplayStyle as DisplayStyleType
+  );
+  const [toggleType, setToggleType] = useState<DisplayStyleToggleType>(
+    savedDisplayStyleToggle
+  );
+
+  const setColorsAndLocalStorage = (toggleType: DisplayStyleToggleType) => {
+    setToggleType(toggleType);
+    localStorage.setItem("display-style-toggle", toggleType);
+    setDarkMode(
+      toggleType === DisplayStyleToggleType.LIGHT
+        ? DisplayStyleType.LIGHT
+        : toggleType === DisplayStyleToggleType.DARK
+        ? DisplayStyleType.DARK
+        : darkModeQuery.matches
+        ? DisplayStyleType.DARK
+        : DisplayStyleType.LIGHT
+    );
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newToggleType =
+      toggleType === DisplayStyleToggleType.LIGHT
+        ? DisplayStyleToggleType.DARK
+        : DisplayStyleToggleType.LIGHT;
+    setColorsAndLocalStorage(newToggleType);
+  };
+
+  useEffect(() => {
+    darkModeQuery.addEventListener("change", (event) =>
+      setDarkMode(
+        event.matches ? DisplayStyleType.DARK : DisplayStyleType.LIGHT
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    document.body.setAttribute("display-style", darkMode);
+  }, [darkMode]);
+
   return (
-    <ToggleSwitchContainerStyled disabled={disabled}>
+    <ToggleSwitchContainerStyled>
       <InputStyled
         type="checkbox"
         id={id}
-        checked={checked}
+        checked={darkMode === DisplayStyleType.DARK}
         onChange={handleChange}
-        disabled={disabled}
       />
-      <ToggleSwitchStyled htmlFor={id} checked={checked} disabled={disabled}>
+      <ToggleSwitchStyled
+        htmlFor={id}
+        checked={darkMode === DisplayStyleType.DARK}
+      >
         <SunWrapperStyled>
           <SunIcon />
         </SunWrapperStyled>
         <MoonWrapperStyled>
           <MoonIcon />
         </MoonWrapperStyled>
-        <ToggleKnobStyled checked={checked} />
+        <ToggleKnobStyled checked={darkMode === DisplayStyleType.DARK} />
       </ToggleSwitchStyled>
     </ToggleSwitchContainerStyled>
   );
 };
 
-const ToggleSwitchContainerStyled = styled.div<{ disabled?: boolean }>`
+const ToggleSwitchContainerStyled = styled.div`
   display: inline-block;
   position: relative;
   margin-right: 10px;
-  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
 `;
 
 const InputStyled = styled.input.attrs({ type: "checkbox" })`
@@ -57,11 +113,8 @@ const InputStyled = styled.input.attrs({ type: "checkbox" })`
   height: 0;
 `;
 
-const ToggleSwitchStyled = styled.label<{
-  checked: boolean;
-  disabled?: boolean;
-}>`
-  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+const ToggleSwitchStyled = styled.label<{ checked: boolean }>`
+  cursor: pointer;
   display: inline-block;
   position: relative;
   background: ${(props) =>
