23e8268a9f5febb2700e20859d28f4b5b9828a7e
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2024-07-25 12:09:46 +0900

    [FE] FIX: user 식별을 위해 axios.log 에서 user 을 가져오도록 추가 #1665

--- frontend/src/Cabinet/api/axios/axios.log.ts
+++ frontend/src/Cabinet/api/axios/axios.log.ts
@@ -1,16 +1,26 @@
 import { captureException } from "@sentry/react";
 import { AxiosError } from "axios";
+import ErrorType from "@/Cabinet/types/enum/error.type.enum";
+import { getCookie } from "@/Cabinet/api/react_cookie/cookies";
 
-export function logAxiosError(error: AxiosError, type: string, errorMsg : string) {
-	error.message =  "[Axios] " + errorMsg;
-	captureException(error, {
-	  tags: {
-		// user:
-		api: error.response?.config.url,
-		httpMethod: error.config?.method?.toUpperCase(),
-		httpStatusCode: (error.response?.status || "").toString(),
-	  },
-	  level: "error",
-	  extra: { type: type },
-	});
-  }
+const token = getCookie("admin_access_token") ?? getCookie("access_token");
+const decodedPayload = JSON.parse(atob(token.split(".")[1]));
+const user = decodedPayload.name;
+
+export function logAxiosError(
+  error: AxiosError,
+  type: ErrorType,
+  errorMsg: string
+) {
+  error.message = "[Axios] " + errorMsg;
+  captureException(error, {
+    tags: {
+      user,
+      api: error.response?.config.url,
+      httpMethod: error.config?.method?.toUpperCase(),
+      httpStatusCode: (error.response?.status ?? "").toString(),
+    },
+    level: "error",
+    extra: { type: type },
+  });
+}
