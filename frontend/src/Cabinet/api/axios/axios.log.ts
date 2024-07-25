65bc722628453c63cd0612f21750571a6a7c79bb
jnkeniaem <jeanne.kim9@gmail.com>
2024-07-25 12:03:40 +0900

    [FE] REFACT: logAxiosError 함수 컨벤션대로 화살표 함수로 정의 방법 변경 #1665

--- frontend/src/Cabinet/api/axios/axios.log.ts
+++ frontend/src/Cabinet/api/axios/axios.log.ts
@@ -1,16 +1,20 @@
 import { captureException } from "@sentry/react";
 import { AxiosError } from "axios";
 
-export function logAxiosError(error: AxiosError, type: string, errorMsg : string) {
-	error.message =  "[Axios] " + errorMsg;
-	captureException(error, {
-	  tags: {
-		// user:
-		api: error.response?.config.url,
-		httpMethod: error.config?.method?.toUpperCase(),
-		httpStatusCode: (error.response?.status || "").toString(),
-	  },
-	  level: "error",
-	  extra: { type: type },
-	});
-  }
+export const logAxiosError = (
+  error: AxiosError,
+  type: string,
+  errorMsg: string
+) => {
+  error.message = "[Axios] " + errorMsg;
+  captureException(error, {
+    tags: {
+      // user:
+      api: error.response?.config.url,
+      httpMethod: error.config?.method?.toUpperCase(),
+      httpStatusCode: (error.response?.status || "").toString(),
+    },
+    level: "error",
+    extra: { type: type },
+  });
+};
