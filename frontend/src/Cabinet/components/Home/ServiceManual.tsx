2d5b8639eba388b9fdedd104ac9b2fdcbe8a0d7e
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2024-08-27 14:10:29 +0900

    [FE] FEAT: CSS clip-path 를 사용해 STORE MandualContentBox 생성 #1681

--- frontend/src/Cabinet/components/Home/ServiceManual.tsx
+++ frontend/src/Cabinet/components/Home/ServiceManual.tsx
@@ -38,8 +38,12 @@ const ServiceManual = ({
         <p className="subtitle">
           당신의 사물함
           <br />
-        당신의 방식으로,
+          당신의 방식으로,
         </p>
+
+        <TicketWrapperStyled>
+          <TicketStyled />
+        </TicketWrapperStyled>
 
         <InfoSectionStyled className="section">
           <article
@@ -82,7 +86,7 @@ const ServiceManual = ({
           >
             {/* <MaunalContentBox contentStatus={ContentStatus.CLUB} />
              */}
-              <MaunalContentBox contentStatus={ContentStatus.IN_SESSION} />
+            <MaunalContentBox contentStatus={ContentStatus.IN_SESSION} />
           </article>
         </InfoSectionStyled>
 
@@ -127,6 +131,78 @@ const ServiceManual = ({
     </WrapperStyled>
   );
 };
+
+const TicketWrapperStyled = styled.div`
+  width: 620px;
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    transform: translateY(-5px);
+    filter: drop-shadow(10px 10px 50px rgba(0, 0, 0, 0.2));
+  }
+`;
+// 16.794
+const TicketStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 24px;
+  font-weight: bold;
+  width: 620px;
+  height: 300px;
+  position: relative;
+  background: linear-gradient(
+    to bottom,
+    var(--ref-purple-400),
+    var(--ref-purple-600)
+  );
+  border-radius: 40px;
+  clip-path: path(
+    "M 0 175
+    A 25 25 1 0 0 0 125
+    L 0 0
+    L 396.56 0
+    L 413.354 16.794
+    L 430.148 0
+    L 620 0
+    L 620 300
+    L 430.148 300
+    L 413.354 283.206
+    L 396.56 300
+    L 0 300
+    Z"
+  );
+  /* Explanation of path:
+  - M 0 175: Move to (0, 175)
+  - A 25 25 1 0 0 0 125: Draw an arc with radius 25, starting from (0, 175) to (0, 125) // radius-x, radius-y, x-axis-rotation, large-arc-flag, sweep-flag, x, y
+  - L 0 0: Draw a line from (0, 125) to (0, 0)
+  - L 396.56 0: Draw a line from (0, 0) to (396.56, 0)
+  - L 413.354 16.794: Draw a line from (396.56, 0) to (413.354, 16.794)
+  - L 430.148 0: Draw a line from (413.354, 16.794) to (430.148, 0)
+  - L 620 0: Draw a line from (430.148, 0) to (620, 0)
+  - L 620 300: Draw a line from (620, 0) to (620, 300)
+  - L 430.148 300: Draw a line from (620, 300) to (430.148, 300)
+  - L 413.354 283.206: Draw a line from (430.148, 300) to (413.354, 283.206)
+  - L 396.56 300: Draw a line from (413.354, 283.206) to (396.56, 300)
+  - L 0 300: Draw a line from (396.56, 300) to (0, 300)
+  - Z: Close the path
+  */
+  &:after {
+    content: "";
+    position: absolute;
+    top: 25px;
+    right: 32.99%; /* 2/3 point */
+    height: 90%;
+    width: 4px;
+    background-image: linear-gradient(
+      to bottom,
+      white 33%,
+      rgba(255, 255, 255, 0) 0%
+    );
+    background-position: right;
+    background-size: 10px 30px;
+    background-repeat: repeat-y;
+  }
+`;
 
 const WrapperStyled = styled.div`
   display: flex;
