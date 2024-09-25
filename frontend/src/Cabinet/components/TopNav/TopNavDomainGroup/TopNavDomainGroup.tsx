8c84ec402c4bb666917b886c4da7dfd3951040d6
gykoh42 <karmakoh42@naver.com>
2024-09-25 20:41:59 +0900

    [FE] FEAT: 불필요한 import 문 삭제 #1649

--- frontend/src/Cabinet/components/TopNav/TopNavDomainGroup/TopNavDomainGroup.tsx
+++ frontend/src/Cabinet/components/TopNav/TopNavDomainGroup/TopNavDomainGroup.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import DarkModeToggleSwitch from "@/Cabinet/components/Common/DarkModeToggleSwitch";
