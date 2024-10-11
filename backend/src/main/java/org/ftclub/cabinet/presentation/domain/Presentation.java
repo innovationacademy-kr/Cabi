b9a9afbe53772f581cbb565b916f2d9fc2393f9d
chyo1 <hyowon3856@gmail.com>
2024-10-11 18:15:16 +0900

    [BE] FEAT:presentation entity에 Table annotation 추가

--- backend/src/main/java/org/ftclub/cabinet/presentation/domain/Presentation.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/domain/Presentation.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ import org.ftclub.cabinet.user.domain.User;
 
 @Entity
 @Getter
+@Table(name = "PRESENTATION")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Presentation {
 
