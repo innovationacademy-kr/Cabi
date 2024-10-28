c2875962271971d4fb16b9c736b54525be26ed91
Jihyun Kim <ksk208105@gmail.com>
2024-10-28 16:03:48 +0900

    [FE] REFACTOR: 가로 스크롤 제한 추가 및 버튼 컴포넌트 컨벤션 적용 #1691

--- frontend/src/Cabinet/components/Home/ServiceManual.tsx
+++ frontend/src/Cabinet/components/Home/ServiceManual.tsx
@@ -29,9 +29,9 @@ const ServiceManual = ({
         <h1 className="title">
           Cabi <span>이용 안내서</span>
         </h1>
-        <NotionBtn className="button" onClick={openNotionLink}>
+        <NotionBtnStyled className="button" onClick={openNotionLink}>
           상세보기
-        </NotionBtn>
+        </NotionBtnStyled>
       </TitleContainerStyled>
 
       <WrapSectionStyled>
@@ -112,6 +112,7 @@ const WrapperStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  overflow-x: hidden;
   padding: 60px 0;
 `;
 
@@ -140,7 +141,7 @@ const TitleContainerStyled = styled.div`
   }
 `;
 
-const NotionBtn = styled.button`
+const NotionBtnStyled = styled.button`
   width: 120px;
   height: 40px;
   border-radius: 8px;
