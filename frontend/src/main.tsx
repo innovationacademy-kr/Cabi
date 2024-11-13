a0f62daadcc8fd37187335432cd9c5731283ed5f
jnkeniaem <jeanne.kim9@gmail.com>
2024-11-13 17:45:25 +0900

    [FE] ETC: VITE_BE_HOST로 dev 브랜치 식별되나 테스트

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,10 +14,15 @@ import "@/index.css";
 import App from "@/App";
 import { GlobalStyle } from "@/Cabinet/assets/data/ColorTheme";
 
+console.log(import.meta.env.VITE_BE_HOST.includes("dev"));
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   environment:
-    import.meta.env.VITE_IS_LOCAL === "true" ? "local" : "production",
+    import.meta.env.VITE_IS_LOCAL === "true"
+      ? "local"
+      : import.meta.env.VITE_BE_HOST.includes("dev")
+      ? "development"
+      : "production",
   release: "^8.18.0",
   integrations: [
     // See docs for support of different versions of variation of react router
