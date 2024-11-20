59fa238121302b8b6bb36e0c25e420de2ab961c8
jnkeniaem <jeanne.kim9@gmail.com>
2024-11-20 16:36:11 +0900

    [FE] HOTFIX: 루트 url에 접속시 /home으로 이동안되는 버그 해결

--- frontend/src/Cabinet/components/TopNav/TopNav.container.tsx
+++ frontend/src/Cabinet/components/TopNav/TopNav.container.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { currentBuildingNameState } from "@/Cabinet/recoil/atoms";
 import { buildingsState } from "@/Cabinet/recoil/selectors";
 import TopNav from "@/Cabinet/components/TopNav/TopNav";
-import { staticColNumData } from "@/Cabinet/assets/data/sectionColNumData";
 import useMenu from "@/Cabinet/hooks/useMenu";
 
 const TopNavContainer: React.FC = () => {
@@ -14,8 +12,6 @@ const TopNavContainer: React.FC = () => {
   );
   const buildingsList = useRecoilValue<Array<string>>(buildingsState);
   const { toggleLeftNav } = useMenu();
-  const navigator = useNavigate();
-  const isLocation = useLocation();
 
   const onClickLogo = () => {
     toggleLeftNav();
@@ -25,13 +21,6 @@ const TopNavContainer: React.FC = () => {
     if (buildingsList.length === 0) return;
     setCurrentBuildingName(buildingsList[0]);
   }, [buildingsList]);
-
-  useEffect(() => {
-    if (currentBuildingName === undefined) return;
-    else if (currentBuildingName === staticColNumData[0].building) {
-      navigator(isLocation);
-    }
-  }, [currentBuildingName]);
 
   return (
     <TopNav
