32e59cc3e8abe1e6a1bc3dd9f2b5af67067374b9
jnkeniaem <jeanne.kim9@gmail.com>
2024-11-28 16:16:08 +0900

    [FE] ETC: gray reference token 명도차이에 따라 숫자 재배치 #1714

--- frontend/src/Cabinet/pages/admin/AdminStorePage.tsx
+++ frontend/src/Cabinet/pages/admin/AdminStorePage.tsx
@@ -341,7 +341,7 @@ const CoinCollectTitleWrapperStyled = styled.div`
   }
 
   & > div > span {
-    color: var(--ref-gray-400);
+    color: var(--ref-gray-300);
     height: 20px;
     line-height: 18px;
   }
