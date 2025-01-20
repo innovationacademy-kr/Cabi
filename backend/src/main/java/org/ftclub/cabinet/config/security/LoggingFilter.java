c0b5cf1be97b82761d19494f653c40532faea7d0
saewoo1 <wooozooo1123@icloud.com>
2025-01-20 15:44:28 +0900

    FEAT: 모든 요청에 대한 loggingFilter

--- backend/src/main/java/org/ftclub/cabinet/config/security/LoggingFilter.java
+++ backend/src/main/java/org/ftclub/cabinet/config/security/LoggingFilter.java
@@ -1,31 +1,41 @@
 package org.ftclub.cabinet.config.security;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
+//@Component 이후에 addFilterAfter 로 적용
 @Slf4j
 public class LoggingFilter extends OncePerRequestFilter {
+
+	private static final List<String> IP_HEADERS = Arrays.asList("X-Forwarded-For",
+			"Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR");
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 			FilterChain filterChain) throws ServletException, IOException {
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String requestURI = request.getRequestURI();
-		String userName = (authentication != null) ? authentication.getName() : "anonymous";
-		String ipAddress = request.getRemoteAddr();
+		String ipAddress = getClientIpAddr(request);
 
-		log.info("Request URL: {}, UserName: {}, IP: {}", requestURI, userName, ipAddress);
+		log.debug("IP: {}, action: {}, status: {}", ipAddress, requestURI, response.getStatus());
 
 		filterChain.doFilter(request, response);
+	}
+
+	private String getClientIpAddr(HttpServletRequest request) {
+		return IP_HEADERS.stream()
+				.map(request::getHeader)
+				.filter(Objects::nonNull)
+				.filter(ip -> !ip.isEmpty() && !ip.equalsIgnoreCase("unknown"))
+				.findFirst()
+				.orElseGet(request::getRemoteAddr);
 	}
 }
