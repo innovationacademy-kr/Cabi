815bc1a7c877990a0de6657107aca9bcbce171b8
saewoo1 <wooozooo1123@icloud.com>
2025-01-26 11:25:42 +0900

    REFACTOR: loadUser 에서는 단순히 유저 정보를 받아오도록 변경

--- backend/src/main/java/org/ftclub/cabinet/config/security/CustomOAuth2UserService.java
+++ backend/src/main/java/org/ftclub/cabinet/config/security/CustomOAuth2UserService.java
@@ -1,17 +1,8 @@
 package org.ftclub.cabinet.config.security;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ftclub.cabinet.auth.domain.FtRole;
 import org.ftclub.cabinet.exception.ExceptionStatus;
-import org.ftclub.cabinet.utils.DateUtil;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -23,16 +14,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-	private final ObjectMapper objectMapper = new ObjectMapper();
-	@Value("${spring.security.oauth2.client.registration.ft.client-name}")
-	private String ftProvider;
-	@Value("${spring.security.oauth2.client.registration.google.client-name}")
-	private String googleProvider;
-
 	/**
-	 * oauth 로그인을 마친 유저의 프로필을 해당 사이트로부터 받아오고,
+	 * oauth 로그인을 마친 유저의 프로필을 해당 사이트로부터 받아온 후
 	 * <p>
-	 * SecurityContextHolder 에 정보를 저장합니다.
+	 * Authentication 객체를 반환합니다.
 	 * <p>
 	 * 메서드들은 application.yml에 명시된 값들을 호출합니다
 	 *
@@ -44,67 +29,15 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		try {
 			OAuth2User oAuth2User = super.loadUser(userRequest);
-			JsonNode rootNode =
-					objectMapper.convertValue(oAuth2User.getAttributes(), JsonNode.class);
 			// oauth 로그인 위치 ex) google, ft
 			String provider = userRequest.getClientRegistration().getRegistrationId();
-			// yml에 명시된 정보를 받아오는 키워드 ex) 42 oauth -> login, google -> sub
-			String userNameAttribute =
-					userRequest.getClientRegistration()
-							.getProviderDetails()
-							.getUserInfoEndpoint()
-							.getUserNameAttributeName();
-
 			// 고유 id ex) 42 -> intraName, google -> uid
-			String userId = rootNode.path(userNameAttribute).asText();
-
-			if (provider.equals(ftProvider)) {
-				return generateFtOauthProfile(
-						rootNode, oAuth2User.getAttributes(), provider, userId);
-			}
+			String userId = oAuth2User.getName();
 
 			return new CustomOauth2User(provider, userId, oAuth2User.getAttributes());
 		} catch (OAuth2AuthenticationException e) {
 			log.error("OAuth2 Authentication Exception : {}", e.getMessage(), e);
 			throw ExceptionStatus.INVALID_AUTHORIZATION.asSpringSecurityException();
 		}
-	}
-
-	// 블랙홀, 롤, userId, email
-	private CustomOauth2User generateFtOauthProfile(JsonNode rootNode,
-			Map<String, Object> oauthAttribute, String provider, String userId) {
-		LocalDateTime blackHoledAt = determineBlackHoledAt(rootNode);
-		FtRole role = determineFtRole(rootNode, blackHoledAt);
-		Map<String, Object> attribute = new HashMap<>();
-		attribute.put("email", oauthAttribute.get("email"));
-		attribute.put("blackHoledAt", blackHoledAt);
-		attribute.put("role", role);
-
-		return new CustomOauth2User(provider, userId, attribute);
-	}
-
-	private FtRole determineFtRole(JsonNode rootNode, LocalDateTime blackHoledAt) {
-		boolean isUserStaff = rootNode.get("staff?").asBoolean();
-		boolean isActive = rootNode.get("active?").asBoolean();
-
-		if (!isActive && blackHoledAt.isAfter(LocalDateTime.now())) {
-			return FtRole.AGU;
-		}
-		if (!isActive) {
-			return FtRole.INACTIVE;
-		}
-		if (isUserStaff) {
-			return FtRole.STAFF;
-		}
-		return FtRole.USER;
-	}
-
-	private LocalDateTime determineBlackHoledAt(JsonNode rootNode) {
-		return Optional.ofNullable(rootNode.path("cursus_users").path(1).path("blackholed_at"))
-				.filter(JsonNode::isTextual)
-				.map(JsonNode::asText)
-				.filter(text -> !text.isEmpty())
-				.map(DateUtil::convertStringToDate)
-				.orElse(null);
 	}
 }
