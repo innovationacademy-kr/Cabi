5d2e76cec9d736941353a4754b92df0db2a11090
soonvro <soonvro@gmail.com>
2025-02-18 17:19:43 +0900

    [BE] REFACTOR: AuthenticationPrincipal, UserInfoDto 적용

--- backend/src/main/java/org/ftclub/cabinet/lent/controller/LentController.java
+++ backend/src/main/java/org/ftclub/cabinet/lent/controller/LentController.java
@@ -2,8 +2,7 @@ package org.ftclub.cabinet.lent.controller;
 
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.ftclub.cabinet.auth.domain.AuthGuard;
-import org.ftclub.cabinet.auth.domain.AuthLevel;
+import org.ftclub.cabinet.config.security.UserInfoDto;
 import org.ftclub.cabinet.dto.CabinetInfoRequestDto;
 import org.ftclub.cabinet.dto.LentEndMemoDto;
 import org.ftclub.cabinet.dto.LentHistoryPaginationDto;
@@ -16,6 +15,7 @@ import org.ftclub.cabinet.user.domain.UserSession;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,9 +39,8 @@ public class LentController {
 	 * @param cabinetId 대여할 사물함 ID
 	 */
 	@PostMapping("/cabinets/{cabinetId}")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public void startLentCabinet(
-			@UserSession UserSessionDto user,
+			@AuthenticationPrincipal UserInfoDto user,
 			@PathVariable Long cabinetId) {
 		lentFacadeService.startLentCabinet(user.getUserId(), cabinetId);
 	}
@@ -54,9 +53,8 @@ public class LentController {
 	 * @param shareCodeDto 공유 사물함 초대 코드
 	 */
 	@PostMapping("/cabinets/share/{cabinetId}")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public void startLentShareCabinet(
-			@UserSession UserSessionDto user,
+			@AuthenticationPrincipal UserInfoDto user,
 			@PathVariable Long cabinetId,
 			@Valid @RequestBody ShareCodeDto shareCodeDto) {
 		lentFacadeService.startLentShareCabinet(user.getUserId(), cabinetId,
@@ -70,9 +68,8 @@ public class LentController {
 	 * @param cabinetId 대여 취소할 사물함 ID
 	 */
 	@PatchMapping("/cabinets/share/cancel/{cabinetId}")
-	@AuthGuard(level = AuthLevel.USER_OR_ADMIN)
 	public void cancelLentShareCabinet(
-			@UserSession UserSessionDto user,
+			@AuthenticationPrincipal UserInfoDto user,
 			@PathVariable Long cabinetId) {
 		lentFacadeService.cancelShareCabinetLent(user.getUserId(), cabinetId);
 	}
@@ -83,9 +80,8 @@ public class LentController {
 	 * @param userSessionDto 사용자 세션
 	 */
 	@PatchMapping("/return")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public void endLent(
-			@UserSession UserSessionDto userSessionDto) {
+			@AuthenticationPrincipal UserInfoDto userSessionDto) {
 		lentFacadeService.endUserLent(userSessionDto.getUserId(), null);
 	}
 
@@ -98,9 +94,8 @@ public class LentController {
 	 * @param lentEndMemoDto 반납 메모
 	 */
 	@PatchMapping("/return-memo")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public void endLentWithMemo(
-			@UserSession UserSessionDto userSessionDto,
+			@AuthenticationPrincipal UserInfoDto userSessionDto,
 			@Valid @RequestBody LentEndMemoDto lentEndMemoDto) {
 		lentFacadeService.endUserLent(userSessionDto.getUserId(), lentEndMemoDto.getCabinetMemo());
 	}
@@ -114,14 +109,14 @@ public class LentController {
 	 * @param cabinetInfoRequestDto 수정할 사물함 정보
 	 */
 	@PatchMapping("/me/cabinet")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public void updateCabinetInfo(
-			@UserSession UserSessionDto user,
+			@AuthenticationPrincipal UserInfoDto user,
 			@Valid @RequestBody CabinetInfoRequestDto cabinetInfoRequestDto) {
 		lentFacadeService.updateLentCabinetInfo(user.getUserId(),
 				cabinetInfoRequestDto.getTitle(), cabinetInfoRequestDto.getMemo());
 	}
 
+	// TODO: UserSession을 AuthenticationPrincipal로 변경
 	/**
 	 * 내 사물함 대여 정보 조회
 	 *
@@ -129,7 +124,6 @@ public class LentController {
 	 * @return 내 사물함 대여 정보 HTTP 응답
 	 */
 	@GetMapping("/me")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public ResponseEntity<MyCabinetResponseDto> getMyLentInfo(
 			@UserSession UserSessionDto user) {
 		MyCabinetResponseDto myCabinetResponseDto = lentFacadeService.getMyLentInfo(user);
@@ -139,6 +133,7 @@ public class LentController {
 		return ResponseEntity.status(HttpStatus.OK).body(myCabinetResponseDto);
 	}
 
+	// TODO: UserSession을 AuthenticationPrincipal로 변경
 	/**
 	 * 내 대여 이력 조회
 	 *
@@ -147,7 +142,6 @@ public class LentController {
 	 * @return 내 대여 이력
 	 */
 	@GetMapping("/me/histories")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public LentHistoryPaginationDto getMyLentLog(
 			@UserSession UserSessionDto user,
 			@Valid Pageable pageable) {
@@ -161,9 +155,8 @@ public class LentController {
 	 * @param cabinetId 이동할 사물함의 ID
 	 */
 	@PostMapping("/swap/{cabinetId}")
-	@AuthGuard(level = AuthLevel.USER_ONLY)
 	public void swap(
-			@UserSession UserSessionDto user,
+			@AuthenticationPrincipal UserInfoDto user,
 			@PathVariable Long cabinetId) {
 		lentFacadeService.swapPrivateCabinet(user.getUserId(), cabinetId);
 	}
