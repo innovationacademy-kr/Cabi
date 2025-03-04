108529ab45ce9546a50987ea59af15a1f113ac15
gykoh42 <karmakoh42@naver.com>
2025-03-04 14:00:16 +0900

    [FE] FEAT: login url 변경 #1726

--- frontend/src/Cabinet/pages/admin/AdminLoginPage.tsx
+++ frontend/src/Cabinet/pages/admin/AdminLoginPage.tsx
@@ -1,8 +1,8 @@
-import AdminLoginTemplate from "@/Cabinet/components/Login/AdminLoginTemplate";
 import "@/Cabinet/assets/css/loginPage.css";
+import AdminLoginTemplate from "@/Cabinet/components/Login/AdminLoginTemplate";
 
 const LoginPage = () => {
-  const url = `${import.meta.env.VITE_BE_HOST}/v4/admin/auth/login`;
+  const url = `${import.meta.env.VITE_BE_HOST}/oauth2/authorization/google`;
 
   return (
     <AdminLoginTemplate
