ee0134e0b03517572134a58f5d5daae0f8fadc54
saewoo1 <wooozooo1123@icloud.com>
2025-03-10 14:41:29 +0900

    REFACTOR: BlackList 관리 시 user, admin 토큰 keySuffix 분리

--- backend/src/main/java/org/ftclub/cabinet/jwt/service/JwtRedisService.java
+++ backend/src/main/java/org/ftclub/cabinet/jwt/service/JwtRedisService.java
@@ -14,23 +14,36 @@ public class JwtRedisService {
 	private final JwtRedis jwtRedis;
 
 	public boolean isUsedAccessToken(Long userId, String accessToken) {
-		String expiredAccessToken = jwtRedis.getExpiredAccessToken(String.valueOf(userId));
+		String expiredAccessToken = jwtRedis.getUserAccessToken(String.valueOf(userId));
 
 		return accessToken.equals(expiredAccessToken);
 	}
 
 	public boolean isUsedRefreshToken(Long userId, String refreshToken) {
-		String refreshTokenInRedis = jwtRedis.getRefreshToken(String.valueOf(userId));
+		String refreshTokenInRedis = jwtRedis.getUserRefreshToken(String.valueOf(userId));
 
 		return refreshToken.equals(refreshTokenInRedis);
 	}
 
-	public void addUsedTokens(Long userId, String accessToken, String refreshToken) {
-		jwtRedis.saveExpiredAccessToken(String.valueOf(userId), accessToken);
-		jwtRedis.saveRefreshToken(String.valueOf(userId), refreshToken);
+	public void addUserUsedTokens(Long userId, String accessToken, String refreshToken) {
+		jwtRedis.saveUserAccessToken(String.valueOf(userId), accessToken);
+		jwtRedis.saveUserRefreshToken(String.valueOf(userId), refreshToken);
 	}
 
-	public void addRefreshToken(Long userId, String refreshToken) {
-		jwtRedis.saveRefreshToken(String.valueOf(userId), refreshToken);
+	public boolean isUsedAdminAccessToken(Long adminId, String accessToken) {
+		String expiredAccessToken = jwtRedis.getAdminAccessToken(String.valueOf(adminId));
+
+		return expiredAccessToken.equals(accessToken);
+	}
+
+	public boolean isUsedAdminRefreshToken(Long id, String refreshToken) {
+		String expiredRefreshToken = jwtRedis.getAdminRefreshToken(String.valueOf(id));
+
+		return expiredRefreshToken.equals(refreshToken);
+	}
+
+	public void addUsedAdminTokens(Long id, String accessToken, String refreshToken) {
+		jwtRedis.saveAdminAccessToken(String.valueOf(id), accessToken);
+		jwtRedis.saveAdminRefreshToken(String.valueOf(id), refreshToken);
 	}
 }
