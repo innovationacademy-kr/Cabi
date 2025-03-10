7e7b6c3acde84e8288375603fb6452e8628b2f99
saewoo1 <wooozooo1123@icloud.com>
2025-03-10 14:43:11 +0900

    REFACTOR: Claim으로 생성되는 UserInfoDto 팩토리 메서드 추가

--- backend/src/main/java/org/ftclub/cabinet/dto/UserInfoDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/UserInfoDto.java
@@ -1,8 +1,11 @@
 package org.ftclub.cabinet.dto;
 
+import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import org.ftclub.cabinet.exception.ExceptionStatus;
+import org.ftclub.cabinet.jwt.domain.JwtTokenConstants;
 
 @AllArgsConstructor
 @ToString
@@ -13,4 +16,14 @@ public class UserInfoDto {
 	private final String oauth;
 	private final String roles;
 
+	public static UserInfoDto fromClaims(Claims claims) {
+		Long userId = claims.get(JwtTokenConstants.USER_ID, Long.class);
+		String provider = claims.get(JwtTokenConstants.OAUTH, String.class);
+		String roles = claims.get(JwtTokenConstants.ROLES, String.class);
+
+		if (userId == null || provider == null || roles == null) {
+			throw ExceptionStatus.JWT_EXCEPTION.asDomainException();
+		}
+		return new UserInfoDto(userId, provider, roles);
+	}
 }
