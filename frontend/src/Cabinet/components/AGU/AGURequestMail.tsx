d57d140ab5367b069cc8bded4e322b0f2b2947da
jnkeniaem <jeanne.kim9@gmail.com>
2025-03-17 18:35:01 +0900

    [FE] FEAT: input css 적용 #1737

--- frontend/src/Cabinet/components/AGU/AGURequestMail.tsx
+++ frontend/src/Cabinet/components/AGU/AGURequestMail.tsx
@@ -1,5 +1,6 @@
 import { HttpStatusCode } from "axios";
 import { useRef } from "react";
+import styled from "styled-components";
 import { AGUSubHeaderStyled } from "@/Cabinet/pages/AGUPage";
 import Button from "@/Cabinet/components/Common/Button";
 import { axiosVerifyAGUUser } from "@/Cabinet/api/axios/axios.custom";
@@ -45,7 +46,7 @@ const AGURequestMail = ({
       <AGUSubHeaderStyled
         dangerouslySetInnerHTML={{ __html: subHeaderMsg }}
       ></AGUSubHeaderStyled>
-      <input ref={idRef} style={{ border: "1px solid black" }}></input>
+      <FormInputStyled ref={idRef} placeholder="인트라 아이디를 입력해주세요" />
       <Button
         onClick={handleButtonClick}
         theme="fill"
@@ -55,5 +56,25 @@ const AGURequestMail = ({
     </>
   );
 };
+
+const FormInputStyled = styled.input`
+  width: 500px;
+  height: 60px;
+  border: 1px solid var(--capsule-btn-border-color);
+  border-radius: 8px;
+  background-color: var(--card-content-bg-color);
+  text-align: left;
+  padding: 0 16px;
+  margin: 48px 0 24px 0;
+  color: #1f1f1f; // TODO : 900
+
+  :focus {
+    border: 1px solid var(--sys-main-color);
+  }
+
+  ::placeholder {
+    color: var(--line-color);
+  }
+`;
 
 export default AGURequestMail;
