a6dc132e4df55ef9aad5b92740d020a6de0ee1f1
saewoo1 <wooozooo1123@icloud.com>
2025-03-18 14:19:42 +0900

    FIX: 중복 필드 제거

--- backend/src/main/java/org/ftclub/cabinet/config/DomainProperties.java
+++ backend/src/main/java/org/ftclub/cabinet/config/DomainProperties.java
@@ -1,6 +1,7 @@
 package org.ftclub.cabinet.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +14,11 @@ public class DomainProperties {
 	private String local;
 	private String dev;
 	private String main;
-	//	private String beHost;
-//	private String feHost;
 	private String adminEmail;
 	private String userEmail;
+	@Value("${cabinet.server.be-host}")
+	private String beHost;
+	@Value("${cabinet.server.fe-host}")
+	private String feHost;
 }
 
