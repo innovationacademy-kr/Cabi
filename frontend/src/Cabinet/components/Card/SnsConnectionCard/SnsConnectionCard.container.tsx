801707dd6a5e1ceeda037c1e2be60850612c34ad
jnkeniaem <jeanne.kim9@gmail.com>
2025-03-23 17:37:06 +0900

    [FE] REFACT: 비즈니스 로직 컨테이너 파일로 이동 #1739

--- frontend/src/Cabinet/components/Card/SnsConnectionCard/SnsConnectionCard.container.tsx
+++ frontend/src/Cabinet/components/Card/SnsConnectionCard/SnsConnectionCard.container.tsx
@@ -1,7 +1,18 @@
+import { HttpStatusCode } from "axios";
 import React from "react";
+import { useSetRecoilState } from "recoil";
+import { userState } from "@/Cabinet/recoil/atoms";
 import SnsConnectionCard from "@/Cabinet/components/Card/SnsConnectionCard/SnsConnectionCard";
 import { IUserOAuthConnectionDto } from "@/Cabinet/types/dto/login.dto";
-import { getSocialAuthUrl } from "@/Cabinet/utils/loginUtils";
+import { UserDto } from "@/Cabinet/types/dto/user.dto";
+import {
+  axiosDisconnectSocialAccount,
+  axiosMyInfo,
+} from "@/Cabinet/api/axios/axios.custom";
+import {
+  getEnabledProviders,
+  getSocialAuthUrl,
+} from "@/Cabinet/utils/loginUtils";
 import { LoginProvider } from "@/Presentation/types/common/loginType";
 
 interface ISnsConnectionCardContainerProps {
@@ -11,24 +22,94 @@ interface ISnsConnectionCardContainerProps {
 const SnsConnectionCardContainer: React.FC<
   ISnsConnectionCardContainerProps
 > = ({ userOauthConnection }) => {
-  // 단일 연결 객체를 배열로 변환
-  // const connections = userOauthConnection ? [userOauthConnection] : [];
-  console.log("====userOauthConnection : ", userOauthConnection);
-  // {email: 'jeekimin3@gmail.com', providerType: 'google'}
-  // 연동된 서비스 배열
+  const setMyLentInfo = useSetRecoilState<UserDto>(userState);
+  const connectedProvider = userOauthConnection
+    ? (userOauthConnection.providerType.toLowerCase() as LoginProvider)
+    : null;
+  // TODO : null말고 빈 문자열로 바꾸기
+  // TODO : 왜 LoginProvider 타입 캐스팅?
+  // console.log("connectedProvider : ", connectedProvider);
+  // ['google']
+  const allProviders = getEnabledProviders();
+  // console.log("allProviders : ", allProviders);
+  // allProviders에서 42(excludeProviders) 제외한 프로바이더 배열
+  const allProvidersWO42: LoginProvider[] = allProviders.filter(
+    (elem) => elem !== "42"
+  );
 
-  // 서비스 연동 기능 - 유틸리티 함수 사용
+  const oauthConnectionAry: IUserOAuthConnectionDto[] = allProvidersWO42.map(
+    (provider) => {
+      if (connectedProvider === provider) {
+        return userOauthConnection!;
+      } else {
+        return {
+          providerType: provider,
+          email: "연결되지 않았습니다",
+        };
+      }
+    }
+  );
+  // console.log("oauthConnectionAry : ", oauthConnectionAry);
+  // ['42', 'google', 'kakao', 'github']
+  const excludeProviders: LoginProvider[] = ["42"];
+  // console.log("excludeProviders : ", excludeProviders);
+  // ['42']
+  const availableProviders = allProviders.filter(
+    (provider) =>
+      !excludeProviders.includes(provider) && connectedProvider !== provider
+  ); // 아직 연동 안된 프로바이더. 42가 아니고, 연동된 프로바이더가 아닌 프로바이더들
+  // console.log("availableProviders : ", availableProviders);
+  // ['kakao', 'github']
+  // TODO: 안 사용하면 지우기
+
+  // console.log("userOauthConnection : ", userOauthConnection);
+  // [{email: 'jeekimin3@gmail.com', providerType: 'google'}]
+
+  const getMyInfo = async () => {
+    try {
+      const response = await axiosMyInfo();
+      setMyLentInfo(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const tryDisconnectSocialAccount = async () => {
+    if (userOauthConnection) {
+      try {
+        const mailState = userOauthConnection.email;
+        const providerTypeState = userOauthConnection.providerType;
+
+        const response = await axiosDisconnectSocialAccount(
+          mailState,
+          providerTypeState
+        );
+
+        if (response.status === HttpStatusCode.Ok) await getMyInfo();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const handleButton = () => {
+    tryDisconnectSocialAccount();
+  };
+
   const handleConnectService = (provider: LoginProvider) => {
     const authUrl = getSocialAuthUrl(provider);
     if (authUrl) {
       window.location.replace(authUrl);
     }
-  };
+  }; // 서비스 연동 기능 - 유틸리티 함수 사용
 
   return (
     <SnsConnectionCard
-      userOauthConnection={userOauthConnection}
       onConnectService={handleConnectService}
+      oauthConnectionAry={oauthConnectionAry}
+      // TODO : oauthConnectionAry oAuthConnectionAry로 수정
+      connectedProvider={connectedProvider}
+      handleButton={handleButton}
     />
   );
 };
--- frontend/src/Cabinet/components/Card/SnsConnectionCard/SnsConnectionCard.tsx
+++ frontend/src/Cabinet/components/Card/SnsConnectionCard/SnsConnectionCard.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
-import { userState } from "@/Cabinet/recoil/atoms";
 import Card from "@/Cabinet/components/Card/Card";
 import {
   CardContentStyled,
@@ -10,110 +8,22 @@ import {
 import { ReactComponent as MinusCircleIcon } from "@/Cabinet/assets/images/minusCircle.svg";
 import { ReactComponent as PlusCircleIcon } from "@/Cabinet/assets/images/plusCircle.svg";
 import { IUserOAuthConnectionDto } from "@/Cabinet/types/dto/login.dto";
-import { UserDto } from "@/Cabinet/types/dto/user.dto";
-import {
-  axiosDisconnectSocialAccount,
-  axiosMyInfo,
-} from "@/Cabinet/api/axios/axios.custom";
-import {
-  getEnabledProviders,
-  getSocialDisplayInfo,
-} from "@/Cabinet/utils/loginUtils";
+import { getSocialDisplayInfo } from "@/Cabinet/utils/loginUtils";
 import { LoginProvider } from "@/Presentation/types/common/loginType";
 
 interface ISnsConnectionCardProps {
-  userOauthConnection: IUserOAuthConnectionDto | null;
   onConnectService: (provider: LoginProvider) => void;
+  oauthConnectionAry: IUserOAuthConnectionDto[];
+  connectedProvider: LoginProvider | null;
+  handleButton: () => void;
 }
 
 const SnsConnectionCard: React.FC<ISnsConnectionCardProps> = ({
-  userOauthConnection,
   onConnectService,
+  oauthConnectionAry,
+  connectedProvider,
+  handleButton,
 }) => {
-  const setMyLentInfo = useSetRecoilState<UserDto>(userState);
-  // const connectedProviders = userOauthConnection
-  //   ? userOauthConnection.map(
-  //       (conn) => conn.providerType.toLowerCase() as LoginProvider
-  //     )
-  //   : [];
-  const connectedProvider = userOauthConnection
-    ? (userOauthConnection.providerType.toLowerCase() as LoginProvider)
-    : null;
-  // TODO : 왜 LoginProvider 타입 캐스팅?
-  // console.log("connectedProvider : ", connectedProvider);
-  // ['google']
-  const allProviders = getEnabledProviders();
-  // console.log("allProviders : ", allProviders);
-  // allProviders에서 42(excludeProviders) 제외한 프로바이더 배열
-  const allProvidersWO42: LoginProvider[] = allProviders.filter(
-    (elem) => elem !== "42"
-  );
-  const oauthConnectionAry: IUserOAuthConnectionDto[] = allProvidersWO42.map(
-    (provider) => {
-      // if (connectedProvider.includes(provider)) {
-      //   const userOauthConnection = userOauthConnection.find(
-      //     (connection) =>
-      //       connection.providerType.toLocaleLowerCase() == provider
-      //   );
-      //   return userOauthConnection!;
-      if (connectedProvider === provider) {
-        return userOauthConnection!;
-      } else {
-        return {
-          providerType: provider,
-          email: "연결되지 않았습니다",
-        };
-      }
-    }
-  );
-  // console.log("oauthConnectionAry : ", oauthConnectionAry);
-  // ['42', 'google', 'kakao', 'github']
-  const excludeProviders: LoginProvider[] = ["42"];
-  // console.log("excludeProviders : ", excludeProviders);
-  // ['42']
-  const availableProviders = allProviders.filter(
-    (provider) =>
-      // !excludeProviders.includes(provider) &&
-      // !connectedProvider.includes(provider)
-      !excludeProviders.includes(provider) && connectedProvider !== provider
-  ); // 아직 연동 안된 프로바이더. 42가 아니고, 연동된 프로바이더가 아닌 프로바이더들
-  // console.log("availableProviders : ", availableProviders);
-  // ['kakao', 'github']
-
-  // console.log("userOauthConnection : ", userOauthConnection);
-  // [{email: 'jeekimin3@gmail.com', providerType: 'google'}]
-
-  const getMyInfo = async () => {
-    try {
-      const response = await axiosMyInfo();
-      setMyLentInfo(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const tryDisconnectSocialAccount = async () => {
-    if (userOauthConnection) {
-      try {
-        const mailState = userOauthConnection.email;
-        const providerTypeState = userOauthConnection.providerType;
-        // TODO : 배열 중 하나 골라야됨
-
-        const response = await axiosDisconnectSocialAccount(
-          mailState,
-          providerTypeState
-        );
-        await getMyInfo();
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
-
-  const handleButton = () => {
-    tryDisconnectSocialAccount();
-  };
-
   return (
     <Card title="소셜 로그인" gridArea="snsConnection" height="290px">
       <>
