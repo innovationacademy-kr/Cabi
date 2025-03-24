8353551d3c48196edd7b482a476411100151e4c7
jnkeniaem <jeanne.kim9@gmail.com>
2025-03-24 22:06:12 +0900

    [FE] FEAT: kakao, github bg color font color 설정 #1739

--- frontend/src/Cabinet/assets/data/login.ts
+++ frontend/src/Cabinet/assets/data/login.ts
@@ -14,6 +14,7 @@ export interface ISocialLoginConfig {
 
 const OAUTH_URL = `${import.meta.env.VITE_BE_HOST}/oauth2/authorization`;
 
+// TODO : 색상 토큰 사용
 export const AUTH_CONFIG: Record<TLoginProvider, ISocialLoginConfig> = {
   42: {
     authUrl: `${OAUTH_URL}/ft`,
@@ -44,8 +45,8 @@ export const AUTH_CONFIG: Record<TLoginProvider, ISocialLoginConfig> = {
     authUrl: `${OAUTH_URL}/kakao`,
     display: {
       text: "kakao 로그인",
-      backgroundColor: "blue",
-      fontColor: "#000000",
+      backgroundColor: "#FEE500",
+      fontColor: "#000000d9",
       icon: React.createElement("img", {
         src: "/src/Cabinet/assets/images/kakaoLogo.svg",
         alt: "kakao",
@@ -56,13 +57,13 @@ export const AUTH_CONFIG: Record<TLoginProvider, ISocialLoginConfig> = {
     authUrl: `${OAUTH_URL}/github`,
     display: {
       text: "gitHub 로그인",
-      backgroundColor: "green",
-      fontColor: "#000000",
+      backgroundColor: "#24292f",
+      fontColor: "#ffffff",
       icon: React.createElement("img", {
         src: "/src/Cabinet/assets/images/githubLogo.svg",
         alt: "gitHub",
       }),
     },
   },
+  // github logo 색 : #24292f
 };
-// TODO : assets/data로 이동?
