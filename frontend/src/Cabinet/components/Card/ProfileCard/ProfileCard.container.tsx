80ca4fc0cdd0d2b2ca5a1bec1ccf16fd5455edff
jnkeniaem <jeanne.kim9@gmail.com>
2025-04-02 13:21:01 +0900

    [FE] REFACT: 안쓰는 코드 삭제 #1741

--- frontend/src/Cabinet/components/Card/ProfileCard/ProfileCard.container.tsx
+++ frontend/src/Cabinet/components/Card/ProfileCard/ProfileCard.container.tsx
@@ -1,12 +1,10 @@
 import { HttpStatusCode } from "axios";
-import { gapi } from "gapi-script";
 import { useNavigate } from "react-router-dom";
 import { useResetRecoilState } from "recoil";
 import {
   currentBuildingNameState,
   currentFloorNumberState,
   currentSectionNameState,
-  myCabinetInfoState,
 } from "@/Cabinet/recoil/atoms";
 import ProfileCard from "@/Cabinet/components/Card/ProfileCard/ProfileCard";
 import { axiosLogout } from "@/Cabinet/api/axios/axios.custom";
@@ -17,7 +15,6 @@ const ProfileCardContainer = ({ name }: { name: string | null }) => {
   const resetCurrentFloor = useResetRecoilState(currentFloorNumberState);
   const resetCurrentSection = useResetRecoilState(currentSectionNameState);
   const resetBuilding = useResetRecoilState(currentBuildingNameState);
-  const resetMyLentInfo = useResetRecoilState(myCabinetInfoState);
 
   const onClickLogoutButton = async (): Promise<void> => {
     try {
@@ -26,16 +23,7 @@ const ProfileCardContainer = ({ name }: { name: string | null }) => {
       if (response.status === HttpStatusCode.Ok) {
         localStorage.setItem("isLoggedOut", "true");
 
-        const { provider } = response.data;
-
-        if (provider === "google") {
-          const googleAuth = gapi.auth2?.getAuthInstance();
-          if (googleAuth) await googleAuth.signOut();
-        } else if (provider === "ft") {
-          const returnUrl = encodeURIComponent(`/login`);
-          window.location.href = `https://profile.intra.42.fr/logout?return_to=${returnUrl}`;
-        }
-      removeCookie("access_token", {
+        removeCookie("access_token", {
           path: "/",
           domain:
             import.meta.env.VITE_IS_LOCAL === "true"
@@ -45,7 +33,6 @@ const ProfileCardContainer = ({ name }: { name: string | null }) => {
         resetBuilding();
         resetCurrentFloor();
         resetCurrentSection();
-        resetMyLentInfo();
         navigator("/login");
       }
     } catch (error) {
