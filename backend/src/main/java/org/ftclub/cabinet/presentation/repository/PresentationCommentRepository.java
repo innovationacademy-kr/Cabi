a06da8e1c7f77bd1db6592b4b171961a741c664a
soonvro <soonvro@gmail.com>
2025-04-07 16:34:43 +0900

    [BE] FEAT: PresentationCommentRepository 추가

---
+++ backend/src/main/java/org/ftclub/cabinet/presentation/repository/PresentationCommentRepository.java
@@ -0,0 +1,8 @@
+package org.ftclub.cabinet.presentation.repository;
+
+import org.ftclub.cabinet.presentation.domain.PresentationComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PresentationCommentRepository extends JpaRepository<PresentationComment, Long> {
+
+}
