dd377dd9a3f6943d0e335c5f9af7267e644970fa
duckgii <shinyonggi@naver.com>
2025-04-07 13:46:25 +0900

    [BE] FEAT: add like Repository

---
+++ backend/src/main/java/org/ftclub/cabinet/presentation/repository/PresentationLikeRepository.java
@@ -0,0 +1,8 @@
+package org.ftclub.cabinet.presentation.repository;
+
+import org.ftclub.cabinet.presentation.domain.PresentationLike;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PresentationLikeRepository extends CrudRepository<PresentationLike, Long> {
+
+}
