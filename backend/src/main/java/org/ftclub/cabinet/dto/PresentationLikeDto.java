b09ad27f253c592601ea8d637928429ac98a4984
duckgii <shinyonggi@naver.com>
2025-04-07 17:40:18 +0900

    [BE] FIX: add NoArgsConstructor

--- backend/src/main/java/org/ftclub/cabinet/dto/PresentationLikeDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/PresentationLikeDto.java
@@ -2,12 +2,14 @@ package org.ftclub.cabinet.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.ftclub.cabinet.presentation.domain.Presentation;
 import org.ftclub.cabinet.user.domain.User;
 
 import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PresentationLikeDto {
 	private Long presentationId;
 	private Long userId;
