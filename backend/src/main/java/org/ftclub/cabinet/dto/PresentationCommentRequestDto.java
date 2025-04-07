aa802a568f3be0812a1a81f44c5fa14ee40d077c
soonvro <soonvro@gmail.com>
2025-04-07 15:04:47 +0900

    [BE] FEAT: PresentationCommentRequestDto, PresentationCommentResponseDto 추가

---
+++ backend/src/main/java/org/ftclub/cabinet/dto/PresentationCommentRequestDto.java
@@ -0,0 +1,16 @@
+package org.ftclub.cabinet.dto;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PresentationCommentRequestDto {
+	@NotNull
+	private String commentDetail;
+}
