c45b8e06ce9f8fe05f84febff08ae808be047917
duckgii <shinyonggi@naver.com>
2025-04-07 13:47:42 +0900

    [BE] FEAT: add Constructor

--- backend/src/main/java/org/ftclub/cabinet/dto/PresentationLikeDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/PresentationLikeDto.java
@@ -1,10 +1,13 @@
 package org.ftclub.cabinet.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.ftclub.cabinet.presentation.domain.Presentation;
 import org.ftclub.cabinet.user.domain.User;
 
 import java.time.LocalDateTime;
-
+@Getter
+@AllArgsConstructor
 public class PresentationLikeDto {
 	private Long presentationId;
 	private Long userId;
