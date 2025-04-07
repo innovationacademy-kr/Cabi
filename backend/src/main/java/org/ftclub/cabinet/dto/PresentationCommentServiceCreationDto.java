81f622bb192d36dc15b36500547377493998fe4b
soonvro <soonvro@gmail.com>
2025-04-07 16:32:01 +0900

    [BE] FEAT: PresentationCommentServiceCreationDto 추가

---
+++ backend/src/main/java/org/ftclub/cabinet/dto/PresentationCommentServiceCreationDto.java
@@ -0,0 +1,16 @@
+package org.ftclub.cabinet.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class PresentationCommentServiceCreationDto {
+    private final Long userId;
+	private final Long presentationId;
+	private final String commentDetail;
+}
