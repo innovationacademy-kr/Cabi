f5a9a50a162d703c52f0a27d5c5205fc582de515
jiminChoi <jimchoi@student.42seoul.co.kr>
2025-04-08 16:18:56 +0900

    [FE] FIX: tailwind preflight: false #1745

--- frontend/src/Presentation/pages/HomePage.tsx
+++ frontend/src/Presentation/pages/HomePage.tsx
@@ -1,11 +1,10 @@
 
-// import '@/Presentation/tailwind.css';
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 const HomePage_test = () => {
-//   return <RecentPresentation presentButtonHandler={presentButtonHandler} />;
-  return <div className="bg-blue-100 w-80 min-h-48">test
-  {/* <Button title="test" >test</Button> */}
+  return <div className="bg-blue-100 w-full h-full">test
+  <Button title="test" >test</Button>
+
   </div>;
 };
 
