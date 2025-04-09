fe97f6bc003e123e8e971cce1799215c8ad55632
jnkeniaem <jeanne.kim9@gmail.com>
2025-04-09 14:51:19 +0900

    [FE] REFACT: SocialAccountLinkCardContent에서 SocialAccountLinkCardContentItem 부분 컴포넌트로 분리 #1760

--- frontend/src/Cabinet/components/Card/SocialAccountLinkCard/SocialAccountLinkCard.tsx
+++ frontend/src/Cabinet/components/Card/SocialAccountLinkCard/SocialAccountLinkCard.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 import Card from "@/Cabinet/components/Card/Card";
 import { TOAuthProviderOrEmpty } from "@/Cabinet/components/Card/SocialAccountLinkCard/SocialAccountLinkCard.container";
+import SocialAccountLinkCardContentItem from "@/Cabinet/components/Card/SocialAccountLinkCard/SocialAccountLinkCardContentItem";
 import ModalPortal from "@/Cabinet/components/Modals/ModalPortal";
 import SocialAccountSwitchModal from "@/Cabinet/components/Modals/SocialAccountLinkModal/SocialAccountSwitchModal";
 import SocialAccountUnlinkModal from "@/Cabinet/components/Modals/SocialAccountLinkModal/SocialAccountUnlinkModal";
 import { TOAuthProvider } from "@/Cabinet/assets/data/oAuth";
-import { ReactComponent as MinusCircleIcon } from "@/Cabinet/assets/images/minusCircle.svg";
-import { ReactComponent as PlusCircleIcon } from "@/Cabinet/assets/images/plusCircle.svg";
 import { IUserOAuthLinkInfoDto } from "@/Cabinet/types/dto/oAuth.dto";
 import { getOAuthDisplayInfo } from "@/Cabinet/utils/oAuthUtils";
 
@@ -48,41 +47,18 @@ const SocialAccountLinkCard = ({
       >
         <CardContentWrapper>
           {userOAuthLinks.map((linkInfo) => {
-            const providerKey = linkInfo.providerType;
-            const displayInfo = getOAuthDisplayInfo(providerKey);
-            const isLinked = linkedProvider === providerKey;
+            const provider = linkInfo.providerType;
 
             return (
-              <CardContentStyled key={providerKey}>
-                <ProviderInfoWrapper>
-                  <ProviderIconWrapper
-                    backgroundColor={displayInfo.backgroundColor}
-                  >
-                    {displayInfo.icon}
-                  </ProviderIconWrapper>
-                  <ProviderInfoTextStyled>
-                    <ProviderNameStyled>{displayInfo.text}</ProviderNameStyled>
-                    {linkInfo.email && (
-                      <EmailAddressStyled isLinked={isLinked}>
-                        {linkInfo.email}
-                      </EmailAddressStyled>
-                    )}
-                  </ProviderInfoTextStyled>
-                </ProviderInfoWrapper>
-                <LinkButtonWrapperStyled isLinked={isLinked}>
-                  {isLinked ? (
-                    <MinusCircleIcon
-                      onClick={() => setIsUnlinkModalOpen(true)}
-                      aria-label="연결 해제"
-                    />
-                  ) : (
-                    <PlusCircleIcon
-                      onClick={() => onLinkSocialAccount(providerKey)}
-                      aria-label="연결"
-                    />
-                  )}
-                </LinkButtonWrapperStyled>
-              </CardContentStyled>
+              <SocialAccountLinkCardContentItem
+                key={provider}
+                provider={provider}
+                onLinkSocialAccount={onLinkSocialAccount}
+                displayInfo={getOAuthDisplayInfo(provider)}
+                linkInfo={linkInfo}
+                isLinked={linkedProvider === provider}
+                setIsUnlinkModalOpen={setIsUnlinkModalOpen}
+              />
             );
           })}
         </CardContentWrapper>
@@ -116,78 +92,6 @@ const CardContentWrapper = styled.div`
   width: 90%;
   display: flex;
   flex-direction: column;
-`;
-
-const CardContentStyled = styled.div`
-  background-color: var(--card-content-bg-color);
-  height: 60px;
-  border-radius: 10px;
-  margin: 5px 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ProviderInfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const ProviderIconWrapper = styled.div<{ backgroundColor: string }>`
-  margin: 0 16px;
-  display: flex;
-  justify-content: center;
-  width: 42px;
-  height: 42px;
-  background-color: ${(props) => props.backgroundColor};
-  border-radius: 50%;
-
-  & > svg {
-    width: 20px;
-  }
-`;
-
-const ProviderInfoTextStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const ProviderNameStyled = styled.div`
-  font-size: 16px;
-  color: var(--normal-text-color);
-`;
-
-const EmailAddressStyled = styled.div<{ isLinked: boolean }>`
-  font-size: 12px;
-  color: var(--gray-line-btn-color);
-  margin-top: 6px;
-`;
-
-const LinkButtonWrapperStyled = styled.button<{ isLinked: boolean }>`
-  margin-right: 16px;
-  width: 18px;
-  height: 18px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  padding: 0;
-  background-color: var(--card-content-bg-color);
-
-  & > svg {
-    width: 16px;
-    height: 16px;
-  }
-
-  :hover {
-    opacity: 0.8;
-  }
-
-  & > svg > circle,
-  & > svg > path {
-    stroke-width: 1.2;
-    stroke: ${(props) => !props.isLinked && "var(--line-color)"};
-  }
 `;
 
 export default SocialAccountLinkCard;
