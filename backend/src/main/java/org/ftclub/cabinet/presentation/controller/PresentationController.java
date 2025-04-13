69fab0228cd10aa17b35ad5198788ce5176cb06f
Jihyun Kim <ksk208105@gmail.com>
2025-04-14 02:43:25 +0900

    [BE] FEAT: Presentation 생성 Controller 작성

--- backend/src/main/java/org/ftclub/cabinet/presentation/controller/PresentationController.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/controller/PresentationController.java
@@ -1,26 +1,18 @@
 package org.ftclub.cabinet.presentation.controller;
 
 
-import java.time.YearMonth;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.ftclub.cabinet.dto.AbleDateResponseDto;
-import org.ftclub.cabinet.dto.InvalidDateResponseDto;
 import org.ftclub.cabinet.dto.PresentationFormRequestDto;
-import org.ftclub.cabinet.dto.PresentationFormResponseDto;
-import org.ftclub.cabinet.dto.PresentationMainData;
-import org.ftclub.cabinet.dto.PresentationMyPagePaginationDto;
-import org.ftclub.cabinet.dto.UserInfoDto;
-import org.ftclub.cabinet.presentation.service.PresentationService;
-import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.ftclub.cabinet.dto.UserDto;
+import org.ftclub.cabinet.presentation.service.PresentationFacadeService;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -28,7 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PresentationController {
 
-	private final PresentationService presentationService;
+	private final PresentationFacadeService presentationFacadeService;
 
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void registerPresentation(
+			@AuthenticationPrincipal UserDto user,
+			@Valid @RequestPart("form") PresentationFormRequestDto form,
+			@RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail
+	) {
+		presentationFacadeService.registerPresentation(
+				user.getUserId(),
+				form,
+				thumbnail
+		);
+	}
 
 }
