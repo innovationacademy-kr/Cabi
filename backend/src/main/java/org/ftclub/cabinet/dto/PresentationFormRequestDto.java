f058be0edc4bf2066832b0f81597707ea2c8e001
Jihyun Kim <ksk208105@gmail.com>
2025-04-14 02:40:14 +0900

    [BE] FEAT: Presentation 등록 requestDto 생성

--- backend/src/main/java/org/ftclub/cabinet/dto/PresentationFormRequestDto.java
+++ backend/src/main/java/org/ftclub/cabinet/dto/PresentationFormRequestDto.java
@@ -1,33 +1,47 @@
 package org.ftclub.cabinet.dto;
 
-import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.ftclub.cabinet.presentation.domain.Category;
-import org.ftclub.cabinet.presentation.domain.PresentationLocation;
 import org.ftclub.cabinet.presentation.domain.Duration;
 import org.hibernate.validator.constraints.Length;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PresentationFormRequestDto {
 
-	private Category category;
+	// TODO: ENUM과 다른 값에 대한 처리
+	@NotNull
 	private Duration duration;
-	private PresentationLocation presentationLocation;
-	private LocalDateTime dateTime;
+	@NotNull
+	private Category category;
+
 	@NotBlank
-	@Length(min = 1, max = 25)
-	private String subject;
+	@Length(min = 1, max = 50)
+	private String title;
 	@NotBlank
-	@Length(min = 1, max = 40)
+	@Length(min = 1, max = 100)
 	private String summary;
 	@NotBlank
 	@Length(min = 1, max = 500)
+	private String outline;
+	@NotBlank
+	@Length(min = 1, max = 10000)
 	private String detail;
+
+	@NotNull
+	private Boolean isRecordingAllowed;
+	@NotNull
+	private Boolean isPublicAllowed;
+
+	// TODO: 유효한 슬롯인지 확인하는 로직 필요
+	@NotNull
+	private Long slotId;
+
 }
