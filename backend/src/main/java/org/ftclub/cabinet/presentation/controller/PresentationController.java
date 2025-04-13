f1ed92ed54b88487658b6c415cae4bbcd6c6e2c6
Jihyun Kim <ksk208105@gmail.com>
2025-04-14 06:34:53 +0900

    [BE] FIX: User를 판별하지 못하는 문제 해결: UserInfoDto로 변경

--- backend/src/main/java/org/ftclub/cabinet/presentation/controller/PresentationController.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/controller/PresentationController.java
@@ -4,7 +4,7 @@ package org.ftclub.cabinet.presentation.controller;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.dto.PresentationFormRequestDto;
-import org.ftclub.cabinet.dto.UserDto;
+import org.ftclub.cabinet.dto.UserInfoDto;
 import org.ftclub.cabinet.presentation.service.PresentationFacadeService;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,7 +24,7 @@ public class PresentationController {
 
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void registerPresentation(
-			@AuthenticationPrincipal UserDto user,
+			@AuthenticationPrincipal UserInfoDto user,
 			@Valid @RequestPart("form") PresentationFormRequestDto form,
 			@RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail
 	) {
