1be516679889644c186039cc9c05abd8fbfce0db
lamodadite <nmb11921@naver.com>
2025-04-13 13:10:58 +0900

    [BE] FEAT : 어드민 슬롯 수정 Service 추가

--- backend/src/main/java/org/ftclub/cabinet/admin/presentation/service/AdminPresentationSlotService.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/presentation/service/AdminPresentationSlotService.java
@@ -66,12 +66,37 @@ public class AdminPresentationSlotService {
 	}
 
 	/**
+	 * 프레젠테이션 슬롯이 겹치는지 확인합니다.
+	 *
+	 * @param slotId       슬롯 ID
+	 * @param newStartTime 시작 시간
+	 */
+	private void validateSlotUpdateOverlap(Long slotId, LocalDateTime newStartTime) {
+		LocalDateTime overlapStartTime = newStartTime.minusHours(PRESENTATION_SLOT_DURATION);
+		LocalDateTime overlapEndTime = newStartTime.plusHours(PRESENTATION_SLOT_DURATION);
+
+		List<PresentationSlot> overlappingSlots = slotRepository.findByStartTimeBetween(
+				overlapStartTime, overlapEndTime);
+
+		if (overlappingSlots.size() > 1 ||
+				(overlappingSlots.size() == 1 && !overlappingSlots.get(0).getId().equals(slotId))) {
+			throw ExceptionStatus.SLOT_ALREADY_EXISTS.asServiceException();
+		}
+	}
+
+	/**
 	 * 프레젠테이션 슬롯을 업데이트합니다.
 	 *
 	 * @param serviceDto 프레젠테이션 슬롯 정보
 	 */
 	public void updatePresentationSlot(
 			PresentationSlotUpdateServiceDto serviceDto) {
-
+		PresentationSlot slot = slotRepository.findById(serviceDto.getSlotId())
+				.orElseThrow(ExceptionStatus.SLOT_NOT_FOUND::asServiceException);
+		validateSlotPassed(serviceDto.getStartTime());
+		validateSlotUpdateOverlap(serviceDto.getSlotId(), serviceDto.getStartTime());
+		slot.changeSlotStartTime(serviceDto.getStartTime());
+		slot.changeSlotLocation(serviceDto.getLocation());
+		// TODO : 프레젠테이션에 연결된 발표 정보도 수정해야 한다면 발표 수정 기능 개발 후에 추가
 	}
 }
