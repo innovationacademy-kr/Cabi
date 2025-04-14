ddb94581d3deb78eb9fcf00669f72d18f3cdeb75
gykoh42 <karmakoh42@naver.com>
2025-04-15 00:36:40 +0900

    [FE] FEAT: shadcn/ui 로 TopNav 기본 레이아웃 구성 #1767

--- frontend/src/Presentation/components/TopNav.tsx
+++ frontend/src/Presentation/components/TopNav.tsx
@@ -1,23 +1,43 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
 
 const TopNav = () => {
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
+          <Link to="home">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              모아보기
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="register">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              신청하기
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="about">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              수요지식회란?
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="profile">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              마이페이지
+            </NavigationMenuLink>
+          </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
