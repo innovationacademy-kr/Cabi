8a9f943d17d658ef7bd9bc47499191f6ffa5f3c9
soonvro <soonvro@gmail.com>
2025-04-14 14:38:59 +0900

    FEAT: findByPresentationIdOrderByCreatedAtAsc 레포지토리 메소드 구현

--- backend/src/main/java/org/ftclub/cabinet/presentation/repository/PresentationCommentRepository.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/repository/PresentationCommentRepository.java
@@ -1,8 +1,11 @@
 package org.ftclub.cabinet.presentation.repository;
 
+import java.util.List;
 import org.ftclub.cabinet.presentation.domain.PresentationComment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PresentationCommentRepository extends JpaRepository<PresentationComment, Long> {
-
+	@EntityGraph(attributePaths = {"user"})
+	List<PresentationComment> findByPresentationIdOrderByCreatedAtAsc(Long presentationId);
 }
