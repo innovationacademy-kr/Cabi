fffe6d0a0a0ddbf9eaee029fd4551ce9dd8ccae3
soonvro <soonvro@gmail.com>
2025-04-14 16:18:27 +0900

    [BE] REFACTOR: Remove unused EntityGraph annotation from PresentationCommentRepository

--- backend/src/main/java/org/ftclub/cabinet/presentation/repository/PresentationCommentRepository.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/repository/PresentationCommentRepository.java
@@ -2,10 +2,8 @@ package org.ftclub.cabinet.presentation.repository;
 
 import java.util.List;
 import org.ftclub.cabinet.presentation.domain.PresentationComment;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PresentationCommentRepository extends JpaRepository<PresentationComment, Long> {
-	@EntityGraph(attributePaths = {"user"})
 	List<PresentationComment> findByPresentationIdOrderByCreatedAtAsc(Long presentationId);
 }
