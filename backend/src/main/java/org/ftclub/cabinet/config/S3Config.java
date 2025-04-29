7568879312d6ddb0c892970c28facaab73d51429
Jihyun Kim <ksk208105@gmail.com>
2025-04-29 21:34:26 +0900

    [BE] FEAT: 환경에 따라 접근할 수 있는 S3 및 MinIO에 대한 설정 #1759

--- backend/src/main/java/org/ftclub/cabinet/config/S3Config.java
+++ backend/src/main/java/org/ftclub/cabinet/config/S3Config.java
@@ -1,31 +1,64 @@
 package org.ftclub.cabinet.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import java.net.URI;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
+@Slf4j
 @Configuration
 public class S3Config {
-	//	@Value("${cabinet.production}")
-//	private Boolean isProduction;
+
+	// check local or prod, dev
+	@Value("${cabinet.local}")
+	private Boolean isLocal;
+
+	// local values
 	@Value("${cloud.aws.credentials.access-key}")
 	private String accessKey;
 	@Value("${cloud.aws.credentials.secret-key}")
 	private String secretKey;
+	@Value("${cloud.aws.s3.endpoint}")
+	private String endpoint;
+	@Value("${cloud.aws.s3.path-style-access-enabled}")
+	private Boolean pathStyleAccessEnabled;
+
+	// common values
 	@Value("${cloud.aws.region.static}")
 	private String region;
 
-	public S3Config() {
-	}
-
 	@Bean
-	public AmazonS3 s3Client() {
-		AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-		return (AmazonS3) ((AmazonS3ClientBuilder) ((AmazonS3ClientBuilder) AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))).withRegion(this.region)).build();
+	public S3Client s3Client() {
+
+		// local: access s3 through MinIO
+		if (isLocal) {
+			log.debug("Local S3 client accessed");
+
+			// setting for access MinIO-server
+			S3Configuration s3Configuration = S3Configuration.builder()
+					.pathStyleAccessEnabled(pathStyleAccessEnabled)
+					.build();
+
+			// create S3 client by builder-pattern
+			return S3Client.builder()
+					.region(Region.of(region))
+					.endpointOverride(URI.create(endpoint))     // set MinIO server address
+					.credentialsProvider(StaticCredentialsProvider.create(
+							AwsBasicCredentials.create(accessKey, secretKey)
+					))
+					.serviceConfiguration(s3Configuration)        // set path-style S3 configuration
+					.build();
+		}
+
+		// dev & prod: access s3 through ec2 instance (OIDC + IAM role)
+		return S3Client.builder()
+				.region(Region.of(region))
+				.build();
 	}
 }
