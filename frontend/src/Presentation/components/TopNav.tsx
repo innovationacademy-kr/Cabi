1c7793c0e4cc1fa110150ebcf47e46e9e8c0e559
gykoh42 <karmakoh42@naver.com>
2025-05-07 23:50:22 +0900

    [FE] FEAT: shadcn/ui 로 TopNav 기본 레이아웃 구성 및 버그 해결 #1767

--- frontend/src/Presentation/components/TopNav.tsx
+++ frontend/src/Presentation/components/TopNav.tsx
@@ -1,47 +1,48 @@
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Link } from "react-router-dom";
 
 const TopNav = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link to="home">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              모아보기
-            </NavigationMenuLink>
+    <NavigationMenu className="h-14">
+      <NavigationMenuList className="h-14">
+        <NavigationMenuItem className="ml-5">
+          <Link
+            to="home"
+            className="p-3 text-foreground h-full flex items-center"
+          >
+            모아보기
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link to="register">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              신청하기
-            </NavigationMenuLink>
+          <Link
+            to="register"
+            className="p-3 text-foreground h-full flex items-center"
+          >
+            신청하기
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link to="about">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              수요지식회란?
-            </NavigationMenuLink>
+          <Link
+            to="about"
+            className="p-3 text-foreground h-full flex items-center"
+          >
+            수요지식회란?
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link to="profile">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              마이페이지
-            </NavigationMenuLink>
+          <Link
+            to="profile"
+            className="p-3 text-foreground h-full flex items-center"
+          >
+            마이페이지
           </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
 };
-
 export default TopNav;
