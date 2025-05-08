7a6d8fec94cfa5bdee97340cf8c87c3251be58f7
gykoh42 <karmakoh42@naver.com>
2025-05-08 19:04:44 +0900

    [FE] FEAT: 전역 폰트 설정 분리 및 구조 개선 #1767

--- frontend/src/Presentation/pages/Layout.tsx
+++ frontend/src/Presentation/pages/Layout.tsx
@@ -4,13 +4,11 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { userState } from "@/Cabinet/recoil/atoms";
-import LoadingAnimation from "@/Cabinet/components/Common/LoadingAnimation";
 import TopNavDomainGroup from "@/Cabinet/components/TopNav/TopNavDomainGroup/TopNavDomainGroup";
 import { UserDto } from "@/Cabinet/types/dto/user.dto";
 import { axiosMyInfo } from "@/Cabinet/api/axios/axios.custom";
 import { getCookie } from "@/Cabinet/api/react_cookie/cookies";
 import useMenu from "@/Cabinet/hooks/useMenu";
-import LeftNav from "@/Presentation_legacy/components/LeftNav/LeftNav";
 import TopNavContainer from "@/Presentation_legacy/components/TopNav/TopNav.container";
 
 const body: HTMLElement = document.body;
@@ -76,7 +74,7 @@ const Layout = (): JSX.Element => {
   ) : (
     <React.Fragment>
       <TopNavDomainGroup />
-      <div className="font-pretendard">
+      <div className="presentation-root">
         <TopNavContainer setIsLoading={setIsLoading} />
         <WrapperStyled>
           <MainStyled>
