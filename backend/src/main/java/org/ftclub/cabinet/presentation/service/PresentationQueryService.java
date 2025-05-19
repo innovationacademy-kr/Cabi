2ffeff619629b872993587b1b815a639c481b128
Jihyun Kim <ksk208105@gmail.com>
2025-05-19 22:25:01 +0900

    [BE] FEAT: admin에서 연월에 해당하는 presentation 조회 service 구현 #1776

--- backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationQueryService.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationQueryService.java
@@ -3,19 +3,21 @@ package org.ftclub.cabinet.presentation.service;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.ftclub.cabinet.exception.ExceptionStatus;
 import org.ftclub.cabinet.presentation.domain.Presentation;
-import org.ftclub.cabinet.presentation.domain.PresentationStatus;
 import org.ftclub.cabinet.presentation.repository.PresentationRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PresentationQueryService {
+
+	private final PresentationRepository presentationRepository;
+
+	public List<Presentation> getPresentationByYearMonth(YearMonth yearMonth) {
+		LocalDateTime startDate = yearMonth.atDay(1).atStartOfDay();
+		LocalDateTime endDate = startDate.plusMonths(1);
+		return presentationRepository.findAllWithinPeriod(startDate, endDate);
+	}
 
 }
