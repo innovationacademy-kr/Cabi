ac3693f948932f0d142a4cde797c6a3203ef18bf
Jihyun Kim <ksk208105@gmail.com>
2025-05-19 22:27:03 +0900

    [BE] FEAT: admin에서 조회한 presentation responseDto 및 변환하기 위한 mapper 구현 #1776

--- backend/src/main/java/org/ftclub/cabinet/mapper/PresentationMapper.java
+++ backend/src/main/java/org/ftclub/cabinet/mapper/PresentationMapper.java
@@ -1,8 +1,21 @@
 package org.ftclub.cabinet.mapper;
 
+import org.ftclub.cabinet.admin.dto.AdminPresentationResponseDto;
+import org.ftclub.cabinet.presentation.domain.Presentation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PresentationMapper {
 
+	/**
+	 * Presentation 엔티티를 AdminPresentationResponseDto로 변환합니다.
+	 *
+	 * @param presentation 변환할 Presentation 엔티티
+	 * @return 변환된 AdminPresentationResponseDto
+	 */
+	@Mapping(target = "presentationId", source = "id")
+	@Mapping(target = "location", source = "presentationLocation")
+	@Mapping(target = "slotId", source = "slot.id")
+	AdminPresentationResponseDto toAdminPresentationResponseDto(Presentation presentation);
 }
