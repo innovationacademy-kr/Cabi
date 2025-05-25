438e50087905d822fd27d03097cb478449dc30bd
Jihyun Kim <ksk208105@gmail.com>
2025-05-26 04:07:44 +0900

    [BE] FEAT: 유저 페이지에서 presentation 상세정보 조회 controller 구현 #1785

--- backend/src/main/java/org/ftclub/cabinet/presentation/controller/PresentationController.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/controller/PresentationController.java
@@ -6,10 +6,14 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ftclub.cabinet.dto.UserInfoDto;
+import org.ftclub.cabinet.presentation.dto.DataResponseDto;
+import org.ftclub.cabinet.presentation.dto.PresentationDetailDto;
 import org.ftclub.cabinet.presentation.dto.PresentationFormRequestDto;
 import org.ftclub.cabinet.presentation.service.PresentationFacadeService;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -44,6 +48,21 @@ public class PresentationController {
 				form,
 				thumbnail
 		);
+	}
+
+	/**
+	 * 프레젠테이션 상세 정보를 조회합니다.
+	 *
+	 * @param presentationId 프레젠테이션 ID
+	 * @return PresentationDetail
+	 */
+	@GetMapping("/{presentationId}")
+	public DataResponseDto<PresentationDetailDto> getPresentationDetail(
+			@AuthenticationPrincipal UserInfoDto user,
+			@PathVariable Long presentationId) {
+		PresentationDetailDto detail = presentationFacadeService.getPresentationDetail(
+				user, presentationId);
+		return new DataResponseDto<>(detail);
 	}
 
 }
