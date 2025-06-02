07d767c68e634da3ef24e2bd66535535f99041bc
lamodadite <nmb11921@naver.com>
2025-06-02 22:28:21 +0900

    [BE] REFACTOR : year와 month 받을때 YearMonth로 받기로 변경

--- backend/src/main/java/org/ftclub/cabinet/admin/presentation/controller/AdminPresentationSlotController.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/presentation/controller/AdminPresentationSlotController.java
@@ -1,5 +1,6 @@
 package org.ftclub.cabinet.admin.presentation.controller;
 
+import java.time.YearMonth;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.ftclub.cabinet.admin.dto.PresentationSlotResponseDto;
 import org.ftclub.cabinet.admin.dto.PresentationSlotSearchServiceDto;
 import org.ftclub.cabinet.admin.dto.PresentationSlotUpdateServiceDto;
 import org.ftclub.cabinet.admin.presentation.service.AdminPresentationSlotService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -73,19 +75,19 @@ public class AdminPresentationSlotController {
 	/**
 	 * 프레젠테이션 슬롯을 조회합니다.
 	 *
-	 * @param year   년도
-	 * @param month  월
-	 * @param status 상태
+	 * @param yearMonth 년월 (yyyy-MM 형식)
+	 * @param status    상태
 	 * @return 프레젠테이션 슬롯 리스트
 	 */
 	@GetMapping("/slots")
 	public List<PresentationSlotResponseDto> getSlots(
-			@RequestParam int year,
-			@RequestParam int month,
+			@RequestParam(value = "yearMonth")
+			@DateTimeFormat(pattern = "yyyy-MM")
+			YearMonth yearMonth,
 			@RequestParam String status) {
 		return adminPresentationSlotService.getAvailableSlots(new PresentationSlotSearchServiceDto(
-				year,
-				month,
+				yearMonth.getYear(),
+				yearMonth.getMonth().getValue(),
 				status
 		));
 	}
