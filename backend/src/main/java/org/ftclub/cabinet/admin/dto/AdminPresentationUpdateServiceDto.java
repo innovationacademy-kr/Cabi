0bbda3da73581e2608456a698994dd03577dca13
Jihyun Kim <ksk208105@gmail.com>
2025-06-07 11:18:51 +0900

    [BE] REFACTOR: toUpdate 설명 추가 #1788

--- backend/src/main/java/org/ftclub/cabinet/admin/dto/AdminPresentationUpdateServiceDto.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/dto/AdminPresentationUpdateServiceDto.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import org.ftclub.cabinet.presentation.domain.Category;
 import org.ftclub.cabinet.presentation.domain.Duration;
 
+/**
+ * 어드민의 update 요청을 위한 dto로 thumbnail은 별도로 관리합니다.
+ */
 @Getter
 @Builder
 public class AdminPresentationUpdateServiceDto {
@@ -23,6 +26,7 @@ public class AdminPresentationUpdateServiceDto {
 	private final boolean publicAllowed;
 	private final boolean thumbnailUpdated;
 
+	// 프레젠테이션 업데이트 시 변경 가능한 필드들 (위 필드에 변동이 있다면 동일하게 업데이트 필요)
 	private final Set<String> toUpdate = Set.of(
 			"title", "category", "duration",
 			"summary", "outline", "detail",
--- backend/src/main/java/org/ftclub/cabinet/presentation/dto/PresentationUpdateServiceDto.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/dto/PresentationUpdateServiceDto.java
@@ -4,6 +4,9 @@ import java.util.Set;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * 유저의 update 요청을 위한 dto로 thumbnail은 별도로 관리합니다.
+ */
 @Getter
 @Builder
 public class PresentationUpdateServiceDto {
@@ -14,5 +17,6 @@ public class PresentationUpdateServiceDto {
 	private final boolean publicAllowed;
 	private final boolean thumbnailUpdated;
 
+	// 프레젠테이션 업데이트 시 변경 가능한 필드들 (위 필드에 변동이 있다면 동일하게 업데이트 필요)
 	private final Set<String> toUpdate = Set.of("summary", "outline", "detail", "publicAllowed");
 }
