fa37ae61c3922e70e50a4f4b3f51032062cbb6dd
Jihyun Kim <ksk208105@gmail.com>
2025-06-08 23:38:58 +0900

    [BE] FEAT: PresentationMyListDto로 변환하기 위한 매퍼와 presentation 엔티티에 status에 대해 구하는 함수 추가하여 매퍼에서 자동으로 인식할 수 있도록 구현 #1790

--- backend/src/main/java/org/ftclub/cabinet/mapper/PresentationMapper.java
+++ backend/src/main/java/org/ftclub/cabinet/mapper/PresentationMapper.java
@@ -4,6 +4,7 @@ import org.ftclub.cabinet.admin.dto.AdminPresentationCalendarItemDto;
 import org.ftclub.cabinet.admin.dto.AdminPresentationUpdateServiceDto;
 import org.ftclub.cabinet.presentation.domain.Presentation;
 import org.ftclub.cabinet.presentation.dto.PresentationDetailDto;
+import org.ftclub.cabinet.presentation.dto.PresentationMyListDto;
 import org.ftclub.cabinet.presentation.dto.PresentationUpdateData;
 import org.ftclub.cabinet.presentation.dto.PresentationUpdateServiceDto;
 import org.mapstruct.Mapper;
@@ -53,4 +54,18 @@ public interface PresentationMapper {
 	 * @return 변환된 PresentationUpdateData
 	 */
 	PresentationUpdateData toPresentationUpdateData(AdminPresentationUpdateServiceDto updateDto);
+
+	/**
+	 * Presentation 엔티티를 PresentationMyListDto로 변환합니다.
+	 * <p>
+	 * 상태에 대한 정보는 엔티티 내부에 선언된 getCurrentStatus 함수를 통해 계산합니다.
+	 * </p>
+	 *
+	 * @param presentation 변환할 Presentation 엔티티
+	 * @return 변환된 PresentationMyListDto
+	 */
+	@Mapping(target = "presentationId", source = "id")
+	@Mapping(target = "presentationStatus", source = "currentStatus")
+	PresentationMyListDto toPresentationMyListDto(Presentation presentation);
+	
 }
