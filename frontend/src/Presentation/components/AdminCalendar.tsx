cf6f3f4358e2e96b2fa698b9991623f84870a317
gykoh42 <karmakoh42@naver.com>
2025-06-16 21:02:45 +0900

    [FE] FEAT: AdminCalendar 추가 #1786

--- frontend/src/Presentation/components/AdminCalendar.tsx
+++ frontend/src/Presentation/components/AdminCalendar.tsx
@@ -1,35 +1,46 @@
-import Calendar from "@toast-ui/calendar";
 import "@toast-ui/calendar/dist/toastui-calendar.min.css";
-import "@toast-ui/calendar/dist/toastui-calendar.css";
-import React, { useEffect, useRef } from "react";
+import Calendar from "@toast-ui/react-calendar";
+import React, { useRef } from "react";
+import "tui-date-picker/dist/tui-date-picker.css";
+import "tui-time-picker/dist/tui-time-picker.css";
 
 const AdminCalendar: React.FC = () => {
-  const calendarRef = useRef<HTMLDivElement>(null);
-  const calendarInstanceRef = useRef<Calendar | null>(null);
+  const calendarRef = useRef<Calendar>(null);
 
-  useEffect(() => {
-    if (calendarRef.current) {
-      calendarInstanceRef.current = new Calendar(calendarRef.current, {
-        defaultView: "month",
-        useFormPopup: true,
-        useDetailPopup: true,
-        calendars: [
-          {
-            id: "1",
-            name: "발표 슬롯",
-            backgroundColor: "#03bd9e",
-            borderColor: "#03bd9e",
-          },
-        ],
-      });
-    }
+  const calendars = [
+    {
+      id: "1",
+      name: "발표 슬롯",
+      backgroundColor: "#03bd9e",
+      borderColor: "#03bd9e",
+    },
+    {
+      id: "2",
+      name: "발표 슬롯2",
+      backgroundColor: "#4ab4f2",
+      borderColor: "#4ab4f2",
+    },
+    {
+      id: "3",
+      name: "발표 슬롯3",
+      backgroundColor: "#f24a4a",
+      borderColor: "#f24a4a",
+    },
+  ];
 
-    return () => {
-      calendarInstanceRef.current?.destroy();
-    };
-  }, []);
-
-  return <div ref={calendarRef} style={{ height: "600px" }} />;
+  return (
+    <div style={{ height: "700px", padding: "20px" }}>
+      <Calendar
+        ref={calendarRef}
+        height="100%"
+        view="month"
+        useFormPopup={true}
+        useDetailPopup={true}
+        calendars={calendars}
+        usageStatistics={false}
+      />
+    </div>
+  );
 };
 
 export default AdminCalendar;
