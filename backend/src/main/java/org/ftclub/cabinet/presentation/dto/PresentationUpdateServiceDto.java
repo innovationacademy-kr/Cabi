80694f8b33ba6d1257d8acf8ebdc4078dd8055a4
Jihyun Kim <ksk208105@gmail.com>
2025-06-19 12:17:15 +0900

    [BE] REFACTOR: user와 admin의 발표 수정 요청 및 서비스 dto 통일

--- backend/src/main/java/org/ftclub/cabinet/presentation/dto/PresentationUpdateServiceDto.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/dto/PresentationUpdateServiceDto.java
@@ -1,22 +1,33 @@
 package org.ftclub.cabinet.presentation.dto;
 
-import java.util.Set;
 import lombok.Builder;
 import lombok.Getter;
+import org.ftclub.cabinet.presentation.domain.Category;
+import org.ftclub.cabinet.presentation.domain.Duration;
 
 /**
- * 유저의 update 요청을 위한 dto로 thumbnail은 별도로 관리합니다.
+ * 프레젠테이션 수정을 위한 공통 DTO
+ * <p>
+ * USER-toUpdate: summary, outline, detail, publicAllowed, thumbnailS3Key
+ * </p>
+ * <p>
+ * ADMIN-toUpdate: USER-toUpdate + { title, category, duration, videoLink, recordingAllowed }
+ * </p>
  */
 @Getter
 @Builder
 public class PresentationUpdateServiceDto {
 
+	// --- [ADMIN, USER] common field ---
 	private final String summary;
 	private final String outline;
 	private final String detail;
 	private final boolean publicAllowed;
-	private final boolean thumbnailUpdated;
 
-	// 프레젠테이션 업데이트 시 변경 가능한 필드들 (위 필드에 변동이 있다면 동일하게 업데이트 필요)
-	private final Set<String> toUpdate = Set.of("summary", "outline", "detail", "publicAllowed");
+	// --- [ADMIN] field ---
+	private final Duration duration;
+	private final Category category;
+	private final String title;
+	private final String videoLink;
+	private final boolean recordingAllowed;
 }
