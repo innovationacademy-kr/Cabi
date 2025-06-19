1471d79cccbfe75116be74dedef0bc259056fa10
Jihyun Kim <ksk208105@gmail.com>
2025-06-19 12:17:47 +0900

    [BE] FEAT: user의 수정가능한 필드들 외의 수정에 대한 검증 로직 추가

--- backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationPolicyService.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationPolicyService.java
@@ -3,6 +3,7 @@ package org.ftclub.cabinet.presentation.service;
 import lombok.RequiredArgsConstructor;
 import org.ftclub.cabinet.exception.ExceptionStatus;
 import org.ftclub.cabinet.presentation.domain.Presentation;
+import org.ftclub.cabinet.presentation.dto.PresentationUpdateServiceDto;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -79,5 +80,34 @@ public class PresentationPolicyService {
 	 */
 	public void verifyAdminPresentationEditAccess(Presentation presentation) {
 		checkPresentationEditable(presentation);
+	}
+
+	/**
+	 * 유저가 수정 불가능한 필드를 수정했는지 검증합니다.
+	 * <p>
+	 * 수정 불가능한 필드: duration, category, title, videoLink, recordingAllowed
+	 * </p>
+	 *
+	 * @param updateForm 프레젠테이션 수정 DTO
+	 * @param origin     프레젠테이션
+	 */
+	public void checkForIllegalFieldModification(
+			PresentationUpdateServiceDto updateForm, Presentation origin) {
+		// if not allowed fields updated, throw exception
+		if (updateForm.getDuration() != origin.getDuration()) {
+			throw ExceptionStatus.PRESENTATION_UPDATE_DURATION_DENIED.asServiceException();
+		}
+		if (updateForm.getCategory() != origin.getCategory()) {
+			throw ExceptionStatus.PRESENTATION_UPDATE_CATEGORY_DENIED.asServiceException();
+		}
+		if (updateForm.getTitle() != origin.getTitle()) {
+			throw ExceptionStatus.PRESENTATION_UPDATE_TITLE_DENIED.asServiceException();
+		}
+		if (updateForm.getVideoLink() != origin.getVideoLink()) {
+			throw ExceptionStatus.PRESENTATION_UPDATE_VIDEO_LINK_DENIED.asServiceException();
+		}
+		if (updateForm.isRecordingAllowed() != origin.isRecordingAllowed()) {
+			throw ExceptionStatus.PRESENTATION_UPDATE_RECORDING_ALLOWED_DENIED.asServiceException();
+		}
 	}
 }
