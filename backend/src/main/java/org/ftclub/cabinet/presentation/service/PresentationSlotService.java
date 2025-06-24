3ef42a2fffa81e2b035e87201a59b6c9b8f3e32d
lamodadite <nmb11921@naver.com>
2025-06-24 17:18:15 +0900

    [BE] FEAT: 사용 가능한 발표 슬롯 조회를 위한 DTO 및 서비스 구현

---
+++ backend/src/main/java/org/ftclub/cabinet/presentation/service/PresentationSlotService.java
@@ -0,0 +1,37 @@
+package org.ftclub.cabinet.presentation.service;
+
+import static org.ftclub.cabinet.presentation.domain.PresentationSlot.ALLOWED_PERIOD;
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.ftclub.cabinet.dto.AvailablePresentationSlotDto;
+import org.ftclub.cabinet.presentation.dto.DataListResponseDto;
+import org.ftclub.cabinet.presentation.repository.PresentationSlotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PresentationSlotService {
+
+	private static final int FIRST_DAY_OF_MONTH = 1;
+	private final PresentationSlotRepository presentationSlotRepository;
+
+	public static LocalDateTime calculateEndTime(LocalDateTime now) {
+		YearMonth targetMonth = YearMonth.from(now).plusMonths(ALLOWED_PERIOD);
+		return targetMonth.atDay(FIRST_DAY_OF_MONTH).atStartOfDay();
+	}
+
+	public DataListResponseDto<AvailablePresentationSlotDto> getPresentationSlots() {
+		LocalDateTime now = LocalDateTime.now();
+		List<AvailablePresentationSlotDto> slotList = presentationSlotRepository.findByStartTimeBetween(
+						now,
+						calculateEndTime(now)).stream().filter(slot -> !slot.hasPresentation()).
+				map(slot -> new AvailablePresentationSlotDto(slot.getId(),
+						slot.getStartTime())
+				).collect(Collectors.toList());
+		return new DataListResponseDto<>(slotList);
+	}
+}
