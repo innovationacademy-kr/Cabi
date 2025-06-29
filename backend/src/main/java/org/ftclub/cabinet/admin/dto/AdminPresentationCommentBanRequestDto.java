3d8a4956ad50c80c3e438877eb573ed6ab0c6bde
soonvro <soonvro@gmail.com>
2025-06-29 21:24:27 +0900

    [BE] TEST: Admin 발표 댓글 테스트 추가

--- backend/src/main/java/org/ftclub/cabinet/admin/dto/AdminPresentationCommentBanRequestDto.java
+++ backend/src/main/java/org/ftclub/cabinet/admin/dto/AdminPresentationCommentBanRequestDto.java
@@ -1,11 +1,15 @@
 package org.ftclub.cabinet.admin.dto;
 
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class AdminPresentationCommentBanRequestDto {
 
 	@NotNull(message = "banned 값은 null일 수 없습니다.")
