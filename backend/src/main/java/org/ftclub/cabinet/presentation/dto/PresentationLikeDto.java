f8b36b1ae98f4c7f89d431b3eb44b9fe41c0ec5e
duckgii <shinyonggi@naver.com>
2025-07-04 09:42:25 +0300

    [BE] RENAME: PresentationLikeDto 위치를 presentation/dto폴더 내부로 변경

--- backend/src/main/java/org/ftclub/cabinet/presentation/dto/PresentationLikeDto.java
+++ backend/src/main/java/org/ftclub/cabinet/presentation/dto/PresentationLikeDto.java
@@ -1,10 +1,8 @@
-package org.ftclub.cabinet.dto;
+package org.ftclub.cabinet.presentation.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.ftclub.cabinet.presentation.domain.Presentation;
-import org.ftclub.cabinet.user.domain.User;
 
 import java.time.LocalDateTime;
 @Getter
