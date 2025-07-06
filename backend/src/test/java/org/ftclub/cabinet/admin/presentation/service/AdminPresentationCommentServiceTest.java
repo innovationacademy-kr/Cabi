8d0b6fa95f17b04e67b24433bdb9cce1760e3ac5
soonvro <soonvro@gmail.com>
2025-07-06 17:13:39 +0900

    [BE] REFACTOR: 필요없는 import 제

--- backend/src/test/java/org/ftclub/cabinet/admin/presentation/service/AdminPresentationCommentServiceTest.java
+++ backend/src/test/java/org/ftclub/cabinet/admin/presentation/service/AdminPresentationCommentServiceTest.java
@@ -29,7 +29,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
