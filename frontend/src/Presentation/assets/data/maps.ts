378804d44100e92f41e1795ca5f2e7dff659f8ee
gykoh42 <karmakoh42@naver.com>
2025-07-11 20:10:19 +0900

    [FE] FEAT: 불필요한 svg 이미지 관련 파일 삭제 #1780

--- frontend/src/Presentation/assets/data/maps.ts
+++ frontend/src/Presentation/assets/data/maps.ts
@@ -4,11 +4,6 @@ import {
   PresentationPeriodType,
   PresentationStatusType,
 } from "@/Presentation/types/enum/presentation.type.enum";
-import { ReactComponent as AcademicIcon } from "@/Cabinet/assets/images/PresentationAcademic.svg";
-import { ReactComponent as DevelopIcon } from "@/Cabinet/assets/images/PresentationDevelop.svg";
-import { ReactComponent as EtcIcon } from "@/Cabinet/assets/images/PresentationEtc.svg";
-import { ReactComponent as FortyTwoIcon } from "@/Cabinet/assets/images/PresentationFortyTwo.svg";
-import { ReactComponent as JobIcon } from "@/Cabinet/assets/images/PresentationJob.svg";
 
 export const PresentationStatusTypeLabelMap: { [key: string]: string } = {
   DONE: "발표 완료",
@@ -24,14 +19,13 @@ export const PresentationPeriodTypeNumberLabelMap = {
   [PresentationPeriodType.TWO_HOUR]: 120,
 };
 
-
 export const PresentationLocationLabelMap: { [key: string]: string } = {
   BASEMENT: "지하 1층 오픈스튜디오",
   FIRST: "1층 오픈스튜디오",
   THIRD: "3층 세미나실",
 };
 
-export const PresentationCategoryTypeLabelMap = {
+export const PresentationCategoryTypeLabelMap: { [key: string]: string } = {
   [PresentationCategoryType.DEVELOP]: "개발",
   [PresentationCategoryType.STUDY]: "학술",
   [PresentationCategoryType.DISCUSSION]: "토의",
@@ -44,13 +38,4 @@ type presentationCategoryIconMap = {
   [key in PresentationCategoryType]: React.ComponentType<
     React.SVGProps<SVGSVGElement>
   >;
-};
-
-export const presentationCategoryIconMap = {
-  [PresentationCategoryType.DEVELOP]: DevelopIcon,
-  [PresentationCategoryType.STUDY]: AcademicIcon,
-  // [PresentationCategoryType.HOBBY]: HobbyIcon,
-  [PresentationCategoryType.JOB]: JobIcon,
-  [PresentationCategoryType.TASK]: FortyTwoIcon,
-  [PresentationCategoryType.ETC]: EtcIcon,
 };
