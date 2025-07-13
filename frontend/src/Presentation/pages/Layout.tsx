0fe1239a0e4adddba724c1fc9e8959c6a8c3e19e
gykoh42 <karmakoh42@naver.com>
2025-07-13 16:39:32 +0900

    [FE] FEAT: TopNav.container 과 Layout 에서 사물함 정보 불러오는 로직 삭제 #1745

--- frontend/src/Presentation/pages/Layout.tsx
+++ frontend/src/Presentation/pages/Layout.tsx
@@ -1,46 +1,20 @@
 import Footer from "@/components/ui/footer";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
-import { userState } from "@/Cabinet/recoil/atoms";
 import TopNavDomainGroup from "@/Cabinet/components/TopNav/TopNavDomainGroup/TopNavDomainGroup";
-import { UserDto } from "@/Cabinet/types/dto/user.dto";
-import { axiosMyInfo } from "@/Cabinet/api/axios/axios.custom";
 import { getCookie } from "@/Cabinet/api/react_cookie/cookies";
 import useMenu from "@/Cabinet/hooks/useMenu";
 import TopNavContainer from "@/Presentation/components/TopNav/TopNav.container";
 
 const Layout = (): JSX.Element => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const setUser = useSetRecoilState<UserDto>(userState);
-  const navigate = useNavigate();
   const location = useLocation();
   const { closeAll } = useMenu();
 
   const token = getCookie("access_token");
   const isLoginPage = location.pathname === "/login";
-
-  const getMyInfo = async () => {
-    try {
-      const { data: myInfo } = await axiosMyInfo();
-      setUser(myInfo);
-      if (isLoginPage) {
-        navigate("/presentations/home", { replace: true });
-      }
-    } catch {
-      navigate("/login");
-    }
-  };
-
-  useEffect(() => {
-    if (!token && !isLoginPage) {
-      navigate("/login");
-    } else if (token) {
-      getMyInfo();
-    }
-  }, [token, isLoginPage, navigate]);
 
   useEffect(() => {
     const root = document.documentElement;
